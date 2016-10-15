24989eb54543af9738e32f67347cefa5f504f725
Pavel Egorov <xoposhiy@mail.ru>
2016-10-15 16:44:26 +0500

    больше подсказок

--- Challenge/WordsStatistics_Tests.cs
+++ Challenge/WordsStatistics_Tests.cs
@@ -1,40 +1,50 @@
-﻿using FluentAssertions;
+﻿using System;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace Challenge
 {
-	[TestFixture]
-	public class WordsStatistics_Tests
-	{
-		public virtual IWordsStatistics CreateStatistics()
-		{
-			// меняется на разные реализации при запуске exe
-			return new WordsStatistics();
-		}
+    [TestFixture]
+    public class WordsStatistics_Tests
+    {
+        public virtual IWordsStatistics CreateStatistics()
+        {
+            // меняется на разные реализации при запуске exe
+            return new WordsStatistics();
+        }
 
-	    private IWordsStatistics statistics;
+        private IWordsStatistics statistics;
 
-		[SetUp]
-		public void SetUp()
-		{
-			statistics = CreateStatistics();
-		}
-
-		[Test]
-		public void GetStatistics_IsEmpty_AfterCreation()
-		{
-			statistics.GetStatistics().Should().BeEmpty();
-		}
+        [SetUp]
+        public void SetUp()
+        {
+            statistics = CreateStatistics();
+        }
 
         [Test]
-		public void AddWord_CountsOnce_WhenSameWord()
-		{
-			statistics.AddWord("aaaaaaaaaa");
-			statistics.AddWord("aaaaaaaaaa");
-			statistics.GetStatistics().Should().HaveCount(1);
-		}
-        
-        // FluentAssetions docs: http://www.fluentassertions.com/
-        // AssertionOptions.AssertEquivalencyUsing(options => options.WithStrictOrdering());
+        public void GetStatistics_IsEmpty_AfterCreation()
+        {
+            statistics.GetStatistics().Should().BeEmpty();
+        }
+
+        [Test]
+        public void AddWord_CountsOnce_WhenSameWord()
+        {
+            statistics.AddWord("aaaaaaaaaa");
+            statistics.AddWord("aaaaaaaaaa");
+            statistics.GetStatistics().Should().HaveCount(1);
+        }
+
+        /* 
+            Коллекции без учёта порядка
+                new[] {1,2,3}.ShouldAllBeEquivalentTo(new [] {3,2,1});
+            Коллекции с учётом порядка
+                new[] {1,2,3}.ShouldAllBeEquivalentTo(new [] {1,2,3}, o => o.WithStrictOrdering());
+            Больше примеров: http://www.fluentassertions.com/
+
+            Исключения:
+                Assert.Throws<ArgumentNullException>(() => SomeOperation());
+            
+        */
     }
 }
