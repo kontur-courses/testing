dc395d1d7a05c91d93882066b7f8351d7a045bd9
Pavel Egorov <xoposhiy@mail.ru>
2017-08-09 09:40:53 +0500

    intern-2017 results

---
+++ Samples/Specifications/Summator_Should.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+
+namespace Samples.Specifications
+{
+	[TestFixture]
+	public class Summator_Should
+	{
+		[Test]
+		public void DoSomething_WhenSomething()
+		{
+			
+		}
+
+	}
+}
+
+
+
+
+
+
