ef7d50e3877f776ceafe39bfcc6854517a6b4a75
Шеломенцев Иван Антонович <shelomentsev@skbkontur.ru>
2017-10-16 17:48:31 +0500

    Added exercises

---
+++ Exercise/ObjectComparison.cs/UseYourHead.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace ObjectComparison.cs
+{
+	public class UseYourHead
+	{
+		public List<string> List = new List<string>();
+		// Задача - отрефакторить и стабилизировать тест с более разумным использованием 
+		// аттрибута timeout.
+
+		[Test]
+		[Timeout(6)]
+		public void Test()
+		{
+			AddToListAndSort("1");
+			AddToListAndSort("1");
+			AddToListAndSort("цуфа5");
+			AddToListAndSort("5ыавы");
+			AddToListAndSort("ываыва5");
+		}
+
+		public void AddToListAndSort(string item)
+		{
+			List.Sort();
+			List.Add(item);
+			List.Sort();
+		}
+	}
+}
