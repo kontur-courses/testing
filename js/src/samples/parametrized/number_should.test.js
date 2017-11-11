2c0987fc794038e93e4cc0ce6d3c4e916640cfce
disturm <disturm@gmail.com>
2017-11-11 15:23:08 +0500

    minor fixes

--- js/src/samples/parametrized/number_should.test.js
+++ js/src/samples/parametrized/number_should.test.js
@@ -1,7 +1,7 @@
 import {assert} from "chai";
 
-describe('Number should', () => {
-    describe('divide', () => {
+describe("Number should", () => {
+    describe("divide", () => {
         const tests = [
             {a: 12, b: 3, expected: 4},
             {a: 12, b: 2, expected: 6},
@@ -15,18 +15,16 @@ describe('Number should', () => {
         });
     });
 
-    describe('parse', () => {
-        const tests = [
+    describe("parse", () => {
+        [
             {input: "123", expectedResult: 123, testName: "integer"},
             {input: "1.1", expectedResult: 1.1, testName: "faction"},
             {input: "1.1e1", expectedResult: 1.1e1, testName: "scientific with positive exp"},
             {input: "1.1e-1", expectedResult: 1.1e-1, testName: "scientific with negative exp"},
             {input: "-0.1", expectedResult: -0.1, testName: "negative fraction"}
-        ];
-
-        tests.forEach(test => {
-            it(test.testName, () => {
-                assert.equal(Number.parseFloat(test.input), test.expectedResult);
+        ].forEach(({input, expectedResult, testName}) => {
+            it(testName, () => {
+                assert.equal(Number.parseFloat(input), expectedResult);
             });
         });
     });
