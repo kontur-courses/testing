b3d96164ba547e8608883bb1b715d560a6b6b834
Ilya Eremin <lotro246135@mail.ru>
2018-10-29 01:36:58 +0500

    Сделал дз

--- cs/HomeExercises/NumberValidatorTests.cs
+++ cs/HomeExercises/NumberValidatorTests.cs
@@ -2,33 +2,58 @@
 using System.Text.RegularExpressions;
 using FluentAssertions;
 using NUnit.Framework;
+using NUnit.Framework.Constraints;
 
 namespace HomeExercises
 {
 	public class NumberValidatorTests
 	{
 		[Test]
-		public void Test()
-		{
+		public void NumberValidator_NegativePrecision_Fail() =>
 			Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
-			Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
-			Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, false));
+		[Test]
+		public void NumberValidator_NegativeScale_Fail() =>
+			Assert.Throws<ArgumentException>(() => new NumberValidator(3, -1, true));
+		[Test]
+		public void NumberValidator_ScaleMoreThenPrecision_Fail() =>
+			Assert.Throws<ArgumentException>(() => new NumberValidator(4, 5, true));
+		[Test]
+		public void NumberValidator_Initialization_Success() =>
 			Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
 
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0"));
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("00.00"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-0.00"));
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+0.00"));
-			Assert.IsTrue(new NumberValidator(4, 2, true).IsValidNumber("+1.23"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+1.23"));
-			Assert.IsFalse(new NumberValidator(17, 2, true).IsValidNumber("0.000"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
+        [TestCase(10,2, true, null, ExpectedResult = false,
+	        TestName = "Null string in IsValidNumber")]
+        [TestCase(10, 2, true, "", ExpectedResult = false, 
+	        TestName = "Empty string in IsValidNumber")]
+        [TestCase(10, 2, true, "0zrt0", ExpectedResult = false, 
+	        TestName = "Invalid string format in IsValidNumber")]
+        [TestCase(4, 3, true, "123.33", ExpectedResult = false,
+	        TestName = "Simbols in number more than precision in IsValidNumber")]
+        [TestCase(11, 3, true, "123.33240", ExpectedResult = false,
+	        TestName = "Simbols in fraction part more than scale in IsValidNumber")]
+        [TestCase(11, 3, true, "-2.44", ExpectedResult = false,
+	        TestName = "Incorrect only positive namber in IsValidNumber")]
+        [TestCase(4, 3, true, "3.141", ExpectedResult = true,
+	        TestName = "Corect only positive namber in IsValidNumber")]
+        [TestCase(11, 3, false, "-2.44", ExpectedResult = true,
+	        TestName = "Negative namber in IsValidNumber")]
+        [TestCase(11, 3, false, "+2.44", ExpectedResult = true,
+	        TestName = "Positive namber in IsValidNumber")]
+        [TestCase(4, 0, true, "3", ExpectedResult = true, 
+	        TestName = "Integer number in IsValidNumber")]
+        [TestCase(4, 3, true, "3.2.2", ExpectedResult = false,
+	        TestName = "More one fraction part in IsValidNumber")]
+        [TestCase(4, 2, false, "+2.44", ExpectedResult = true,
+	        TestName = "Consider the sign in front of the number in IsValidNumber")]
+        [TestCase(3, 2, false, "2,44", ExpectedResult = true,
+	        TestName = "Consider a comma in IsValidNumber")]
+        [TestCase(3, 2, false, ".44", ExpectedResult = false,
+	        TestName = "No integer part in IsValidNumber")]
+        public bool Test_IsValidNumber(int precision, int scale, bool onlyPositive,  string input)
+		{
+			return new NumberValidator(precision, scale,onlyPositive ).IsValidNumber(input);
 		}
-	}
+    }
 
 	public class NumberValidator
 	{
--- cs/HomeExercises/ObjectComparison.cs
+++ cs/HomeExercises/ObjectComparison.cs
@@ -1,83 +1,105 @@
-﻿using FluentAssertions;
+﻿using System;
+using FluentAssertions;
+using FluentAssertions.Equivalency;
+using JetBrains.Annotations;
 using NUnit.Framework;
 
 namespace HomeExercises
 {
-	public class ObjectComparison
-	{
-		[Test]
-		[Description("Проверка текущего царя")]
-		[Category("ToRefactor")]
-		public void CheckCurrentTsar()
-		{
-			var actualTsar = TsarRegistry.GetCurrentTsar();
+    public class ObjectComparison
+    {
+        [Test]
+        [Description("Проверка текущего царя")]
+        [Category("ToRefactor")]
+        public void CheckCurrentTsar()
+        {
+            var actualTsar = TsarRegistry.GetCurrentTsar();
 
-			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
-				new Person("Vasili III of Russia", 28, 170, 60, null));
+            var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
+                new Person("Vasili III of Russia", 28, 170, 60, null));
 
-			// Перепишите код на использование Fluent Assertions.
-			Assert.AreEqual(actualTsar.Name, expectedTsar.Name);
-			Assert.AreEqual(actualTsar.Age, expectedTsar.Age);
-			Assert.AreEqual(actualTsar.Height, expectedTsar.Height);
-			Assert.AreEqual(actualTsar.Weight, expectedTsar.Weight);
+            actualTsar.Should()
+                .BeEquivalentTo(expectedTsar, options => options.Excluding(info =>
+                    info.SelectedMemberInfo.DeclaringType == typeof(Person) &&
+                    info.SelectedMemberInfo.Name == nameof(Person.Id)));
+        }
 
-			Assert.AreEqual(expectedTsar.Parent.Name, actualTsar.Parent.Name);
-			Assert.AreEqual(expectedTsar.Parent.Age, actualTsar.Parent.Age);
-			Assert.AreEqual(expectedTsar.Parent.Height, actualTsar.Parent.Height);
-			Assert.AreEqual(expectedTsar.Parent.Parent, actualTsar.Parent.Parent);
-		}
+        [Test]
+        [Description("Альтернативное решение. Какие у него недостатки?")]
+        public void CheckCurrentTsar_WithCustomEquality()
+        {
+            var actualTsar = TsarRegistry.GetCurrentTsar();
+            var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
+                new Person("Vasili III of Russia", 28, 170, 60, null));
 
-		[Test]
-		[Description("Альтернативное решение. Какие у него недостатки?")]
-		public void CheckCurrentTsar_WithCustomEquality()
-		{
-			var actualTsar = TsarRegistry.GetCurrentTsar();
-			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
-				new Person("Vasili III of Russia", 28, 170, 60, null));
+            // Данный подход менее читаемый, т.к. дописан дополнительный класс AreEqual.
+            // Такой подход менее расширяемый, т.е. если добавить новые поля в класс Person придётся
+            // менять AreEqual,
+            // а в моём решении придётся подправить только инициализацию в случае,
+            // если значение поля будет задваться через конструктуор.
+            // Но если добавить поле, которое мы не хотим учитывать при сравнении объектов,
+            // такое как Id, то в моё решение придётся добавлять ещё один Excluding, 
+            // Эту проблему можно решить вот так:  
+            //actualTsar.Should().BeEquivalentTo(expectedTsar, MakeOptions(nameof(Person.Id)));
 
-			// Какие недостатки у такого подхода? 
-			Assert.True(AreEqual(actualTsar, expectedTsar));
-		}
+            //public Func<EquivalencyAssertionOptions<Person>, EquivalencyAssertionOptions<Person>> MakeOptions(
+            // params string[] fields)
+            //{
+            // Func<EquivalencyAssertionOptions<Person>, EquivalencyAssertionOptions<Person>> predFunc = options =>
+            // {
+            //  foreach (var field in fields)
+            //  {
+            //   options.Excluding(info =>
+            //    info.SelectedMemberInfo.DeclaringType == typeof(Person) &&
+            //    info.SelectedMemberInfo.Name == field);
+            //  }
+            //  return options;
+            // };
+            // return predFunc;
+            //}
+            // Но такой код не становится более читаемым, но зато расширяемость возрастает.
+            Assert.True(AreEqual(actualTsar, expectedTsar));
+        }
 
-		private bool AreEqual(Person actual, Person expected)
-		{
-			if (actual == expected) return true;
-			if (actual == null || expected == null) return false;
-			return
-				actual.Name == expected.Name
-				&& actual.Age == expected.Age
-				&& actual.Height == expected.Height
-				&& actual.Weight == expected.Weight
-				&& AreEqual(actual.Parent, expected.Parent);
-		}
-	}
+        private bool AreEqual(Person actual, Person expected)
+        {
+            if (actual == expected) return true;
+            if (actual == null || expected == null) return false;
+            return
+                actual.Name == expected.Name
+                && actual.Age == expected.Age
+                && actual.Height == expected.Height
+                && actual.Weight == expected.Weight
+                && AreEqual(actual.Parent, expected.Parent);
+        }
+    }
 
-	public class TsarRegistry
-	{
-		public static Person GetCurrentTsar()
-		{
-			return new Person(
-				"Ivan IV The Terrible", 54, 170, 70,
-				new Person("Vasili III of Russia", 28, 170, 60, null));
-		}
-	}
+    public class TsarRegistry
+    {
+        public static Person GetCurrentTsar()
+        {
+            return new Person(
+                "Ivan IV The Terrible", 54, 170, 70,
+                new Person("Vasili III of Russia", 28, 170, 60, null));
+        }
+    }
 
-	public class Person
-	{
-		public static int IdCounter = 0;
-		public int Age, Height, Weight;
-		public string Name;
-		public Person Parent;
-		public int Id;
+    public class Person
+    {
+        public static int IdCounter = 0;
+        public int Age, Height, Weight;
+        public string Name;
+        public Person Parent;
+        public int Id;
 
-		public Person(string name, int age, int height, int weight, Person parent)
-		{
-			Id = IdCounter++;
-			Name = name;
-			Age = age;
-			Height = height;
-			Weight = weight;
-			Parent = parent;
-		}
-	}
+        public Person(string name, int age, int height, int weight, Person parent)
+        {
+            Id = IdCounter++;
+            Name = name;
+            Age = age;
+            Height = height;
+            Weight = weight;
+            Parent = parent;
+        }
+    }
 }
