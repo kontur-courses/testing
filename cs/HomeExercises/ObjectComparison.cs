dd3096ef14bb470713d786ce07fff1de2b1a64e1
Anton Voitsishevskii <grygry0@gmail.com>
2018-10-29 16:57:13 +0300

    Object comparsion  test fix: literal name -> nameof()

--- cs/HomeExercises/ObjectComparison.cs
+++ cs/HomeExercises/ObjectComparison.cs
@@ -13,10 +13,11 @@ namespace HomeExercises
             var actualTsar = TsarRegistry.GetCurrentTsar();
             var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
                                           new Person("Vasili III of Russia", 28, 170, 60, null));
+
             actualTsar.Should()
                       .BeEquivalentTo(expectedTsar,
-                                      options => options.Excluding(info => info.SelectedMemberPath.EndsWith("Id")
-																	   && info.SelectedMemberInfo.DeclaringType == typeof(Person)));
+                                      options => options.Excluding(info => (info.SelectedMemberPath.Equals(nameof(Person.Id)) || info.SelectedMemberPath.EndsWith(nameof(Person.Parent.Id)))
+                                                                       && info.SelectedMemberInfo.DeclaringType == typeof(Person)));
         }
 
         [Test]
