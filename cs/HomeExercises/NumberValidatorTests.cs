d63ca07eee014aef91cdc2d2757d4ba3571168f7
GH2OOST <38152065+GH2OOST@users.noreply.github.com>
2018-10-29 21:29:41 +0500

    NumberValidatorTests refactorings and new tests

--- cs/HomeExercises/NumberValidatorTests.cs
+++ cs/HomeExercises/NumberValidatorTests.cs
@@ -1,42 +1,52 @@
 ﻿using FluentAssertions;
 using NUnit.Framework;
 using System;
-using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 
 namespace HomeExercises
 {
     public class NumberValidatorTests
     {
-	    [TestCase(-1, 2, true, TestName = "ThrowsException_WhenPrecisionIsNegative")]
-	    [TestCase(1, 2, true, TestName = "ThrowsException_WhenScaleBiggerThenPrecision")]
-	    [TestCase(1, -2, true, TestName = "ThrowsException_WhenScaleIsNegative")]
-	    [TestCase(1, 0, false, TestName = "DoesNotThrowsException_WhenValidData")]
-        public void ExceptionTests(int precision, int scale, bool shouldThrow)
+	    [TestCase(-1, 2, TestName = "When Precision Is Negative")]
+	    [TestCase(1, 2, TestName = "When Scale Bigger Then Precision")]
+	    [TestCase(1, -2, TestName = "When Scale Is Negative")]
+        public void Should_ThrowException(int precision, int scale)
 	    {
 		    Action validator = () => new NumberValidator(precision, scale);
-			if (shouldThrow)
-				validator.ShouldThrow<ArgumentException>();
-			else
-				validator.ShouldNotThrow<ArgumentException>();
+			validator.ShouldThrow<ArgumentException>();
         }
 
-        [TestCase(17, 2, true, "0.0", TestName = "IsValid_WhenIntAndFrancPartLessThanPrecisionAndFrancLessThanScale", ExpectedResult = true)]
-        [TestCase(4, 2, true, "+1.23", TestName = "IsValid_WhenIntAndFrancPartLessThanPrecisionAndFrancLessThanScaleWithSign", ExpectedResult = true)]
-        [TestCase(3, 2, true, "00.00", TestName = "IsNotValid_WhenIntAndFrancPartBiggerThanPrecision", ExpectedResult = false)]
-        [TestCase(3, 2, true, "-0.00", TestName = "IsNotValid_WhenIntAndFrancPartWithSignBiggerThanPrecision", ExpectedResult = false)]
-        [TestCase(3, 2, true, "+0.00", TestName = "IsNotValid_WhenIntAndFrancPartWithSignBiggerThanPrecision", ExpectedResult = false)]
-        [TestCase(17, 2, true, "0.000", TestName = "IsNotValid_WhenFrancBiggerThenScale", ExpectedResult = false)]
-        [TestCase(3, 2, true, "a.sd", TestName = "IsNotValid_WhenNotANumber", ExpectedResult = false)]
-        [TestCase(3, 2, true, null, TestName = "IsNotValid_WhenNull", ExpectedResult = false)]
-        [TestCase(3, 2, true, "abrakadabra", TestName = "IsNotValid_WhenAbrakadabra", ExpectedResult = false)]
-        [TestCase(17, 2, true, "-5.7", TestName = "IsNotValid_WhenNegativeIfOnlyPositive", ExpectedResult = false)]
-        [TestCase(17, 2, true, "0,0", TestName = "IsValid_WithComma", ExpectedResult = true)]
-        [TestCase(3, 2, true, "", TestName = "IsNotValid_WhenEmpty", ExpectedResult = false)]
-        public bool Test(int precision, int scale, bool onlyPositive, string value)
-        {
-            return new NumberValidator(precision, scale, onlyPositive).IsValidNumber(value);
+	    [TestCase(1, 0, TestName = "When Valid Data")]
+        public void ShouldNot_ThrowException(int precision, int scale)
+	    {
+		    Action validator = () => new NumberValidator(precision, scale);
+		    validator.ShouldNotThrow<ArgumentException>();
         }
+
+        [TestCase(17, 2, true, "0.0", TestName = "When Int And Fraction Part Less Than Precision And Franc Less Than Scale")]
+        [TestCase(4, 2, true, "+1.23", TestName = "When Int And Fraction Part Less Than Precision And Franc Less Than Scale With Sign")]
+        [TestCase(17, 2, true, "0,0", TestName = "With Comma")]
+		[TestCase(17, 2, false, "-12.2", TestName = "When Negative Number")]
+        [TestCase(17, 2, false, "000012.20", TestName = "When First Are Zeros")]
+        [TestCase(17, 0, false, "1232", TestName = "When Only Int Part")]
+        public void Should_BeValid(int precision, int scale, bool onlyPositive, string value)
+        {
+           new NumberValidator(precision, scale, onlyPositive).IsValidNumber(value).Should().BeTrue();
+        }
+
+		[TestCase(17, 2, false, "-121.", TestName = "When Nothing After Dot")]
+	    [TestCase(3, 2, true, "00.00", TestName = "When Int And Fraction Part Bigger Than Precision")]
+	    [TestCase(3, 2, true, "-0.00", TestName = "When Int And Fraction Part With Sign Bigger Than Precision")]
+	    [TestCase(17, 2, true, "0.000", TestName = "When Fraction Bigger Than Scale")]
+	    [TestCase(3, 2, true, "a.sd", TestName = "When Not A Number")]
+	    [TestCase(3, 2, true, null, TestName = "When Null")]
+        [TestCase(3, 2, true, "abrakadabra", TestName = "When Abrakadabra")]
+	    [TestCase(17, 2, true, "-5.7", TestName = "When Negative If Only Positive")]
+        [TestCase(3, 2, true, "", TestName = "When Empty")]
+        public void ShouldNot_BeValid(int precision, int scale, bool onlyPositive, string value)
+	    {
+		    new NumberValidator(precision, scale, onlyPositive).IsValidNumber(value).Should().BeFalse();
+	    }
     }
 
     public class NumberValidator
