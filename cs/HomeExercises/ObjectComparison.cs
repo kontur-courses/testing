a3d7299701e1b5537eccbd72e3141bd1fcaa3085
Max <lamax_99@mail.ru>
2018-10-30 17:16:08 +0500

    Added protection of rewriting test if class was changed

--- cs/HomeExercises/ObjectComparison.cs
+++ cs/HomeExercises/ObjectComparison.cs
@@ -28,7 +28,8 @@ namespace HomeExercises
 			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
 				new Person("Vasili III of Russia", 28, 170, 60, null));
 
-            actualTsar.Should().BeEquivalentTo(expectedTsar, options => options.ExcludeField("Id"));
+			var fieldName = nameof(actualTsar.Id);
+            actualTsar.Should().BeEquivalentTo(expectedTsar, options => options.ExcludeField(fieldName));
         }
 
 		[Test]
