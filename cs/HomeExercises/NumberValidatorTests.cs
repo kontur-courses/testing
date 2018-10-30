03a5b2c76e0c216f22d04a22800afa8718d43b1e
Gosha20 <gosha3548@gmail.com>
2018-10-30 14:39:16 +0500

    done

--- cs/HomeExercises/NumberValidatorTests.cs
+++ cs/HomeExercises/NumberValidatorTests.cs
@@ -7,26 +7,51 @@ namespace HomeExercises
 {
 	public class NumberValidatorTests
 	{
-		[Test]
-		public void Test()
+		[TestCase(17, 2, true, "0.000", ExpectedResult = false, TestName = "when scale value more then scale")]
+		[TestCase(3, 2, true, "00.00", ExpectedResult = false, TestName = "when length value more then precision ")]
+		[TestCase(3, 2, true, "-5.5", ExpectedResult = false, TestName = "when value include minus and onlyPositive true")]
+		[TestCase(5, 3, false, "--5.5", ExpectedResult = false, TestName = "when value include more then one sign")]
+		[TestCase(5, 3, false, "5.5.5", ExpectedResult = false, TestName = "when value include more then one decimal point")]
+		[TestCase(3, 2, false, "+1.23", ExpectedResult = false, TestName = "when length value with plus more then precision")]
+		[TestCase(3, 2, false, "-0.00", ExpectedResult = false, TestName = "when length value with minus more then precision")]
+		[TestCase(3, 2, false, "", ExpectedResult = false, TestName = "when value is empty string")]
+		[TestCase(3, 2, false, null, ExpectedResult = false, TestName = "when value is null")]
+		[TestCase(3, 2, false, "5.s", ExpectedResult = false, TestName = "when value include letters")]
+		public bool NotValidateNumber(int precision, int scale, bool onlyPositive, string value)
 		{
-			Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
-			Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
-			Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, false));
-			Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
+			return new NumberValidator(precision, scale, onlyPositive).IsValidNumber(value);
+		}
 
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0"));
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("00.00"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-0.00"));
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+0.00"));
-			Assert.IsTrue(new NumberValidator(4, 2, true).IsValidNumber("+1.23"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+1.23"));
-			Assert.IsFalse(new NumberValidator(17, 2, true).IsValidNumber("0.000"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
+		[TestCase(3, 2, false, "-0,0", ExpectedResult = true, TestName = "when separated by comma")]
+		[TestCase(3, 2, false, "-0.0", ExpectedResult = true, TestName = "when separated by decimal point")]
+		[TestCase(17, 2, true, "0", ExpectedResult = true, TestName = "when value without scale")]
+		[TestCase(17, 2, true, "0.0", ExpectedResult = true, TestName = "when value length less then precision")]
+		[TestCase(3, 2, false, "+5.5", ExpectedResult = true, TestName = "when value include plus")]
+		[TestCase(3, 2, false, "-5.5", ExpectedResult = true, TestName = "when value include minus")]
+		public bool ValidateNumber(int precision, int scale, bool onlyPositive, string value)
+		{
+			return new NumberValidator(precision, scale, onlyPositive).IsValidNumber(value);
+		}
+
+
+		[TestCase(-1, 2, true, TestName = "when precision is negative number")]
+		[TestCase(1, -2, true, TestName = "when scale is negative number")]
+		[TestCase(0, 2, true, TestName = "when precision is zero")]
+		[TestCase(1, 1, true, TestName = "when precision equal with scale")]
+		[TestCase(1, 2, true, TestName = "when scale more then precision")]
+		public void ConstructorThrowArgumentException(int precision, int scale, bool onlyPositive)
+		{
+			Action act = () => new NumberValidator(precision, scale, onlyPositive);
+			act.Should().Throw<ArgumentException>();
+		}
+
+
+		[TestCase(1, 0, true, TestName = "when scale is zero")]
+		[TestCase(2, 1, true, TestName = "when scale less then precision")]
+		public void ConstructorDoesNotThrowArgumentException(int precision, int scale, bool onlyPositive)
+		{
+			Action act = () => new NumberValidator(precision, scale, onlyPositive);
+			act.Should().NotThrow<ArgumentException>();
 		}
 	}
 
--- cs/HomeExercises/ObjectComparison.cs
+++ cs/HomeExercises/ObjectComparison.cs
@@ -1,8 +1,21 @@
-﻿using FluentAssertions;
+﻿using System;
+using FluentAssertions;
+using FluentAssertions.Equivalency;
 using NUnit.Framework;
 
 namespace HomeExercises
 {
+	public static class Extension
+	{
+		public static EquivalencyAssertionOptions<T> ExcludingMember<T>(this EquivalencyAssertionOptions<T> option,
+			params string[] fields)
+		{
+			foreach (var field in fields)
+				option.Excluding(t => t.SelectedMemberInfo.Name == field);
+			return option;
+		}
+	}
+
 	public class ObjectComparison
 	{
 		[Test]
@@ -15,16 +28,8 @@ namespace HomeExercises
 			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
 				new Person("Vasili III of Russia", 28, 170, 60, null));
 
-			// Перепишите код на использование Fluent Assertions.
-			Assert.AreEqual(actualTsar.Name, expectedTsar.Name);
-			Assert.AreEqual(actualTsar.Age, expectedTsar.Age);
-			Assert.AreEqual(actualTsar.Height, expectedTsar.Height);
-			Assert.AreEqual(actualTsar.Weight, expectedTsar.Weight);
-
-			Assert.AreEqual(expectedTsar.Parent.Name, actualTsar.Parent.Name);
-			Assert.AreEqual(expectedTsar.Parent.Age, actualTsar.Parent.Age);
-			Assert.AreEqual(expectedTsar.Parent.Height, actualTsar.Parent.Height);
-			Assert.AreEqual(expectedTsar.Parent.Parent, actualTsar.Parent.Parent);
+			expectedTsar.Should().BeEquivalentTo(actualTsar,
+				option => option.ExcludingMember(nameof(Person.Id)));
 		}
 
 		[Test]
@@ -35,7 +40,6 @@ namespace HomeExercises
 			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
 				new Person("Vasili III of Russia", 28, 170, 60, null));
 
-			// Какие недостатки у такого подхода? 
 			Assert.True(AreEqual(actualTsar, expectedTsar));
 		}
 
