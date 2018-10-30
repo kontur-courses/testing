86f2934eb8fec429618af1a40303d12e394e73ed
Vicheslav <rozentor2@gmail.com>
2018-10-30 20:55:09 +0500

    Completed

--- cs/HomeExercises/NumberValidatorTests.cs
+++ cs/HomeExercises/NumberValidatorTests.cs
@@ -5,76 +5,156 @@ using NUnit.Framework;
 
 namespace HomeExercises
 {
-	public class NumberValidatorTests
-	{
-		[Test]
-		public void Test()
-		{
-			Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
-			Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
-			Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, false));
-			Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
 
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0"));
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("00.00"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-0.00"));
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+0.00"));
-			Assert.IsTrue(new NumberValidator(4, 2, true).IsValidNumber("+1.23"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+1.23"));
-			Assert.IsFalse(new NumberValidator(17, 2, true).IsValidNumber("0.000"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
-		}
-	}
+    [TestFixture]
+    public class NumberValidatorTests
+    {
+        [TestFixture]
+        public class Constructor_Should
+        {
+            private Func<int, int, Action> constructorMaker;
 
-	public class NumberValidator
-	{
-		private readonly Regex numberRegex;
-		private readonly bool onlyPositive;
-		private readonly int precision;
-		private readonly int scale;
+            [SetUp]
+            public void SetUp()
+            {
+                constructorMaker = (precison, scale) =>
+                    () => new NumberValidator(precison, scale);
+            }
+            [Test]
+            public void ThrowsArgumentException_WhenPrecisionIsNegativeOrZero()
+            {
+                constructorMaker(0, 0).Should().Throw<ArgumentException>();
+                constructorMaker(-1, 0).Should().Throw<ArgumentException>();
+            }
 
-		public NumberValidator(int precision, int scale = 0, bool onlyPositive = false)
-		{
-			this.precision = precision;
-			this.scale = scale;
-			this.onlyPositive = onlyPositive;
-			if (precision <= 0)
-				throw new ArgumentException("precision must be a positive number");
-			if (scale < 0 || scale >= precision)
-				throw new ArgumentException("precision must be a non-negative number less or equal than precision");
-			numberRegex = new Regex(@"^([+-]?)(\d+)([.,](\d+))?$", RegexOptions.IgnoreCase);
-		}
+            [Test]
+            public void ThrowsArgumentException_WhenScaleIsNegativeOrGreaterOrEqualThanPrecision()
+            {
+                constructorMaker(1, 2).Should().Throw<ArgumentException>();
+                constructorMaker(1, -1).Should().Throw<ArgumentException>();
+                constructorMaker(1, 1).Should().Throw<ArgumentException>();
+            }
 
-		public bool IsValidNumber(string value)
-		{
-			// Проверяем соответствие входного значения формату N(m,k), в соответствии с правилом, 
-			// описанным в Формате описи документов, направляемых в налоговый орган в электронном виде по телекоммуникационным каналам связи:
-			// Формат числового значения указывается в виде N(m.к), где m – максимальное количество знаков в числе, включая знак (для отрицательного числа), 
-			// целую и дробную часть числа без разделяющей десятичной точки, k – максимальное число знаков дробной части числа. 
-			// Если число знаков дробной части числа равно 0 (т.е. число целое), то формат числового значения имеет вид N(m).
+            [Test]
+            public void DoesNotThrowException_WhenPrecisionIsPositiveAndScaleIsNonNegativeAndLessThanPrecision()
+            {
+                constructorMaker(1, 0).Should().NotThrow<Exception>();
+                constructorMaker(2, 1).Should().NotThrow<Exception>();
+            }
+        }
 
-			if (string.IsNullOrEmpty(value))
-				return false;
+        [TestFixture]
+        public class Method_IsValidNumber_Should
+        {
+            private Func<string, bool> checker;
 
-			var match = numberRegex.Match(value);
-			if (!match.Success)
-				return false;
+            [SetUp]
+            public void SetUp()
+            {
+                var validator = new NumberValidator(6, 2);
+                checker = (number) => validator.IsValidNumber(number);
+            }
 
-			// Знак и целая часть
-			var intPart = match.Groups[1].Value.Length + match.Groups[2].Value.Length;
-			// Дробная часть
-			var fracPart = match.Groups[4].Value.Length;
+            [Test]
+            public void ReturnFalse_WhenInputIsNullOrEmpty()
+            {
+                checker(" ").Should().BeFalse();
+                checker(null).Should().BeFalse();
+            }
 
-			if (intPart + fracPart > precision || fracPart > scale)
-				return false;
+            [Test]
+            public void ReturnFalse_WhenFractPartIsGreaterThanScale()
+            {
+                checker("-1.002").Should().BeFalse();
+            }
 
-			if (onlyPositive && match.Groups[1].Value == "-")
-				return false;
-			return true;
-		}
-	}
+            [Test]
+            public void ReturnFalse_WhenIntPartIsGreaterThanPrecision()
+            {
+                checker("1234567").Should().BeFalse();
+                checker("-123456").Should().BeFalse();
+            }
+
+            [Test]
+            public void ReturnTrue_WhenInputLengthLessOrEqualToPrecision()
+            {
+                checker("0").Should().BeTrue();
+                checker("-0.0").Should().BeTrue();
+                checker("000.00").Should().BeTrue();
+                checker("-12345").Should().BeTrue();
+                checker("-1234.5").Should().BeTrue();
+            }
+
+            [Test]
+            public void ReturnFalse_WhenInputIsNotNumber()
+            {
+                checker("lol.xD").Should().BeFalse();
+                checker("Even.1").Should().BeFalse();
+                checker("-2.NO").Should().BeFalse();
+                checker("-2X01").Should().BeFalse();
+                checker("-2.-1").Should().BeFalse();
+            }
+
+            [TestFixture]
+            public class WhenValidatorIsOnlyForPositive
+            {
+                [Test]
+                public void ReturnFalse_WhenInputIsNegative()
+                {
+                    var validator = new NumberValidator(6, 0, true);
+                    Func<string, bool> checker = (number) => validator.IsValidNumber(number);
+                    checker("-1").Should().BeFalse();
+                }
+            }
+        }
+
+    }
+
+    public class NumberValidator
+    {
+        private readonly Regex numberRegex;
+        private readonly bool onlyPositive;
+        private readonly int precision;
+        private readonly int scale;
+
+        public NumberValidator(int precision, int scale = 0, bool onlyPositive = false)
+        {
+            this.precision = precision;
+            this.scale = scale;
+            this.onlyPositive = onlyPositive;
+            if (precision <= 0)
+                throw new ArgumentException("precision must be a positive number");
+            if (scale < 0 || scale >= precision)
+                throw new ArgumentException("scale must be a non-negative number less or equal than precision");
+            numberRegex = new Regex(@"^([+-]?)(\d+)([.,](\d+))?$", RegexOptions.IgnoreCase);
+        }
+
+        public bool IsValidNumber(string value)
+        {
+            // Проверяем соответствие входного значения формату N(m,k), в соответствии с правилом, 
+            // описанным в Формате описи документов, направляемых в налоговый орган в электронном виде по телекоммуникационным каналам связи:
+            // Формат числового значения указывается в виде N(m.к), где m – максимальное количество знаков в числе, включая знак (для отрицательного числа), 
+            // целую и дробную часть числа без разделяющей десятичной точки, k – максимальное число знаков дробной части числа. 
+            // Если число знаков дробной части числа равно 0 (т.е. число целое), то формат числового значения имеет вид N(m).
+
+            if (string.IsNullOrEmpty(value))
+                return false;
+
+            var match = numberRegex.Match(value);
+            if (!match.Success)
+                return false;
+
+            // Знак и целая часть
+            var intPart = match.Groups[1].Value.Length + match.Groups[2].Value.Length;
+            // Дробная часть
+            var fracPart = match.Groups[4].Value.Length;
+
+            if (intPart + fracPart > precision || fracPart > scale)
+                return false;
+
+            if (onlyPositive && match.Groups[1].Value == "-")
+                return false;
+            return true;
+        }
+    }
 }
--- cs/HomeExercises/ObjectComparison.cs
+++ cs/HomeExercises/ObjectComparison.cs
@@ -15,16 +15,9 @@ namespace HomeExercises
 			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
 				new Person("Vasili III of Russia", 28, 170, 60, null));
 
-			// Перепишите код на использование Fluent Assertions.
-			Assert.AreEqual(actualTsar.Name, expectedTsar.Name);
-			Assert.AreEqual(actualTsar.Age, expectedTsar.Age);
-			Assert.AreEqual(actualTsar.Height, expectedTsar.Height);
-			Assert.AreEqual(actualTsar.Weight, expectedTsar.Weight);
-
-			Assert.AreEqual(expectedTsar.Parent.Name, actualTsar.Parent.Name);
-			Assert.AreEqual(expectedTsar.Parent.Age, actualTsar.Parent.Age);
-			Assert.AreEqual(expectedTsar.Parent.Height, actualTsar.Parent.Height);
-			Assert.AreEqual(expectedTsar.Parent.Parent, actualTsar.Parent.Parent);
+		    actualTsar.Should().BeEquivalentTo(expectedTsar,
+		        options => options.Excluding(m => m.SelectedMemberInfo.Name == "Id")
+		    );
 		}
 
 		[Test]
@@ -37,6 +30,7 @@ namespace HomeExercises
 
 			// Какие недостатки у такого подхода? 
 			Assert.True(AreEqual(actualTsar, expectedTsar));
+		    //Переписывать метод AreEqual каждый раз при изменении класса(добавлении нового поля/св-ва) Person БОЛЬ
 		}
 
 		private bool AreEqual(Person actual, Person expected)
