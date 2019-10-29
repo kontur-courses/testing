46431bc00f455881e45df6280d9aadbb9de93a3f
Sergey <hariton27sy@gmail.com>
2019-10-29 16:59:49 +0500

    Repair methods of check

--- cs/HomeExercises/ObjectComparison.cs
+++ cs/HomeExercises/ObjectComparison.cs
@@ -3,113 +3,102 @@ using NUnit.Framework;
 
 namespace HomeExercises
 {
-	public class Test_TsarRegistry
-	{
-		private Person actualKing;
-		private Person expectedKing;
+    public class ObjectComparison
+    {
+        [Test]
+        [Description("Проверка текущего царя")]
+        [Category("ToRefactor")]
+        public void CheckCurrentTsar()
+        {
+            var actualTsar = TsarRegistry.GetCurrentTsar();
 
-		[SetUp]
-		public void SetUp()
-		{
-			// Так как в обоих тестах одни и те же данные
-			// Можно их инициализировать в Сетапе
-			actualKing = TsarRegistry.GetCurrentTsar();
-			expectedKing = new Person("Ivan IV The Terrible", 54, 170, 70,
-				new Person("Vasili III of Russia", 28, 170, 60, null));
+            var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
+                new Person("Vasili III of Russia", 28, 170, 60, null));
+
+            // Перепишите код на использование Fluent Assertions.
+            Test_PersonsAreEqual(expectedTsar, actualTsar);
+
+			// Используя такую проверку можно увидеть на каком именно поле провалился тест.
+			// Также, если в классе появится новое поле, то достаточно добавить одну строку кода
+			// в метод Test_PersonsAreEqual. И в нем по-прежнему остается рекурсивная 
+			// Также данный метод лучше тем что использует нерекурсивную проверку
+			// из-за которой может произойти переполнение стэка
         }
 
-		[Test]
-		[Description("Проверка текущего царя")]
-		[Category("ToRefactor")]
-		public void CheckCurrentKing()
-		{
-			// Использовать Fluent API лучше тем, что можно сразу определить
-			// где ожидаемое значение, а где полученное.
-			actualKing.Should().NotBeNull();
-			actualKing.Name.Should().Be(expectedKing.Name);
-			actualKing.Age.Should().Be(expectedKing.Age);
-			actualKing.Height.Should().Be(expectedKing.Height);
-			actualKing.Weight.Should().Be(expectedKing.Weight);
+        [Test]
+        [Description("Альтернативное решение. Какие у него недостатки?")]
+        public void CheckCurrentTsar_WithCustomEquality()
+        {
+            var actualTsar = TsarRegistry.GetCurrentTsar();
+            var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
+                new Person("Vasili III of Russia", 28, 170, 60, null));
 
-			actualKing.Parent.Should().NotBeNull();
-			actualKing.Parent.Name.Should().Be(expectedKing.Parent.Name);
-			actualKing.Parent.Age.Should().Be(expectedKing.Parent.Age);
-			actualKing.Parent.Height.Should().Be(expectedKing.Parent.Height);
-			actualKing.Parent.Weight.Should().Be(expectedKing.Parent.Weight);
+            // Какие недостатки у такого подхода? 
+            Assert.True(AreEqual(actualTsar, expectedTsar));
+
+			// Если тест упадет, то будет непонятно, в каком именно поле будет ошибка
+			// Так как метод возвращает лишь ответ верны объекты или неверны
+			// Также если будет слишком глубокая рекурсия родителей, то в теории может
+			// произойти переполнение стэка
         }
 
-		[Test]
-		[Description("Альтернативное решение. Какие у него недостатки?")]
-		public void CheckCurrentTsar_WithCustomEquality()
+        private bool AreEqual(Person actual, Person expected)
+        {
+            if (actual == expected) return true;
+            if (actual == null || expected == null) return false;
+            return
+                actual.Name == expected.Name
+                && actual.Age == expected.Age
+                && actual.Height == expected.Height
+                && actual.Weight == expected.Weight
+                && AreEqual(actual.Parent, expected.Parent);
+        }
+
+		public static void Test_PersonsAreEqual(Person expected, Person actual)
 		{
-			// Какие недостатки у такого подхода?
-			// Если тест не пройдет, то будет непонятно, где именно
-			// Assert.True(AreEqual(actualKing, expectedKing));
-
-			// В данном решении проверяется не только отец текущего человека,
-			// но и все отцы отцов
-			// Также, если тест упадет то можно увидеть, на чем ошибка 
-			Test_PersonsAreEqual(expectedKing, actualKing, "current King");
-		}
-
-		private bool AreEqual(Person actual, Person expected)
-		{
-			if (actual == expected) return true;
-			if (actual == null || expected == null) return false;
-			return
-				actual.Name == expected.Name
-				&& actual.Age == expected.Age
-				&& actual.Height == expected.Height
-				&& actual.Weight == expected.Weight
-				&& AreEqual(actual.Parent, expected.Parent);
-		}
-
-
-		public static void Test_PersonsAreEqual(Person expected, Person actual, string because)
-		{
-			actual.Name.Should().Be(expected.Name, $"because names of {because} should be equal");
-			actual.Age.Should().Be(expected.Age, $"because ages of {because} should be equal");
-			actual.Height.Should().Be(expected.Height, $"because heights of {because} should be equal");
-			actual.Weight.Should().Be(expected.Weight, $"because weights of {because} should be equal");
-
-			if (expected.Parent != null)
+			string because = "Person";
+			while (expected != null)
 			{
-				actual.Parent.Should().NotBeNull($"because parent of {because} shouldn't be null");
-				Test_PersonsAreEqual(expected.Parent, actual.Parent, "parent of " + because);
+				actual.Should().NotBeNull();
+				actual.Name.Should().Be(expected.Name, $"because names of {because} should be equal");
+				actual.Age.Should().Be(expected.Age, $"because ages of {because} should be equal");
+				actual.Height.Should().Be(expected.Height, $"because heights of {because} should be equal");
+				actual.Weight.Should().Be(expected.Weight, $"because weights of {because} should be equal");
+				expected = expected.Parent;
+				actual = actual.Parent;
+				because = "Parent of " + because;
 			}
-			else
-			{
-				actual.Parent.Should().BeNull($"because parent of {because} should be null");
-			}
-		}
-	}
 
-	public class TsarRegistry
-	{
-		public static Person GetCurrentTsar()
-		{
-			return new Person(
-				"Ivan IV The Terrible", 54, 170, 70,
-				new Person("Vasili III of Russia", 28, 170, 60, null));
+			actual.Should().BeNull($"because parent of {because} should be null");
 		}
-	}
+    }
 
-	public class Person
-	{
-		public static int IdCounter = 0;
-		public int Age, Height, Weight;
-		public string Name;
-		public Person Parent;
-		public int Id;
+    public class TsarRegistry
+    {
+        public static Person GetCurrentTsar()
+        {
+            return new Person(
+                "Ivan IV The Terrible", 54, 170, 70,
+                new Person("Vasili III of Russia", 28, 170, 60, null));
+        }
+    }
 
-		public Person(string name, int age, int height, int weight, Person parent)
-		{
-			Id = IdCounter++;
-			Name = name;
-			Age = age;
-			Height = height;
-			Weight = weight;
-			Parent = parent;
-		}
-	}
+    public class Person
+    {
+        public static int IdCounter = 0;
+        public int Age, Height, Weight;
+        public string Name;
+        public Person Parent;
+        public int Id;
+
+        public Person(string name, int age, int height, int weight, Person parent)
+        {
+            Id = IdCounter++;
+            Name = name;
+            Age = age;
+            Height = height;
+            Weight = weight;
+            Parent = parent;
+        }
+    }
 }
