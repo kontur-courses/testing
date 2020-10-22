d2bdadd8d1f8403afdf0512bf1da098cc448b9a6
AppleePie <64743297+AppleePie@users.noreply.github.com>
2020-10-22 22:48:11 +0500

    Reset parent of tsar to default

--- cs/HomeExercises/ObjectComparison.cs
+++ cs/HomeExercises/ObjectComparison.cs
@@ -54,8 +54,7 @@ namespace HomeExercises
 		{
 			return new Person(
 				"Ivan IV The Terrible", 54, 170, 70,
-				new Person("Vasili III of Russia", 28, 170, 60, 
-					new Person("Vasili III of Russia", 28, 170, 60, null)));
+				new Person("Vasili III of Russia", 28, 170, 60, null));
 		}
 	}
 
