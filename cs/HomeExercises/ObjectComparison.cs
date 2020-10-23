b364ea54d3b5cedfd86822536e012c81de90d46f
Dima <d.d.demin@urfu.me>
2020-10-23 10:12:30 +0500

    "CheckCurrentTsar" rewritten using FluentAssertion

--- cs/HomeExercises/ObjectComparison.cs
+++ cs/HomeExercises/ObjectComparison.cs
@@ -16,15 +16,11 @@ namespace HomeExercises
 				new Person("Vasili III of Russia", 28, 170, 60, null));
 
 			// Перепишите код на использование Fluent Assertions.
-			Assert.AreEqual(actualTsar.Name, expectedTsar.Name);
-			Assert.AreEqual(actualTsar.Age, expectedTsar.Age);
-			Assert.AreEqual(actualTsar.Height, expectedTsar.Height);
-			Assert.AreEqual(actualTsar.Weight, expectedTsar.Weight);
-
-			Assert.AreEqual(expectedTsar.Parent!.Name, actualTsar.Parent!.Name);
-			Assert.AreEqual(expectedTsar.Parent.Age, actualTsar.Parent.Age);
-			Assert.AreEqual(expectedTsar.Parent.Height, actualTsar.Parent.Height);
-			Assert.AreEqual(expectedTsar.Parent.Parent, actualTsar.Parent.Parent);
+			actualTsar.Should().BeEquivalentTo(expectedTsar,
+				options => options.Excluding(memberInfo =>
+					memberInfo.SelectedMemberInfo.DeclaringType == typeof(Person) &&
+					memberInfo.SelectedMemberInfo.Name == "Id"));
+			//либо можно использовать: memberInfo.SelectedMemberPath.EndsWith()
 		}
 
 		[Test]
