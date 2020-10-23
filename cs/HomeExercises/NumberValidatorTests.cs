5a7003799aa2cc72185a74ec468811a8ba38a7f2
Artemy Gordon <artemy.gordon@gmail.com>
2020-10-24 02:11:20 +0500

    Deleted primordial test

--- cs/HomeExercises/NumberValidatorTests.cs
+++ cs/HomeExercises/NumberValidatorTests.cs
@@ -8,28 +8,6 @@ namespace HomeExercises
     public class NumberValidatorTests
     {
         [Test]
-        public void Test()
-        {
-            Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
-            Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
-            Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2));
-            Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
-
-            Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-            Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0"));
-            Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("00.00"));
-            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-0.00"));
-            Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+0.00"));
-            Assert.IsTrue(new NumberValidator(4, 2, true).IsValidNumber("+1.23"));
-            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+1.23"));
-            Assert.IsFalse(new NumberValidator(17, 2, true).IsValidNumber("0.000"));
-            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
-            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
-        }
-
-        [Test]
         public void NumberValidator_ThrowsException_IfPrecisionIsNotPositive()
         {
             Action act = () => new NumberValidator(0, 2);
