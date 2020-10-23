8b44611d2512b60e303f7c0f967295ef5a9a7ba5
Dima <d.d.demin@urfu.me>
2020-10-23 12:26:04 +0500

    "NumberValidatorTests". Test "CheckValidNumber_TrueOrFalse" was created

--- cs/HomeExercises/NumberValidatorTests.cs
+++ cs/HomeExercises/NumberValidatorTests.cs
@@ -18,10 +18,10 @@ namespace HomeExercises
 
 			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
 			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0"));
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
+			// Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
 			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("00.00"));
 			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-0.00"));
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
+			// Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
 			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+0.00"));
 			Assert.IsTrue(new NumberValidator(4, 2, true).IsValidNumber("+1.23"));
 			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+1.23"));
@@ -30,15 +30,67 @@ namespace HomeExercises
 			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
 		}
 
-		[TestCase(-1, 0, true,TestName = "When precision < 0")]
-		[TestCase(0, TestName = "When precision == 0")]
-		[TestCase(2, -1, TestName = "When scale < 0")]
-		[TestCase(2, -1, TestName = "When scale < 0")]
-		[TestCase(2, 2, true, TestName = "When scale == precision")]
-		[TestCase(2, 3, true, TestName = "When scale > precision")]
-		public void СreateValidator_ThrowArgumentException_FlagOnlyPositiveDoesNotMatter(int precision, int scale = 0, bool onlyPositive = false)
+		[TestCase(-1, 0, true,TestName = "When precision < 0, onlyPositive don't matter")]
+		[TestCase(0, TestName = "When precision == 0, onlyPositive don't matter")]
+		[TestCase(2, -1, TestName = "When scale < 0, onlyPositive don't matter")]
+		[TestCase(2, 2, false, TestName = "When scale == precision, onlyPositive don't matter")]
+		[TestCase(2, 3, true, TestName = "When scale > precision, onlyPositive don't matter")]
+		public void СreateValidator_ThrowArgumentException_FlagOnlyPositiveDoesNotMatter(int precision, int scale = 0, 
+			bool onlyPositive = false)
 		{
 			Assert.Throws<ArgumentException>(() => new NumberValidator(precision, scale, onlyPositive));
+		}
+		
+		[TestCase("", false,17, 2, 
+			TestName = "False when value is empty")]
+		[TestCase(null, false,2, 1, 
+			TestName = "False when value is null")]
+		[TestCase("aaa", false,5, 1, 
+			TestName = "False when value consists only of letters")]
+		[TestCase("\n\t\r", false,5, 1, 
+			TestName = "False when value consists only of special symbols")]
+		[TestCase("+aa", false,5, 2, 
+			TestName = "False when value consists of + and letters")]
+		[TestCase("-aa", false,5, 2, 
+			TestName = "False when value consists of - and letters")]
+		[TestCase("+1.0aa", false,5, 2, 
+			TestName = "False when value consists of +, number and letters at the end")]
+		[TestCase("ff+1.0", false,5, 2, 
+			TestName = "False when value consists of letters at the beginning, + and number")]
+		[TestCase("., -1.0.", false,5, 2, 
+			TestName = "False when value consists of white space or other symbols, - and number")]
+		[TestCase("-1.0", true,5, 2, 
+			TestName = "True when value consists of -, number and . between int and fracPart, onlyPositive is false")]
+		[TestCase("+1.0", true,5, 2, 
+			TestName = "True when value consists of +, number and . between int and fracPart, onlyPositive is false")]
+		[TestCase("-1,0", true,5, 2, 
+			TestName = "True when value consists of -, number and , between int and fracPart, onlyPositive is false")]
+		[TestCase("-12.00", false,3, 2, 
+			TestName = "False when int+fracPart > precision, onlyPositive don't matter")]
+		[TestCase("-14.00", false,4, 1, 
+			TestName = "False when fracPart > scale, onlyPositive don't matter")]
+		[TestCase("+55555.00", true,17, 2, true, 
+			TestName = "True when value consists of + and onlyPositive is true")]
+		[TestCase("55555.00", true,17, 2, true, 
+			TestName = "True when value doesn't consist of + or - and onlyPositive is true")]
+		[TestCase("55555.00", true,17, 2, 
+			TestName = "True when value doesn't consist of + or - and onlyPositive is false")]
+		[TestCase("-55555.00", true,17, 2, 
+			TestName = "True when value consists of - and onlyPositive is false")]
+		[TestCase("-55555.00", false,17, 2, true,
+			TestName = "False when value consists of - and onlyPositive is true")]
+		[TestCase("+0.12", true,17, 2, 
+			TestName = "True when int+fracPart <= precision and positive is false")]
+		[TestCase("+55.010", true,17, 3, true, 
+			TestName = "True when int+fracPart < precision and " + "positive is true")]
+		public void CheckValidNumber_TrueOrFalse(string value, bool actualResult, 
+			int precision, int scale = 0, bool onlyPositive = false)
+		{
+			var validator = new NumberValidator(precision, scale, onlyPositive);
+
+			var expectedResult = validator.IsValidNumber(value);
+			
+			Assert.AreEqual(actualResult, expectedResult);
 		}
 	}
 
