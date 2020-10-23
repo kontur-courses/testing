0c6d2fd3a337cc9fd99cffb1f2c24667c8c78dd1
Alexey <winderton228@yandex.ru>
2020-10-23 19:39:08 +0500

    Change method CheckCurrentTsar()

--- cs/HomeExercises/ObjectComparison.cs
+++ cs/HomeExercises/ObjectComparison.cs
@@ -5,26 +5,25 @@ namespace HomeExercises
 {
 	public class ObjectComparison
 	{
+		/*
+		 * Недостаток метода CheckCurrentTsar_WithCustomEquality() такой:
+		 * При добавлении новых полей, обязательно нужно прописывать новые проверки на равенство полей
+		 * В моем методе проверка по всем полям происходит автоматически
+		 * При довавление нового поля, которое не нужно учитывать, нужно всего лишь прописать дополнительный Excluding()
+		 * К тому же при падении теста в CheckCurrentTsar_WithCustomEquality() 
+		 * Нужно будет искать ошибку в трассировке стека
+		*/
 		[Test]
 		[Description("Проверка текущего царя")]
 		[Category("ToRefactor")]
 		public void CheckCurrentTsar()
 		{
 			var actualTsar = TsarRegistry.GetCurrentTsar();
-
 			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
 				new Person("Vasili III of Russia", 28, 170, 60, null));
 
-			// Перепишите код на использование Fluent Assertions.
-			Assert.AreEqual(actualTsar.Name, expectedTsar.Name);
-			Assert.AreEqual(actualTsar.Age, expectedTsar.Age);
-			Assert.AreEqual(actualTsar.Height, expectedTsar.Height);
-			Assert.AreEqual(actualTsar.Weight, expectedTsar.Weight);
-
-			Assert.AreEqual(expectedTsar.Parent!.Name, actualTsar.Parent!.Name);
-			Assert.AreEqual(expectedTsar.Parent.Age, actualTsar.Parent.Age);
-			Assert.AreEqual(expectedTsar.Parent.Height, actualTsar.Parent.Height);
-			Assert.AreEqual(expectedTsar.Parent.Parent, actualTsar.Parent.Parent);
+			actualTsar!.Should().BeEquivalentTo(expectedTsar!,
+				options => options.Excluding(x => x.SelectedMemberInfo.Name == nameof(Person.Id)));
 		}
 
 		[Test]
