161060fa60a01e06f88b693eae68d4eb0d350b6b
PShenica <polebota@gmail.com>
2020-10-24 12:18:48 +0500

    add new tests for NumberValidator

--- cs/HomeExercises/NumberValidatorTests.cs
+++ cs/HomeExercises/NumberValidatorTests.cs
@@ -7,26 +7,25 @@ namespace HomeExercises
 {
 	public class NumberValidatorTests
 	{
-		[TestCase(-1, 2, true, TestName = "Negative precision")]
-		[TestCase(0, 2, false, TestName = "Zero precision")]
-		[TestCase(2, -1, false, TestName = "Negative scale")]
+		[TestCase(-1, 2, true, true, TestName = "Negative precision")]
+		[TestCase(0, 2, false, true, TestName = "Zero precision")]
+		[TestCase(2, -1, false, true, TestName = "Negative scale")]
+		[TestCase(3, 0, true, false, TestName = "Zero scale")]
+		[TestCase(10, 5, false, false, TestName = "Natural scale")]
+		[TestCase(1, 0, true, false, TestName = "Positive precision")]
 		public void ThrowException_When(
-			int precision, int scale, bool onlyPositive)
+			int precision, int scale, bool onlyPositive, bool shouldThrowException)
 		{
-			Action validator = () => new NumberValidator(precision, scale, onlyPositive);
+			Func<NumberValidator> validator = () => new NumberValidator(precision, scale, onlyPositive);
 
-			validator.Should().Throw<ArgumentException>();
-		}
-
-		[TestCase(3, 0, true, TestName = "Zero scale")]
-		[TestCase(10, 5, false, TestName = "Natural scale")]
-		[TestCase(1, 0, true, TestName = "Positive precision")]
-		public void DontThrowException_When(
-			int precision, int scale, bool onlyPositive)
-		{
-			Action validator = () => new NumberValidator(precision, scale, onlyPositive);
-
-			validator.Should().NotThrow<ArgumentException>();
+			if (shouldThrowException)
+			{
+				validator.Should().Throw<ArgumentException>();
+			}
+			else
+			{
+				validator.Should().NotThrow<ArgumentException>();
+			}
 		}
 
 		[TestCase("+1337", 4, 2, false, false, TestName = "Integer is bigger than precision")]
@@ -46,10 +45,12 @@ namespace HomeExercises
 		[TestCase("-1.0", 4, 2, true, false, TestName = "Negative double when only positive")]
 		[TestCase("-100", 4, 2, true, false, TestName = "Negative integer when only positive")]
 		[TestCase("abc", 4, 2, false, false, TestName = "Only letters")]
-		[TestCase(null, 4,2, false, false, TestName = "String is null")]
+		[TestCase(null, 4, 2, false, false, TestName = "String is null")]
 		[TestCase(" ", 4, 2, false, false, TestName = "String is empty")]
+		[TestCase("+3,", 4, 2, false, false, TestName = "No digits after separator")]
+		[TestCase("1.2,3", 6, 3, false, false, TestName = "More than one separator")]
 		[TestCase("13,10", 4, 2, false, true, TestName = "Scale equals fractional part")]
-		[TestCase("-5,1", 4, 2, false, true, TestName = "Scale is bigger than fractional part")]
+		[TestCase("-5,1", 20, 10, false, true, TestName = "Scale is bigger than fractional part")]
 		[TestCase("+12", 4, 2, false, true, TestName = "Positive integer")]
 		[TestCase("20,0", 4, 2, false, true, TestName = "Positive double")]
 		[TestCase("-454", 4, 2, false, true, TestName = "Negative integer")]
@@ -57,7 +58,7 @@ namespace HomeExercises
 		[TestCase("2,3", 4, 2, false, true, TestName = "Decimal comma")]
 		[TestCase("3.2", 4, 2, false, true, TestName = "Decimal point")]
 		[TestCase("1337", 4, 2, false, true, TestName = "Number equals precision length")]
-		[TestCase("2,3", 4, 2, false, true, TestName = "Number length is smaller then precision")]
+		[TestCase("4,990", 50, 6, false, true, TestName = "Number length is smaller then precision")]
 		[TestCase("1941.4", 1000000, 2, false, true, TestName = "Big precision")]
 		[TestCase("-191939.291945", 1000000, 10000, false, true, TestName = "Big scale")]
 		[TestCase("0", 4, 2, false, true, TestName = "Zero integer")]
