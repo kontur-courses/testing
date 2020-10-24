e400955ace07382dfb547291c76db417ae5026a0
Ilya_Shumilin <i.sh-vk@yandex.ru>
2020-10-25 00:20:44 +0500

    рефакторинг решения

--- cs/HomeExercises/NumberValidatorTests.cs
+++ cs/HomeExercises/NumberValidatorTests.cs
@@ -8,60 +8,64 @@ namespace HomeExercises
     public class NumberValidatorTests
     {
         [Test]
-        [TestCase(-1, 0, TestName = "ConstructorThrowArgumentException_WhenNonPositivePrecision")]
-        [TestCase(3, -2, TestName = "ConstructorThrowArgumentException_WhenNegativeScale")]
-        [TestCase(1, 3, TestName = "ConstructorThrowArgumentException_WhenScaleOverPrecision")]
+        [TestCase(-1, 0, TestName = "WhenNonPositivePrecision")]
+        [TestCase(3, -2, TestName = "WhenNegativeScale")]
+        [TestCase(1, 3, TestName = "WhenScaleOverPrecision")]
         public void NumberValidatorThrowExceptionOnWrongParameters(int precision, int scale)
         {
             ((Action)(() => new NumberValidator(precision, scale))).Should().Throw<ArgumentException>();
         }
 
         [Test]
-        [TestCase(2, 1, TestName = "ConstructorNotThrowArgumentException_WhenPrecisionPositive")]
-        [TestCase(2, 0, TestName = "ConstructorNotThrowArgumentException_WhenScaleZero")]
+        [TestCase(2, 1, TestName = "WhenPrecisionPositive")]
+        [TestCase(2, 0, TestName = "WhenScaleZero")]
         public void NumberValidatorNotThrowExceptionOnCorrectParameters(int precision, int scale)
         {
             ((Action)(() => new NumberValidator(precision, scale))).Should().NotThrow();
         }
 
         [Test]
-        [TestCase(17, 2, "0.0", TestName = "NumberIsValid_WhenValueCorrect")]
-        [TestCase(17, 0, "2", TestName = "NumberIsValid_WhenValueInt")]
-        [TestCase(17, 2, "+2.3", TestName = "NumberIsValid_WhenValuePositive")]
-        [TestCase(17, 2, "-2.3", TestName = "NumberIsValid_WhenValueNegative")]
-        [TestCase(17, 2, "2,3", TestName = "NumberIsValid_WhenCommaIsSeparatorInValue")]
-        [TestCase(4, 2, "12,12", TestName = "NumberIsValid_SeparatorNotAffectOnCountNumberInPrecision")]
-        public void ValidNumberOnCorrectValue(int precision, int scale, string value, bool onlyPositive = false)
+        [TestCase(17, 2, "0.0", true, TestName = "WhenValueCorrect")]
+        [TestCase(17, 0, "2", true, TestName = "WhenValueInt")]
+        [TestCase(17, 2, "+2.3", true, TestName = "WhenValuePositive")]
+        [TestCase(17, 2, "-2.3", true, TestName = "WhenValueNegative")]
+        [TestCase(17, 2, "2,3", true, TestName = "WhenCommaIsSeparatorInValue")]
+        [TestCase(4, 2, "12,12", true, TestName = "SeparatorNotAffectOnCountNumberInPrecision")]
+        [TestCase(3, 2, "12,12", false, TestName = "WhenNumbersInValueOverPrecision")]
+        [TestCase(5, 1, "12,123", false, TestName = "WhenFractionalPartInValueOverScale")]
+        [TestCase(4, 2, "-123,1", false, TestName = "SignAffectOnCountNumberInPrecision")]
+        [TestCase(4, 2, "asfa", false, TestName = "WhenValueWrong")]
+        [TestCase(4, 2, "12,", false, TestName = "WhenValueNotContainFractionalPart")]
+        [TestCase(4, 2, ",12", false, TestName = "WhenValueNotContainIntegerPart")]
+        [TestCase(4, 2, " ", false, TestName = "WhenValueEmpty")]
+        [TestCase(4, 2, null, false, TestName = "WhenValueNull")]
+        [TestCase(4, 2, " 12.1", false, TestName = "WhenValueNotAtBeginningOfLine")]
+        [TestCase(4, 2, "12.1 ", false, TestName = "WhenValueNotAtEndOfLine")]
+        public void ValidNumberOnValues(int precision, int scale, string value, bool expected)
         {
-            var validator = new NumberValidator(precision, scale, onlyPositive);
-            validator.IsValidNumber(value).Should().BeTrue();
+            var validator = new NumberValidator(precision, scale);
+            validator.IsValidNumber(value).Should().Be(expected);
         }
 
         [Test]
-        [TestCase(3, 2, "12,12", TestName = "NumberNotValid_WhenNumbersInValueOverPrecision")]
-        [TestCase(5, 1, "12,123", TestName = "NumberNotValid_WhenFractionalPartInValueOverScale")]
-        [TestCase(4, 2, "-123,1", TestName = "NumberNotValid_SignAffectOnCountNumberInPrecision")]
-        [TestCase(4, 2, "-1,1", true, TestName = "NumberNotValid_WhenValueNegativeAndOnlyPositiveIsTrue")]
-        [TestCase(4, 2, "asfa", TestName = "NumberNotValid_WhenValueWrong")]
-        [TestCase(4, 2, "12,", TestName = "NumberNotValid_WhenValueNotContainFractionalPart")]
-        [TestCase(4, 2, ",12", TestName = "NumberNotValid_WhenValueNotContainIntegerPart")]
-        [TestCase(4, 2, " ", TestName = "NumberNotValid_WhenValueEmpty")]
-        [TestCase(4, 2, null, TestName = "NumberNotValid_WhenValueNull")]
-        [TestCase(4, 2, " 12.1", TestName = "NumberNotValid_WhenValueNotAtBeginningOfLine")]
-        [TestCase(4, 2, "12.1 ", TestName = "NumberNotValid_WhenValueNotAtEndOfLine")]
-        public void ValidNumberOnWrongValue(int precision, int scale, string value, bool onlyPositive = false)
+        public void ValidNumberOnValuesWhenValueNegativeAndOnlyPositiveIsTrue()
         {
-            var validator = new NumberValidator(precision, scale, onlyPositive);
-            validator.IsValidNumber(value).Should().BeFalse();
+            var validator = new NumberValidator(4, 2, true);
+            validator.IsValidNumber("-1,1").Should().BeFalse();
         }
     }
 
     public class NumberValidator
     {
-        private readonly Regex numberRegex;
+        private readonly static Regex numberRegex;
         private readonly bool onlyPositive;
         private readonly int precision;
         private readonly int scale;
+
+        static NumberValidator()
+        {
+            numberRegex = new Regex(@"^([+-]?)(\d+)([.,](\d+))?$", RegexOptions.IgnoreCase);
+        }
 
         public NumberValidator(int precision, int scale = 0, bool onlyPositive = false)
         {
@@ -72,7 +76,6 @@ namespace HomeExercises
                 throw new ArgumentException("precision must be a positive number");
             if (scale < 0 || scale >= precision)
                 throw new ArgumentException("precision must be a non-negative number less or equal than precision");
-            numberRegex = new Regex(@"^([+-]?)(\d+)([.,](\d+))?$", RegexOptions.IgnoreCase);
         }
 
         public bool IsValidNumber(string value)
