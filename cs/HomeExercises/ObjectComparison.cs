e812b90018d61728c2c9eaedc367db1552dd7c66
Kisus228 <nik.babushkin.201408@gmail.com>
2020-10-24 12:15:15 +0500

    Fixed object "Tsar" comparison

--- cs/HomeExercises/ObjectComparison.cs
+++ cs/HomeExercises/ObjectComparison.cs
@@ -1,100 +1,89 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
 using FluentAssertions;
 using NUnit.Framework;
 
 namespace HomeExercises
 {
-	public class ObjectComparison
-	{
-		[Test]
-		[Description("Проверка текущего царя")]
-		[Category("ToRefactor")]
-		public void CheckCurrentTsar()
-		{
-			var actualTsar = TsarRegistry.GetCurrentTsar();
+    public class ObjectComparison
+    {
+        [Test]
+        [Description("Проверка текущего царя")]
+        [Category("ToRefactor")]
+        public void CheckCurrentTsar()
+        {
+            var actualTsar = TsarRegistry.GetCurrentTsar();
 
-			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
-				new Person("Vasili III of Russia", 28, 170, 60, null));
+            var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
+                new Person("Vasili III of Russia", 28, 170, 60, null));
 
-			// Перепишите код на использование Fluent Assertions.
-			
-			actualTsar.Name.Should().Be(expectedTsar.Name);
-			actualTsar.Age.Should().Be(expectedTsar.Age);
-			actualTsar.Height.Should().Be(expectedTsar.Height);
-			actualTsar.Weight.Should().Be(expectedTsar.Weight);
-			
-			expectedTsar.Parent!.Name.Should().Be(actualTsar.Parent!.Name);
-			expectedTsar.Parent.Age.Should().Be(actualTsar.Parent.Age);
-			expectedTsar.Parent.Height.Should().Be(actualTsar.Parent.Height);
-			expectedTsar.Parent.Parent.Should().Be(expectedTsar.Parent.Parent);
-		}
+            // Перепишите код на использование Fluent Assertions.
 
-		[Test]
-		[Description("Альтернативное решение. Какие у него недостатки?")]
-		public void CheckCurrentTsar_WithCustomEquality()
-		{
-			var actualTsar = TsarRegistry.GetCurrentTsar();
-			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
-				new Person("Vasili III of Russia", 28, 170, 60, null));
+            actualTsar.Name.Should().Be(expectedTsar.Name);
+            actualTsar.Age.Should().Be(expectedTsar.Age);
+            actualTsar.Height.Should().Be(expectedTsar.Height);
+            actualTsar.Weight.Should().Be(expectedTsar.Weight);
 
-			// Какие недостатки у такого подхода? 
-			// Недостаток такой, что при добавлении нового поля в классе Person, придётся добавлять корректировать метод AreEquals. 
-			AreEqual(actualTsar, expectedTsar).Should().BeTrue();
-		}
-		
-		// Моё решение отличается от предыдущего тем, что в нём использована рефлексия, которая позволяет сравнивать поля классов.
-		// То есть даже при добавлении нового поля, тест переписывать не придётся
-		private bool AreEqual(Person? actual, Person? expected)
-		{
-			if (actual == expected) return true;
-			if (actual == null || expected == null) return false;
-			var fields = typeof(Person).GetFields();
-			foreach (var fieldInfo in fields.Where((field => field.Name != "Id")))
-			{
-				if (fieldInfo.FieldType == typeof(Person))
-				{
-					if (!AreEqual((Person?)fieldInfo.GetValue(actual), (Person?)fieldInfo.GetValue(expected)))
-						return false;
-				}
-				else
-				{
-					if (!fieldInfo.GetValue(actual)!.Equals(fieldInfo.GetValue(expected)))
-					{
-						return false;
-					}
-				}
-			}
+            expectedTsar.Parent!.Name.Should().Be(actualTsar.Parent!.Name);
+            expectedTsar.Parent.Age.Should().Be(actualTsar.Parent.Age);
+            expectedTsar.Parent.Height.Should().Be(actualTsar.Parent.Height);
+            expectedTsar.Parent.Parent.Should().Be(expectedTsar.Parent.Parent);
+        }
 
-			return true;
-		}
-	}
+        [Test]
+        [Description("Альтернативное решение. Какие у него недостатки?")]
+        public void CheckCurrentTsar_WithCustomEquality()
+        {
+            var actualTsar = TsarRegistry.GetCurrentTsar();
+            var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
+                new Person("Vasili III of Russia", 28, 170, 60, null));
 
-	public class TsarRegistry
-	{
-		public static Person GetCurrentTsar()
-		{
-			return new Person(
-				"Ivan IV The Terrible", 54, 170, 70,
-				new Person("Vasili III of Russia", 28, 170, 60, null));
-		}
-	}
+            // Какие недостатки у такого подхода? 
+            // Недостаток такой, что при добавлении нового поля в классе Person, придётся добавлять корректировать метод AreEquals. 
 
-	public class Person
-	{
-		public static int IdCounter = 0;
-		public int Age, Height, Weight;
-		public string Name;
-		public Person? Parent;
-		public int Id;
+            // Моё решение отличается от предыдущего тем, что в нём использована рефлексия, которая позволяет сравнивать поля классов.
+            // То есть даже при добавлении нового поля, тест переписывать не придётся
+            // К тому же до проверки равности полей мы проверяем находятся ли цари и их родители в рекурсивном отношении.
+            var tsarIdSet = new HashSet<int>(actualTsar.Id);
+            var firstTsar = TsarRegistry.GetCurrentTsar();
+            while (firstTsar.Parent != null)
+            {
+                Assert.DoesNotThrow(() => tsarIdSet.Add(firstTsar.Parent.Id));
+                firstTsar = firstTsar.Parent;
+            }
 
-		public Person(string name, int age, int height, int weight, Person? parent)
-		{
-			Id = IdCounter++;
-			Name = name;
-			Age = age;
-			Height = height;
-			Weight = weight;
-			Parent = parent;
-		}
-	}
+            actualTsar.Should().BeEquivalentTo(expectedTsar,
+                option => option.Excluding(person => person.Id)
+                    .Excluding(info =>
+                        info.SelectedMemberPath.EndsWith($"{nameof(Person.Parent)}.{nameof(Person.Id)}")));
+        }
+
+        public class TsarRegistry
+        {
+            public static Person GetCurrentTsar()
+            {
+                return new Person(
+                    "Ivan IV The Terrible", 54, 170, 70,
+                    new Person("Vasili III of Russia", 28, 170, 60, null));
+            }
+        }
+
+        public class Person
+        {
+            public static int IdCounter;
+            public int Age, Height, Weight;
+            public int Id;
+            public string Name;
+            public Person? Parent;
+
+            public Person(string name, int age, int height, int weight, Person? parent)
+            {
+                Id = IdCounter++;
+                Name = name;
+                Age = age;
+                Height = height;
+                Weight = weight;
+                Parent = parent;
+            }
+        }
+    }
 }
