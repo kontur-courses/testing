6150cffc370264b9ee9e09188c842c12da83e1ed
Denis Bautin <denis020301@mail.ru>
2020-10-24 16:44:43 +0500

    Работа на занятии
    Изменены WordsStatistics_Tests.cs, YourName.cs

--- cs/Challenge/WordsStatistics_Tests.cs
+++ cs/Challenge/WordsStatistics_Tests.cs
@@ -1,4 +1,8 @@
-﻿using FluentAssertions;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace Challenge
@@ -18,6 +22,22 @@ namespace Challenge
 		public void SetUp()
 		{
 			wordsStatistics = CreateStatistics();
+		}
+
+		public void AddWordsToWordsStatistics(params string[] words)
+		{
+			foreach (var word in words)
+			{
+				wordsStatistics.AddWord(word);
+			}
+		}
+
+		public string[] GetWordsFromStatistics()
+		{
+			return wordsStatistics
+				.GetStatistics()
+				.Select(wordCount => wordCount.Word)
+				.ToArray();
 		}
 
 		[Test]
@@ -40,8 +60,96 @@ namespace Challenge
 			wordsStatistics.AddWord("def");
 			wordsStatistics.GetStatistics().Should().HaveCount(2);
 		}
+		
+		[Test]
+		public void GetStatistics_OrderByWordCountDescending()
+		{
+			var wordsToAdd = new[] {"bb", "aa", "bb", "bb"};
+			
+			AddWordsToWordsStatistics(wordsToAdd);
+			var words = GetWordsFromStatistics();
+			
+			Assert.That(words, Is.EqualTo(new[] {"bb", "aa"}));
+		}
 
+		[Test]
+		public void GetStatistics_SeveralCall_GiveSameResult()
+		{
+			wordsStatistics.AddWord("ab");
+			var first = wordsStatistics.GetStatistics();
+			var second = wordsStatistics.GetStatistics();
+			
+			Assert.That(first, Is.EqualTo(second));
+		}
 
+		[Test]
+		public void GetStatistics_IfCountEqual_OrderByLexicographic()
+		{
+			var wordsToAdd = new[] {"bb", "aa", "bb", "aa"};
+
+			AddWordsToWordsStatistics(wordsToAdd);
+			var words = GetWordsFromStatistics();
+			
+			Assert.That(words, Is.EqualTo(new[] {"aa", "bb"}));
+		}
+
+		[Test]
+		public void AddWord_IfGotNull_ThrowArgumentNullException()
+		{
+			Assert.That(
+				() => { wordsStatistics.AddWord(null); },
+				Throws.ArgumentNullException);
+		}
+
+		[TestCase("", TestName = "Got empty string")]
+		[TestCase(" ", TestName = "Got WhiteSpace strings")]
+		public void AddWord_IfGotEmptyOrWhiteSpaceString_DoesntAddIt(string wordToAdd)
+		{
+			wordsStatistics.AddWord(wordToAdd);
+			var words = GetWordsFromStatistics();
+			
+			Assert.That(words, Is.Empty);
+		}
+
+		[Test]
+		public void AddWord_IfWordLengthGreaterThan10_AddOnlyFirst10Chars()
+		{
+			wordsStatistics.AddWord("abcdefghijk");
+			var words = GetWordsFromStatistics();
+
+			Assert.That(words, Is.EqualTo(new[] { "abcdefghij" }));
+		}
+
+		[Test]
+		public void AddWord_IfWordLengthLessOrEqual10_AddAllWord()
+		{
+			wordsStatistics.AddWord("abc");
+			var words = GetWordsFromStatistics();
+
+			Assert.That(words, Is.EqualTo(new[] {"abc"}));
+		}
+		
+		[Test]
+		public void AddWord_IgnoreCaseOfWord()
+		{
+			var wordsToAdd = new[] {"abc", "ABC"};
+			
+			AddWordsToWordsStatistics(wordsToAdd);
+			var words = GetWordsFromStatistics();
+			
+			Assert.That(words, Is.EqualTo(new[] {"abc"}));
+		}
+		
+		[Test]
+		public void AddWord_CorrectCount()
+		{
+			var wordsToAdd = new[] {"ab", "ab"};
+			
+			AddWordsToWordsStatistics(wordsToAdd);
+			var wordCount = wordsStatistics.GetStatistics().First();
+			
+			Assert.That(wordCount, Is.EqualTo(new WordCount("ab", 2)));
+		}
 		// Документация по FluentAssertions с примерами : https://github.com/fluentassertions/fluentassertions/wiki
 	}
 }
