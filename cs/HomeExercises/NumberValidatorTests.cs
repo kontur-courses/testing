ae3c02794f680f083cdfc8c41be65b3d1e1be92f
catsAndSuicide <anisa.hamidullina00@gmail.com>
2020-10-24 21:41:14 +0500

    1) NumberValidator: исправила ошибку в сообщении исключения.
    2) NumberValidatorTests: удалила старый тест Test() и избыточную проверку Constructor_DoesNotThrowException_OnCorrectArguments.

--- cs/HomeExercises/NumberValidatorTests.cs
+++ cs/HomeExercises/NumberValidatorTests.cs
@@ -6,28 +6,6 @@ namespace HomeExercises
 {
 	public class NumberValidatorTests
 	{
-		[Test]
-		public void Test()
-		{
-            Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
-            Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
-            Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, false));
-            Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
-
-            Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-            Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0"));
-            Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("00.00"));
-            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-0.00"));
-            Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+0.00"));
-            Assert.IsTrue(new NumberValidator(4, 2, true).IsValidNumber("+1.23"));
-            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+1.23"));
-            Assert.IsFalse(new NumberValidator(17, 2, true).IsValidNumber("0.000"));
-            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
-            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
-        }
-
 		[TestCase(0, 1, Description = "Zero precision")]
 		[TestCase(-1, 1, Description = "Negative precision")]
 		[TestCase(2, -1, Description = "Negative scale")]
@@ -38,16 +16,6 @@ namespace HomeExercises
 			Action action = () => new NumberValidator(precision, scale);
 			action.Should().Throw<ArgumentException>();
 		}
-
-
-		[TestCase(1, 0)]
-		[TestCase(7, 5)]
-		public void Constructor_DoesNotThrowException_OnCorrectArguments(int precision, int scale)
-		{
-			Action action = () => new NumberValidator(precision, scale);
-			action.Should().NotThrow();
-		}
-
 
 		[TestCase("")]
 		[TestCase("a.sd")]
