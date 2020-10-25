4781f4a2fa45d1a0a5b1b9a2a1c1946369093a31
BandLComplete <boroviki2000@mail.ru>
2020-10-25 13:34:12 +0500

    Рефакторинг тестов для NumberValidator

--- cs/HomeExercises/NumberValidatorTests.cs
+++ cs/HomeExercises/NumberValidatorTests.cs
@@ -7,62 +7,60 @@ namespace HomeExercises
 {
 	public class NumberValidatorTests
 	{
-		[TestCase(false, 2, 1, TestName = "Precision Greater Than Scale")]
-		[TestCase(false, 2, 0, TestName = "Scale Equals Zero")]
-		[TestCase(false, 2, 0, true, TestName = "Only Positive")]
-		[TestCase(true, 0, 1, TestName = "Precision Equals Zero")]
-		[TestCase(true, -1, 1, TestName = "Negative Precision")]
-		[TestCase(true, 2, -1, TestName = "Negative Scale")]
-		[TestCase(true, 2, 3, TestName = "Scale Greater Than Precision")]
-		[TestCase(true, 2, 2, TestName = "Scale Equals Precision")]
-		public void Creation(bool shouldThrow, int precision, int scale, bool onlyPositive = false)
+		[TestCase(2, 1, TestName = "PrecisionGreaterThanScale")]
+		[TestCase(2, 0, TestName = "NonNegativeScale")]
+		[TestCase(2, 1, true, TestName = "OnlyPositiveNumberValidator")]
+		public void Creation_DoesNotThrowException(int precision, int scale, bool onlyPositive = false)
 		{
-			Func<NumberValidator> creation = () => new NumberValidator(precision, scale, onlyPositive);
-			var message = $"precision={precision}, scale={scale}";
-
-			if (shouldThrow)
-				creation.Should().Throw<ArgumentException>(message);
-			else
-				creation.Should().NotThrow(message);
+			Assert.DoesNotThrow(() => new NumberValidator(precision, scale, onlyPositive));
 		}
 		
-		[TestCase(true, "123", 3, 0, TestName = "Integer Number Length Equals Precision")]
-		[TestCase(true, "123", 4, 0, TestName = "Integer Number Length Less Than Precision")]
-		[TestCase(true, "12.3", 3, 2, TestName = "Fractional Number Length Equals Precision")]
-		[TestCase(true, "12.3", 4, 2, TestName = "Fractional Number Length Less Than Precision")]
-		[TestCase(true, "-12", 3, 0, TestName = "Negative Number Length Equals Precision")]
-		[TestCase(true, "-12", 4, 0, TestName = "Negative Number Length Less Than Precision")]
-		[TestCase(true, "-12.3", 4, 2, TestName = "Fractional Negative Number Length Equals Precision")]
-		[TestCase(true, "-12.3", 5, 2, TestName = "Fractional Negative Number Length Less Than Precision")]
-		[TestCase(true, "+12", 3, 0, TestName = "Positive Number Length Equals Precision")]
-		[TestCase(true, "+12", 4, 0, TestName = "Positive Number Length Less Than Precision")]
-		[TestCase(true, "+12.3", 4, 2, TestName = "Fractional Positive Number Length Equals Precision")]
-		[TestCase(true, "+12.3", 5, 2, TestName = "Fractional Positive Number Length Less Than Precision")]
-		[TestCase(true, "1.23", 10, 2, TestName = "Fractional Part Length Equals Scale")]
-		[TestCase(true, "1.23", 10, 3, TestName = "Fractional Part Length Less Than Scale")]
-		[TestCase(true, "1", 2, 0, true, TestName = "Positive Number With Only Positive Validator")]
-		[TestCase(true, "1.2", 3, 2, true, TestName = "Positive Fractional Number With Only Positive Validator")]
+		[TestCase(0, 1, TestName = "PrecisionEqualsZero")]
+		[TestCase(-1, 1, TestName = "NegativePrecision")]
+		[TestCase(2, -1, TestName = "NegativeScale")]
+		[TestCase(2, 3, TestName = "ScaleGreaterThanPrecision")]
+		[TestCase(2, 2, TestName = "ScaleEqualsPrecision")]
+		public void Creation_ThrowsArgumentException(int precision, int scale, bool onlyPositive = false)
+		{
+			Assert.Throws<ArgumentException>(() => new NumberValidator(precision, scale, onlyPositive));
+		}
 		
-		[TestCase(false, null, 5, 4, TestName = "Null String")]
-		[TestCase(false, "", 5, 4, TestName = "Empty String")]
-		[TestCase(false, "a.12", 5, 4, TestName = "NaN String")]
-		[TestCase(false, "1.2.3", 5, 4, TestName = "String With Two Points")]
-		[TestCase(false, "123", 2, 0, TestName = "Integer Number Length Greater Than Precision")]
-		[TestCase(false, "12.3", 2, 1, TestName = "Fractional Number Length Greater Than Precision")]
-		[TestCase(false, "1.23", 3, 1, TestName = "Fractional Part Length Greater Than Scale")]
-		[TestCase(false, "-12", 2, 0, TestName = "Negative Number Length Greater Than Precision")]
-		[TestCase(false, "+12", 2, 0, TestName = "Positive Number Length Greater Than Precision")]
-		[TestCase(false, "-1", 3, 0, true, TestName = "Negative Number With Only Positive Validator")]
-		public void TestIsValidNumber(bool isValid, string number, int precision, int scale, bool onlyPositive = false)
+		[TestCase("123", 3, 0, TestName = "IntegerNumberLengthEqualsPrecision")]
+		[TestCase("123", 4, 0, TestName = "IntegerNumberLengthLessThanPrecision")]
+		[TestCase("12.3", 3, 2, TestName = "FractionalNumberLengthEqualsPrecision")]
+		[TestCase("12.3", 4, 2, TestName = "FractionalNumberLengthLessThanPrecision")]
+		[TestCase("-12", 3, 0, TestName = "NegativeNumberLengthEqualsPrecision")]
+		[TestCase("-12", 4, 0, TestName = "NegativeNumberLengthLessThanPrecision")]
+		[TestCase("-12.3", 4, 2, TestName = "FractionalNegativeNumberLengthEqualsPrecision")]
+		[TestCase("-12.3", 5, 2, TestName = "FractionalNegativeNumberLengthLessThanPrecision")]
+		[TestCase("+12", 3, 0, TestName = "PositiveNumberLengthEqualsPrecision")]
+		[TestCase("+12", 4, 0, TestName = "PositiveNumberLengthLessThanPrecision")]
+		[TestCase("+12.3", 4, 2, TestName = "FractionalPositiveNumberLengthEqualsPrecision")]
+		[TestCase("+12.3", 5, 2, TestName = "FractionalPositiveNumberLengthLessThanPrecision")]
+		[TestCase("1.23", 10, 2, TestName = "FractionalPartLengthEqualsScale")]
+		[TestCase("1.23", 10, 3, TestName = "FractionalPartLengthLessThanScale")]
+		[TestCase("1", 2, 0, true, TestName = "PositiveNumberWithOnlyPositiveValidator")]
+		[TestCase("1.2", 3, 2, true, TestName = "PositiveFractionalNumberWithOnlyPositiveValidator")]
+		public void IsValidNumber_ShouldBeTrue(string number, int precision, int scale, bool onlyPositive = false)
 		{
 			var validator = new NumberValidator(precision, scale, onlyPositive);
-			var result = validator.IsValidNumber(number);
-			var message = $"number={number}, precision={precision} scale={scale}";
-
-			if (isValid)
-				result.Should().BeTrue(message);
-			else
-				result.Should().BeFalse(message);
+			validator.IsValidNumber(number).Should().BeTrue();
+		}
+		
+		[TestCase(null, 5, 4, TestName = "NullString")]
+		[TestCase("", 5, 4, TestName = "EmptyString")]
+		[TestCase("a.12", 5, 4, TestName = "NaNString")]
+		[TestCase("1.2.3", 5, 4, TestName = "StringWithTwoPoints")]
+		[TestCase("123", 2, 0, TestName = "IntegerNumberLengthGreaterThanPrecision")]
+		[TestCase("12.3", 2, 1, TestName = "FractionalNumberLengthGreaterThanPrecision")]
+		[TestCase("1.23", 3, 1, TestName = "FractionalPartLengthGreaterThanScale")]
+		[TestCase("-12", 2, 0, TestName = "NegativeNumberLengthGreaterThanPrecision")]
+		[TestCase("+12", 2, 0, TestName = "PositiveNumberLengthGreaterThanPrecision")]
+		[TestCase("-1", 3, 0, true, TestName = "NegativeNumberWithOnlyPositiveValidator")]
+		public void IsValidNumber_ShouldBeFalse(string number, int precision, int scale, bool onlyPositive = false)
+		{
+			var validator = new NumberValidator(precision, scale, onlyPositive);
+			validator.IsValidNumber(number).Should().BeFalse();
 		}
 	}
 
