cd0412ef1f3008088a78f65d48ca2f3e4968d35e
xsitin <m9ckom@gmail.com>
2020-10-26 19:21:41 +0500

    variables renamed and type checking added

--- cs/HomeExercises/ObjectComparison.cs
+++ cs/HomeExercises/ObjectComparison.cs
@@ -3,78 +3,81 @@ using NUnit.Framework;
 
 namespace HomeExercises
 {
-	public class ObjectComparison
-	{
-		private static Person actualTsar;
-		private static Person expectedTsar;
+    public class ObjectComparison
+    {
+        private static Person actualTsar;
+        private static Person expectedTsar;
 
 
-		[SetUp]
-		public void SetUp()
-		{
-			actualTsar = TsarRegistry.GetCurrentTsar();
+        [SetUp]
+        public void SetUp()
+        {
+            actualTsar = TsarRegistry.GetCurrentTsar();
 
-			expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
-				new Person("Vasili III of Russia", 28, 170, 60, null));
-		}
+            expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
+                new Person("Vasili III of Russia", 28, 170, 60, null));
+        }
 
 
-		[Test]
-		[Description("Проверка текущего царя")]
-		[Category("ToRefactor")]
-		public void TsarRegistry_GetCurrentTsar_ShouldReturnIvanIV() =>
-			actualTsar.Should().BeEquivalentTo(expectedTsar,
-				options => options.Excluding(person => person.SelectedMemberInfo.Name == nameof(Person.Id)));
+        [Test]
+        [Description("Проверка текущего царя")]
+        [Category("ToRefactor")]
+        public void CheckCurrentTsar() =>
+            actualTsar.Should().BeEquivalentTo(expectedTsar,
+                options => options.Excluding(member =>
+                    member.SelectedMemberInfo.DeclaringType == typeof(Person) &&
+                    member.SelectedMemberInfo.Name == nameof(Person.Id)));
 
-		[Test]
-		[Description("Альтернативное решение. Какие у него недостатки?")]
-		public void CheckCurrentTsar_WithCustomEquality()
-		{
-			// Какие недостатки у такого подхода? 
-			Assert.True(AreEqual(actualTsar, expectedTsar));
-			//Cравнение использует функцию сравнения, которая нигде не проверяется и при этом её придется дописывать
-			//по мере дополнения класса Person. Из-за этого может быть допущена ошибка в тестах.
-		}
+        [Test]
+        [Description("Альтернативное решение. Какие у него недостатки?")]
+        public void CheckCurrentTsar_WithCustomEquality()
+        {
+            // Какие недостатки у такого подхода? 
+            Assert.True(AreEqual(actualTsar, expectedTsar));
+            //тест использует функцию сравнения, которая нигде не проверяется и при этом её придется дописывать
+            //по мере дополнения класса Person
+            //нет понятного сообщения об ошибке и проверки на цикличные ссылки parent
+        }
 
-		private bool AreEqual(Person? actual, Person? expected)
-		{
-			if (actual == expected) return true;
-			if (actual == null || expected == null) return false;
-			return
-				actual.Name == expected.Name
-				&& actual.Age == expected.Age
-				&& actual.Height == expected.Height
-				&& actual.Weight == expected.Weight
-				&& AreEqual(actual.Parent, expected.Parent);
-		}
-	}
+        private bool AreEqual(Person? actual, Person? expected)
+        {
+            if (actual == expected) return true;
+            if (actual == null || expected == null) return false;
+            return
+                actual.Name == expected.Name
+                && actual.Age == expected.Age
+                && actual.Height == expected.Height
+                && actual.Weight == expected.Weight
+                && AreEqual(actual.Parent, expected.Parent);
+        }
+    }
 
-	public class TsarRegistry
-	{
-		public static Person GetCurrentTsar()
-		{
-			return new Person(
-				"Ivan IV The Terrible", 54, 170, 70,
-				new Person("Vasili III of Russia", 28, 170, 60, null));
-		}
-	}
+    public class TsarRegistry
+    {
+        public static Person GetCurrentTsar()
+        {
+            return new Person(
+                "Ivan IV The Terrible", 54, 170, 70,
+                new Person("Vasili III of Russia", 28, 170, 60, null));
+        }
+    }
 
-	public class Person
-	{
-		public static int IdCounter = 0;
-		public int Age, Height, Weight;
-		public string Name;
-		public Person? Parent;
-		public int Id;
+    public class Person
+    {
+        public static int IdCounter = 0;
+        public int Age, Height, Weight;
+        public string Name;
+        public Person? Parent;
+        public int Id;
 
-		public Person(string name, int age, int height, int weight, Person? parent)
-		{
-			Id = IdCounter++;
-			Name = name;
-			Age = age;
-			Height = height;
-			Weight = weight;
-			Parent = parent;
-		}
-	}
+        public Person(string name, int age, int height, int weight, Person? parent)
+        {
+            Id = IdCounter++;
+            Name = name;
+            Age = age;
+            Height = height;
+            Weight = weight;
+            Parent = parent;
+        }
+    }
 }
