fa261f540282ffe1b4c7e6c32316c239ff96596d
MaxCross <dan25001@gmail.com>
2020-10-27 00:36:03 +0500

    Выполнил задание "Сравнение объектов"

--- cs/HomeExercises/ObjectComparison.cs
+++ cs/HomeExercises/ObjectComparison.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace HomeExercises
@@ -16,19 +17,45 @@ namespace HomeExercises
 				new Person("Vasili III of Russia", 28, 170, 60, null));
 
 			// Перепишите код на использование Fluent Assertions.
-			Assert.AreEqual(actualTsar.Name, expectedTsar.Name);
-			Assert.AreEqual(actualTsar.Age, expectedTsar.Age);
-			Assert.AreEqual(actualTsar.Height, expectedTsar.Height);
-			Assert.AreEqual(actualTsar.Weight, expectedTsar.Weight);
-
-			Assert.AreEqual(expectedTsar.Parent!.Name, actualTsar.Parent!.Name);
-			Assert.AreEqual(expectedTsar.Parent.Age, actualTsar.Parent.Age);
-			Assert.AreEqual(expectedTsar.Parent.Height, actualTsar.Parent.Height);
-			Assert.AreEqual(expectedTsar.Parent.Parent, actualTsar.Parent.Parent);
+			/*
+			 * Я очень надеюсь, что есть менее костыльный и более изящный метод,
+			 * который бы позволил исключить поле Id у объекта класса Person,
+			 * включая вложенные объекты (а не только Id и Parent.Id),
+			 * который бы так же не давал ложных срабатываний,
+			 * если поле какого-нибудь другого класса называется "Id" / заканчивается на "Id",
+			 * и который так же бы подхватывался при рефакторинге Rider'а
+			 * (если я, например, захочу переименовать Person в Persona, Id в PersonId)
+			 *
+			 * Я надеялся на что-то вроде .Excluding(o => o.Id).InclidingNestedMembers
+			 * или .ExcludingFromType<Person>(p => p.Id),
+			 * но, увы, я подобных не нашёл...
+			 */
+			expectedTsar.Should().BeEquivalentTo(actualTsar, options => options
+				.Excluding(
+					o => o.SelectedMemberInfo.DeclaringType == typeof(Person)
+					     && o.SelectedMemberInfo.Name == nameof(Person.Id)));
+			Console.WriteLine(nameof(Person.Id));
 		}
 
 		[Test]
 		[Description("Альтернативное решение. Какие у него недостатки?")]
+		/*
+		 * Как минимум - не используется fluentAssertion
+		 * 
+		 * Далее - учитывая, что тест должен проходить даже при несовпадающих id,
+		 * автору данного теста пришлось городить собственный метод, проверяющий равенство объектов.
+		 * Во первых, это просто дольше и сложнее писать писать:
+		 * приходится вручную определять, какие именно поля должны быть равны
+		 * и при каких дополнительных условиях (вроде если оба объекта null) они будут/не будут равны.
+		 * Во вторых, при добавлении новых полей в person придётся дополнять тест.
+		 * Да и просто - определять, какие поля ДОЛЖНЫ быть равны,
+		 * когда нам нужно лишь НЕ УЧИТЫВАТЬ какое-то поле - излишне. Не учитывать поле нам нужно чаще.
+		 *
+		 * Далее - Assert.True() просто выдаст, что в строчке с ней должен быть true, а не false,
+		 * в то время, как FluentAssertion подробно распишет,
+		 * какое именно поле отличается, как именно отличается,
+		 * какие правила для сравнения использовались и.т.п
+		 */
 		public void CheckCurrentTsar_WithCustomEquality()
 		{
 			var actualTsar = TsarRegistry.GetCurrentTsar();
