f3c5862fa3d700aff9b89480578a19c89a2993a1
CakeOrRiot <pozdnyakovmaks99@mail.ru>
2020-10-27 19:15:44 +0500

    Added integer part test

--- cs/HomeExercises/NumberValidatorTests.cs
+++ cs/HomeExercises/NumberValidatorTests.cs
@@ -30,6 +30,12 @@ namespace HomeExercises
             sutValidator.IsValidNumber(input).Should().BeFalse();
         }
 
+        [Test]
+        public void NumberValidator_ShouldNotValidateNumber_WithoutIntegerPart()
+        {
+            sutValidator.IsValidNumber(".12").Should().BeFalse();
+        }
+
         [TestCase("13,3", TestName = "Number with comma as a delimiter")]
         [TestCase("13.3", TestName = "Number with dot as a delimiter")]
         public void NumberValidator_ShouldValidateNumber_WithDifferentFractionalPartDelimiters(string input)
