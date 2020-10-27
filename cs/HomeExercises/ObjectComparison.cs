ee8d8e7740b9e5079baab7ac90de1b3d0ad96a31
CakeOrRiot <pozdnyakovmaks99@mail.ru>
2020-10-27 17:09:53 +0500

    Removed IncludingNestedObjects options

--- cs/HomeExercises/ObjectComparison.cs
+++ cs/HomeExercises/ObjectComparison.cs
@@ -17,11 +17,11 @@ namespace HomeExercises
 
             var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
                 new Person("Vasili III of Russia", 28, 170, 60, null));
-            
+
             actualTsar.Should()
                 .BeEquivalentTo(expectedTsar,
-                    options => options.IncludingNestedObjects()
-                        .Excluding(person => excludedMembers.Contains(person.SelectedMemberInfo.Name)));
+                    options => options.Excluding(
+                        person => excludedMembers.Contains(person.SelectedMemberInfo.Name)));
         }
 
         [Test]
