6b3df0469a253133457cca8581bac67daeeea9b6
yourname <yourname@site.com>
2020-10-27 11:53:08 +0500

    numberRegex

--- cs/HomeExercises/NumberValidator.cs
+++ cs/HomeExercises/NumberValidator.cs
@@ -31,7 +31,7 @@ namespace HomeExercises
 			if (string.IsNullOrEmpty(value))
 				return false;
 
-			var match = NumberRegex.Match(value);
+			var match = numberRegex.Match(value);
 			if (!match.Success)
 				return false;
 
