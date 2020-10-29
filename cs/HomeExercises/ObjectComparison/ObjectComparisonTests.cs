470bc79f0582042e256c3a1bd040b8e9cd9c4aa6
VladSavitskiy <cyberl1fe@yandex.ru>
2020-10-29 14:57:01 +0500

    Separated object classes with their tests

--- cs/HomeExercises/ObjectComparison/ObjectComparisonTests.cs
+++ cs/HomeExercises/ObjectComparison/ObjectComparisonTests.cs
@@ -2,9 +2,9 @@
 using FluentAssertions;
 using NUnit.Framework;
 
-namespace HomeExercises
+namespace HomeExercises.ObjectComparison
 {
-	public class ObjectComparison
+	public class ObjectComparisonTests
 	{
 		private readonly HashSet<string> excludingFieldNames = new HashSet<string> {nameof(Person.Id)};
 
@@ -56,35 +56,6 @@ namespace HomeExercises
 				&& actual.Height == expected.Height
 				&& actual.Weight == expected.Weight
 				&& AreEqual(actual.Parent, expected.Parent);
-		}
-	}
-
-	public class TsarRegistry
-	{
-		public static Person GetCurrentTsar()
-		{
-			return new Person(
-				"Ivan IV The Terrible", 54, 170, 70,
-				new Person("Vasili III of Russia", 28, 170, 60, null));
-		}
-	}
-
-	public class Person
-	{
-		public static int IdCounter = 0;
-		public int Age, Height, Weight;
-		public string Name;
-		public Person? Parent;
-		public int Id;
-
-		public Person(string name, int age, int height, int weight, Person? parent)
-		{
-			Id = IdCounter++;
-			Name = name;
-			Age = age;
-			Height = height;
-			Weight = weight;
-			Parent = parent;
 		}
 	}
 }
