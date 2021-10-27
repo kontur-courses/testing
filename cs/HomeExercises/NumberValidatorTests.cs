39503e2a1063236c38116b136a23e4d4b17cc8d2
Tripletri <Tripletri@yandex.ru>
2021-10-27 20:28:01 +0500

    Divided tests into groups. Added tests with big numbers.

--- cs/HomeExercises/NumberValidatorTests.cs
+++ cs/HomeExercises/NumberValidatorTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Text.RegularExpressions;
 using FluentAssertions;
 using NUnit.Framework;
@@ -47,28 +48,60 @@ namespace HomeExercises
 		[TestCase(".1")]
 		[TestCase("+")]
 		[TestCase("-")]
-		[TestCase("12", 1,TestName = "precision less than value length")]
+		public void IsValidNumber_False_WithIncorrectValue(string value)
+		{
+			new NumberValidator(int.MaxValue, int.MaxValue - 1).IsValidNumber(value).Should().BeFalse();
+		}
+
+		[TestCase("12", 1, TestName = "precision less than value length")]
 		[TestCase("1.2", 2, 0, TestName = "scale less than fraction length")]
-		[TestCase("1.2", 1, 0,TestName = "precision less than fraction+int")]
-		[TestCase("-1.2", 2, 1,TestName = "precision consider signs")]
-		[TestCase("-1", 1, 0, true)]
+		[TestCase("1.2", 1, 0, TestName = "precision less than fraction+int")]
+		[TestCase("-1.2", 2, 1, TestName = "precision consider signs")]
+		[TestCase("-1", 2, 0, true, TestName = "onlyPositive and negative int")]
+		[TestCase("-1.2", 3, 1, true, TestName = "onlyPositive and negative float")]
+		[TestCase("00001", 1, TestName = "unaccounted leading zeros in precision")]
+		[TestCase("1.100", 2, 1, TestName = "unaccounted following zeros")]
 		public void IsValidNumber_False_With(string value, int precision = 1, int scale = 0, bool onlyPositive = false)
 		{
 			var validator = new NumberValidator(precision, scale, onlyPositive);
 			validator.IsValidNumber(value).Should().BeFalse();
 		}
 
+		[TestCase("1.1", 2)]
+		[TestCase("1,1", 2)]
+		[TestCase("-1,1", 3)]
+		[TestCase("+1,1", 3)]
+		[TestCase("1.12345", 6, 5)]
+		public void IsValidNumber_True_WithFraction(string value, int precision = 1, int scale = 1)
+		{
+			var validator = new NumberValidator(precision, scale);
+			validator.IsValidNumber(value).Should().BeTrue();
+		}
+
 		[TestCase("1")]
-		[TestCase("1.1", 2, 1)]
-		[TestCase("1,1", 2, 1)]
-		[TestCase("-1,1", 3, 1)]
-		[TestCase("+1,1", 3, 1)]
 		[TestCase("-1", 2)]
 		[TestCase("+1", 2)]
-		[TestCase("+1", 2, 0, true)]
-		public void IsValidNumber_True_With(string value, int precision = 1, int scale = 0, bool onlyPositive = false)
+		[TestCase("+1", 2, true)]
+		public void IsValidNumber_True_WithInteger(string value, int precision = 1, bool onlyPositive = false)
 		{
-			var validator = new NumberValidator(precision, scale, onlyPositive);
+			var validator = new NumberValidator(precision, 0, onlyPositive);
+			validator.IsValidNumber(value).Should().BeTrue();
+		}
+
+		private static IEnumerable IsValidNumberTrueWithBigNumberCases
+		{
+			get
+			{
+				yield return new TestCaseData(new string('1', 1000)) {TestName = "Integer"};
+				yield return new TestCaseData(
+					$"{new string('1', 1000)}.{new string('2', 1000)}") {TestName = "Float"};
+			}
+		}
+
+		[TestCaseSource(nameof(IsValidNumberTrueWithBigNumberCases))]
+		public void IsValidNumber_True_WithBigNumber(string value)
+		{
+			var validator = new NumberValidator(int.MaxValue, int.MaxValue - 1);
 			validator.IsValidNumber(value).Should().BeTrue();
 		}
 	}
