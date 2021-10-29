061dc48487c1174fdb648db97acfe29915965659
Dmitry_Murzin <dmitrymurzin91@gmail.com>
2021-10-30 02:06:24 +0500

    homework

--- cs/HomeExercises/NumberValidatorTests.cs
+++ cs/HomeExercises/NumberValidatorTests.cs
@@ -5,28 +5,53 @@ using NUnit.Framework;
 
 namespace HomeExercises
 {
-	public class NumberValidatorTests
+	public class NumberValidator_Should
 	{
-		[Test]
-		public void Test()
+		[TestCase(-1, 0, true, TestName = "precision is less than zero")]
+		[TestCase(0, 0, true, TestName = "precision equals zero")]
+		[TestCase(1, -1, true, TestName = "scale is less than zero")]
+		[TestCase(1, 1, true, TestName = "scale equals precision")]
+		[TestCase(1, 2, true, TestName = "scale is greater than precision")]
+		public void ThrowArgumentExceptionWhen(int precision, int scale, bool onlyPositive)
 		{
-			Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
-			Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
-			Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, false));
-			Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
+			Action act = () => { new NumberValidator(0, 0, true); };
 
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0"));
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("00.00"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-0.00"));
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+0.00"));
-			Assert.IsTrue(new NumberValidator(4, 2, true).IsValidNumber("+1.23"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+1.23"));
-			Assert.IsFalse(new NumberValidator(17, 2, true).IsValidNumber("0.000"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
+			act.Should().Throw<ArgumentException>();
+		}
+
+		[TestCase(5, 4, true, "a.sd", TestName = "value contains letters")]
+		[TestCase(5, 4, true, "1$.0d", TestName = "value contains random symbols")]
+		[TestCase(5, 4, true, ".00", TestName = "integer is missed but separator is not")]
+		[TestCase(5, 4, true, "1.", TestName = "fraction is missed but separator is not")]
+		[TestCase(5, 4, true, "", TestName = "value is empty")]
+		[TestCase(5, 4, true, null, TestName = "value is null")]
+		[TestCase(5, 4, true, "11111.0000", TestName = "value length is bigger than precision")]
+		[TestCase(5, 2, true, "1.0000", TestName = "fraction length is bigger than scale")]
+		[TestCase(5, 4, true, "-1.00", TestName = "value should be positive as onlyPositive parameter is true")]
+		[TestCase(5, 0, true, "1.00", TestName = "fraction length should be equal zero")]
+		public void FailWhen(int precision, int scale, bool onlyPositive, string value)
+		{
+			var numberValidator = new NumberValidator(precision, scale, true);
+
+			var validationResult = numberValidator.IsValidNumber(value);
+
+			validationResult.Should().BeFalse();
+		}
+
+		[TestCase(3, 2, true, "1.0", TestName = "value length equals precision")]
+		[TestCase(3, 2, true, "1.00", TestName = "fractional part of value equal scale")]
+		[TestCase(15, 14, true, "3.14159265", TestName = "fractional part of value less than scale")]
+		[TestCase(17, 2, true, "111.00", TestName = "value length less than precision")]
+		[TestCase(17, 2, true, "111", TestName = "there is integer part of value only")]
+		[TestCase(3, 2, true, "+1.00", TestName = "precision doesn't consider the plus")]
+		[TestCase(4, 2, false, "-1.00", TestName = "value contains the minus")]
+		public void PassWhen(int precision, int scale, bool onlyPositive, string value)
+		{
+			var numberValidator = new NumberValidator(precision, scale, onlyPositive);
+
+			var validationResult = numberValidator.IsValidNumber(value);
+
+			validationResult.Should().BeTrue();
 		}
 	}
 
@@ -52,10 +77,14 @@ namespace HomeExercises
 		public bool IsValidNumber(string value)
 		{
 			// Проверяем соответствие входного значения формату N(m,k), в соответствии с правилом, 
-			// описанным в Формате описи документов, направляемых в налоговый орган в электронном виде по телекоммуникационным каналам связи:
-			// Формат числового значения указывается в виде N(m.к), где m – максимальное количество знаков в числе, включая знак (для отрицательного числа), 
-			// целую и дробную часть числа без разделяющей десятичной точки, k – максимальное число знаков дробной части числа. 
-			// Если число знаков дробной части числа равно 0 (т.е. число целое), то формат числового значения имеет вид N(m).
+			// описанным в Формате описи документов, направляемых в налоговый орган в электронном
+			// виде по телекоммуникационным каналам связи:
+			// Формат числового значения указывается в виде N(m.к),
+			// где m – максимальное количество знаков в числе, включая знак (для отрицательного числа), 
+			// целую и дробную часть числа без разделяющей десятичной точки,
+			// k – максимальное число знаков дробной части числа. 
+			// Если число знаков дробной части числа равно 0 (т.е. число целое),
+			// то формат числового значения имеет вид N(m).
 
 			if (string.IsNullOrEmpty(value))
 				return false;
--- cs/HomeExercises/ObjectComparison.cs
+++ cs/HomeExercises/ObjectComparison.cs
@@ -15,16 +15,13 @@ namespace HomeExercises
 			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
 				new Person("Vasili III of Russia", 28, 170, 60, null));
 
-			// Перепишите код на использование Fluent Assertions.
-			Assert.AreEqual(actualTsar.Name, expectedTsar.Name);
-			Assert.AreEqual(actualTsar.Age, expectedTsar.Age);
-			Assert.AreEqual(actualTsar.Height, expectedTsar.Height);
-			Assert.AreEqual(actualTsar.Weight, expectedTsar.Weight);
-
-			Assert.AreEqual(expectedTsar.Parent!.Name, actualTsar.Parent!.Name);
-			Assert.AreEqual(expectedTsar.Parent.Age, actualTsar.Parent.Age);
-			Assert.AreEqual(expectedTsar.Parent.Height, actualTsar.Parent.Height);
-			Assert.AreEqual(expectedTsar.Parent.Parent, actualTsar.Parent.Parent);
+			actualTsar.Should().BeEquivalentTo(expectedTsar, options =>
+				options.Excluding(tsar => tsar.SelectedMemberPath.EndsWith("Id")));
+			// Плюсы такого подхода:
+			// 1) можно добавлять поля c минимальными изменениями в методе BeEquivalentTo()
+			// 2) уменьшается количество строк кода и, соответственно, увеличивается читаемость
+			// 3) методы FluenAssertion делают код намного гибче - здесь, например,
+			// мы рекурсивно исключили поле Id из сравнения.
 		}
 
 		[Test]
@@ -35,7 +32,14 @@ namespace HomeExercises
 			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
 				new Person("Vasili III of Russia", 28, 170, 60, null));
 
-			// Какие недостатки у такого подхода? 
+            // Какие недостатки у такого подхода?
+
+            // Можно сделать, например, так:
+			// actualTsar.Parent.Parent = actualTsar;
+			// expectedTsar.Parent.Parent = expectedTsar;
+			// и тест уходит в бесконечную рекурсию.
+			// А моё решение быстро замечает, что есть зацикленность и падает
+			// с соответствующим сообщением
 			Assert.True(AreEqual(actualTsar, expectedTsar));
 		}
 
