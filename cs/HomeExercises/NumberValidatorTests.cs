9d94d146819396ba00fc3a5b17e6efa65700f90b
Aripov Sergey <aripovsergey3@gmail.com>
2021-10-29 09:37:52 +0500

    Исправления NumberValidatorTests

--- cs/HomeExercises/NumberValidatorTests.cs
+++ cs/HomeExercises/NumberValidatorTests.cs
@@ -5,12 +5,12 @@ using System.Text.RegularExpressions;
 
 namespace HomeExercises
 {
-    public class NumberValidatorTests
+    [TestFixture]
+    public class ValidatorShould
     {
         [Test]
-        public void Test()
+        public void DoEverything()
         {
-
             Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
             Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
             Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, false));//повтор
@@ -31,129 +31,95 @@ namespace HomeExercises
             //не хватает тестов на некорректный формат, на scale, на пустую (или null) строку
         }
 
-        private NumberValidator _smallValidator = null!;
-        private NumberValidator _bigValidator = null!;
-        private NumberValidator _negativeValidator = null!;
-
-        [SetUp]
-        public void Setup()
+        [TestCase(-1, 0, "negative precision (\"{0}\") was provided")]
+        [TestCase(3, -1, "negative scale (\"{1}\") was provided")]
+        [TestCase(1, 2, "scale (\"{1}\") was bigger than precision (\"{0}\")")]
+        public void Throw_WhenConstructorArgsAreIncorrect(int precision, int scale, string because)
         {
-            _smallValidator = new NumberValidator(6, 2, true);
-            _bigValidator = new NumberValidator(17, 8, true);
-            _negativeValidator = new NumberValidator(17, 8, false);
+            Action action = () => new NumberValidator(precision, scale);
+
+            action.Should().Throw<ArgumentException>(because, precision, scale);
         }
 
-        [Test]
-        public void Validator_Throws_WhenNonPositivePrecision()
+        [TestCase(2)]
+        [TestCase(2, 1)]
+        [TestCase(2, 1, true)]
+        public void NotThrow_WhenConstructorArgsAreCorrect(int precision, int scale = 0, bool onlyPositive = false)
         {
-            Action action = () => new NumberValidator(-1);
-            action.Should().Throw<ArgumentException>();
-        }
+            Action action = () => new NumberValidator(precision, scale, onlyPositive);
 
-        [Test]
-        public void Validator_Throws_WhenNegativeScale()
-        {
-            Action action = () => new NumberValidator(3, -1);
-            action.Should().Throw<ArgumentException>();
-        }
-
-        [Test]
-        public void Validator_Throws_WhenScaleLargerThenPrecision()
-        {
-            Action action = () => new NumberValidator(3, 5);
-            action.Should().Throw<ArgumentException>();
-        }
-
-        [Test]
-        public void Validator_NotThrows_WhenCorrectArgs()
-        {
-            Action action = () => new NumberValidator(2);
             action.Should().NotThrow();
         }
 
-        [Test]
-        public void Validator_False_WhenNotNumber()
+        [TestCase("-1.23", true, "a negative number (\"{0}\") was provided when onlyPositive was (\"{3}\")")]
+        [TestCase("1.2300", false, "a number with big frac part (\"{0}\") was provided when scale was ({2})")]
+        [TestCase("+1222.45", false, "a number with big total length (\"{0}\") was provided when scale was ({2}) and precision was ({1})")]
+        public void ReturnFalse_WhenValueIsInvalidNumber(string value, bool onlyPositive, string because)
         {
-            var arg = "a.bc";
-            _smallValidator.IsValidNumber(arg).Should().BeFalse("a non-number (\"{0}\") was provided", arg);
+            var precision = 5;
+            var scale = 2;
+            var validator = new NumberValidator(precision, scale, onlyPositive);
+
+            var result = validator.IsValidNumber(value);
+
+            result.Should().BeFalse(because, value, precision, scale, onlyPositive);
         }
 
         [Test]
-        public void Validator_False_WhenOnlyPositiveWithNegativeNumber()
+        public void ReturnFalse_WhenValueIsNull()
         {
-            var arg = "-1.23";
-            _bigValidator.IsValidNumber(arg).Should().BeFalse("a negative number (\"{0}\") was provided", arg);
+            string? value = null;
+            var validator = new NumberValidator(1);
+
+            var result = validator.IsValidNumber(value!);
+
+            result.Should().BeFalse("null was provided");
         }
 
         [Test]
-        public void Validator_False_WhenFracPartToBig()
-        {
-            var arg = "1.2300";
-            _smallValidator.IsValidNumber(arg).Should().BeFalse("a number with big frac part (\"{0}\") was provided", arg);
-        }
-
-        [Test]
-        public void Validator_False_WhenTotalLengthToBig()
-        {
-            var arg = "+1222.45";
-            _smallValidator.IsValidNumber(arg).Should().BeFalse("a number with big total length (\"{0}\") was provided", arg);
-        }
-
-        [Test]
-        public void Validator_False_WhenNullOrEmpty()
+        public void ReturnFalse_WhenValueIsEmpty()
         {
             var arg = "";
-            _smallValidator.IsValidNumber(arg).Should().BeFalse("an empty number (\"{0}\") was provided", arg);
+            var validator = new NumberValidator(1);
 
-            arg = null;
-            _smallValidator.IsValidNumber(arg!).Should().BeFalse("null was provided");
+            var result = validator.IsValidNumber(arg);
+
+            result.Should().BeFalse("an empty number (\"{0}\") was provided", arg);
         }
 
-        [Test]
-        public void Validator_False_WhenWrongFormat()
+        [TestCase("a.bc", "a non-number (\"{0}\") was provided")]
+        [TestCase("+-0.0", "a number with several signs (\"{0}\") was provided")]
+        [TestCase("+0.0.0", "a number with several dots (\"{0}\") was provided")]
+        [TestCase("0+0.0", "a number with sing inside (\"{0}\") was provided")]
+        [TestCase(".0", "a number without starting digit (\"{0}\") was provided")]
+        [TestCase("0.", "a number without end digit (\"{0}\") was provided")]
+        [TestCase(".", "a number without digits (\"{0}\") was provided")]
+        [TestCase(" ", "a number with only white spaces (\"{0}\") was provided")]
+        public void ReturnFalse_WhenValueIsInWrongFormat(string value, string because)
         {
-            var arg = "+-0.0";
-            _smallValidator.IsValidNumber(arg).Should().BeFalse("a number with several signs (\"{0}\") was provided", arg);
+            var validator = new NumberValidator(10, 5);
 
-            arg = "+0.0.0";
-            _smallValidator.IsValidNumber(arg).Should().BeFalse("a number with several dots (\"{0}\") was provided", arg);
+            var result = validator.IsValidNumber(value);
 
-            arg = "0+0.0";
-            _smallValidator.IsValidNumber(arg).Should().BeFalse("a number with sing inside (\"{0}\") was provided", arg);
-
-            arg = ".0";
-            _smallValidator.IsValidNumber(arg).Should().BeFalse("a number without starting digit (\"{0}\") was provided", arg);
-
-            arg = "0.";
-            _smallValidator.IsValidNumber(arg).Should().BeFalse("a number without end digit (\"{0}\") was provided", arg);
-
-            arg = ".";
-            _smallValidator.IsValidNumber(arg).Should().BeFalse("a number without digits (\"{0}\") was provided", arg);
+            result.Should().BeFalse(because, value);
         }
 
-        [Test]
-        public void Validator_True_OnCorrectNumber()
+        [TestCase("0", 1, 0, true)]
+        [TestCase("-0", 2, 0, false)]
+        [TestCase("01", 2, 0, true)]
+        [TestCase("+1", 2, 0, true)]
+        [TestCase("12", 2, 0, true)]
+        [TestCase("12.3", 3, 1, true)]
+        [TestCase("0.12", 3, 2, true)]
+        [TestCase("12.34", 4, 2, true)]
+        [TestCase("-12.34", 5, 2, false)]
+        public void ReturnTrue_WhenValueIsCorrectNumber(string value, int precision, int scale, bool onlyPositive)
         {
-            var arg = "+1.23";
-            _smallValidator.IsValidNumber(arg).Should().BeTrue("a number with correct format (\"{0}\") was provided", arg);
+            var validator = new NumberValidator(precision, scale, onlyPositive);
 
-            arg = "2";
-            _smallValidator.IsValidNumber(arg).Should().BeTrue("a number with correct format (\"{0}\") was provided", arg);
+            var result = validator.IsValidNumber(value);
 
-            arg = "+0.12";
-            _smallValidator.IsValidNumber(arg).Should().BeTrue("a number with correct format (\"{0}\") was provided", arg);
-
-            arg = "0.12";
-            _smallValidator.IsValidNumber(arg).Should().BeTrue("a number with correct format (\"{0}\") was provided", arg);
-
-            arg = "12.34";
-            _smallValidator.IsValidNumber(arg).Should().BeTrue("a number with correct format (\"{0}\") was provided", arg);
-
-            arg = "+12.34";
-            _smallValidator.IsValidNumber(arg).Should().BeTrue("a number with correct format (\"{0}\") was provided", arg);
-
-            arg = "-0.123";
-            _negativeValidator.IsValidNumber(arg).Should().BeTrue("a number with correct format (\"{0}\") was provided", arg);
+            result.Should().BeTrue("a number with correct format (\"{0}\") for Validator with settings ({1}, {2}, {3}) was provided", value, precision, scale, onlyPositive);
         }
     }
 
@@ -170,14 +136,10 @@ namespace HomeExercises
             this.scale = scale;
             this.onlyPositive = onlyPositive;
             if (precision <= 0)
-            {
                 throw new ArgumentException("precision must be a positive number");
-            }
 
             if (scale < 0 || scale >= precision)
-            {
-                throw new ArgumentException("precision must be a non-negative number less or equal than precision");
-            }
+                throw new ArgumentException("scale must be a non-negative number less or equal than precision");
 
             numberRegex = new Regex(@"^([+-]?)(\d+)([.,](\d+))?$", RegexOptions.IgnoreCase);
         }
@@ -191,15 +153,11 @@ namespace HomeExercises
             // Если число знаков дробной части числа равно 0 (т.е. число целое), то формат числового значения имеет вид N(m).
 
             if (string.IsNullOrEmpty(value))
-            {
                 return false;
-            }
 
-            Match? match = numberRegex.Match(value);
+            var match = numberRegex.Match(value);
             if (!match.Success)
-            {
                 return false;
-            }
 
             // Знак и целая часть
             var intPart = match.Groups[1].Value.Length + match.Groups[2].Value.Length;
@@ -207,14 +165,10 @@ namespace HomeExercises
             var fracPart = match.Groups[4].Value.Length;
 
             if (intPart + fracPart > precision || fracPart > scale)
-            {
                 return false;
-            }
 
             if (onlyPositive && match.Groups[1].Value == "-")
-            {
                 return false;
-            }
 
             return true;
         }
