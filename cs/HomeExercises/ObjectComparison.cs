49d5f261df7227505344c54a1163a7aca9da7595
waverma <63812725+waverma@users.noreply.github.com>
2021-10-30 20:26:22 +0500

    solve part 1

--- cs/HomeExercises/ObjectComparison.cs
+++ cs/HomeExercises/ObjectComparison.cs
@@ -1,5 +1,12 @@
 ﻿using FluentAssertions;
+using FluentAssertions.Equivalency;
+// using FluentAssertions.Equivalency;
 using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System;
+using System.Reflection;
+using FluentAssertions;
+using FluentAssertions.Equivalency;
 
 namespace HomeExercises
 {
@@ -11,20 +18,31 @@ namespace HomeExercises
 		public void CheckCurrentTsar()
 		{
 			var actualTsar = TsarRegistry.GetCurrentTsar();
-
 			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
 				new Person("Vasili III of Russia", 28, 170, 60, null));
-
+			
+			
+			actualTsar.Should().BeEquivalentTo(
+				expectedTsar,
+				options => options
+					.IncludingNestedObjects()
+					.AllowingInfiniteRecursion()
+					.IncludingFields()
+					.Excluding(ctx => $".{ctx.SelectedMemberPath}".EndsWith(".Id")
+					)
+			);
+			
+			
 			// Перепишите код на использование Fluent Assertions.
-			Assert.AreEqual(actualTsar.Name, expectedTsar.Name);
-			Assert.AreEqual(actualTsar.Age, expectedTsar.Age);
-			Assert.AreEqual(actualTsar.Height, expectedTsar.Height);
-			Assert.AreEqual(actualTsar.Weight, expectedTsar.Weight);
-
-			Assert.AreEqual(expectedTsar.Parent!.Name, actualTsar.Parent!.Name);
-			Assert.AreEqual(expectedTsar.Parent.Age, actualTsar.Parent.Age);
-			Assert.AreEqual(expectedTsar.Parent.Height, actualTsar.Parent.Height);
-			Assert.AreEqual(expectedTsar.Parent.Parent, actualTsar.Parent.Parent);
+			// Assert.AreEqual(actualTsar.Name, expectedTsar.Name);
+			// Assert.AreEqual(actualTsar.Age, expectedTsar.Age);
+			// Assert.AreEqual(actualTsar.Height, expectedTsar.Height);
+			// Assert.AreEqual(actualTsar.Weight, expectedTsar.Weight);
+			//
+			// Assert.AreEqual(expectedTsar.Parent!.Name, actualTsar.Parent!.Name);
+			// Assert.AreEqual(expectedTsar.Parent.Age, actualTsar.Parent.Age);
+			// Assert.AreEqual(expectedTsar.Parent.Height, actualTsar.Parent.Height);
+			// Assert.AreEqual(expectedTsar.Parent.Parent, actualTsar.Parent.Parent);
 		}
 
 		[Test]
@@ -36,6 +54,28 @@ namespace HomeExercises
 				new Person("Vasili III of Russia", 28, 170, 60, null));
 
 			// Какие недостатки у такого подхода? 
+			// 
+			// 1)	Этот тест должен проверять метод GetCurrentTsar(),
+			//	т.е. тест проверяет то, что текущий царь - тот, которого мы ожидаем.
+			//	Но в этом решении есть написанный нами метод AreEqual(Person? actual, Person? expected).
+			//	И может показаться, что мы проверяем этот метод сравнения,
+			//	особенно, если его расположить в другом месте, например в Person.
+			//
+			// 2)	Метод AreEqual тоже требует отдельныйх тестов, т.к. он написан нами и содержит логику. 
+			//
+			// 3)	Но, так или иначе, если метод AreEqual написан с ошибкой,
+			//	то упадет и этот тест, даже если текущий царь - верный.
+			//
+			// 4)	При добавлении нового поля в Person нужно не забыть добавить его в метод AreEqual.
+			//
+			// 5)	Не предусмотренно обработка рекурсии. Во-первых, возможно исключение StackOverflowException,
+			//	во-вторых нет настройки для максимальной глубины рекурсии при сравнении. 
+			//
+			// 6)	Использование метода Assert.True влечет плохую читаемость кода,
+			//	а также абсолютно не информативное сообщение при провале теста
+			//
+			// 7)	Такое решение менее гибкое, чем использование BeEquivalentTo.
+			
 			Assert.True(AreEqual(actualTsar, expectedTsar));
 		}
 
