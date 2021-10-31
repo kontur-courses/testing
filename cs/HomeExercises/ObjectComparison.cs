d1f0c6ae061d6fa937d65ffbea91f1ebd00786a2
SlivnyiArtem <slivnay1965@gmail.com>
2021-10-31 20:16:22 +0500

    Use FluentAssertions for tests

--- cs/HomeExercises/ObjectComparison.cs
+++ cs/HomeExercises/ObjectComparison.cs
@@ -16,15 +16,14 @@ namespace HomeExercises
 				new Person("Vasili III of Russia", 28, 170, 60, null));
 
 			// Перепишите код на использование Fluent Assertions.
-			Assert.AreEqual(actualTsar.Name, expectedTsar.Name);
-			Assert.AreEqual(actualTsar.Age, expectedTsar.Age);
-			Assert.AreEqual(actualTsar.Height, expectedTsar.Height);
-			Assert.AreEqual(actualTsar.Weight, expectedTsar.Weight);
-
-			Assert.AreEqual(expectedTsar.Parent!.Name, actualTsar.Parent!.Name);
-			Assert.AreEqual(expectedTsar.Parent.Age, actualTsar.Parent.Age);
-			Assert.AreEqual(expectedTsar.Parent.Height, actualTsar.Parent.Height);
-			Assert.AreEqual(expectedTsar.Parent.Parent, actualTsar.Parent.Parent);
+			actualTsar.Name.Should().Be(expectedTsar.Name);
+			actualTsar.Age.Should().Be(expectedTsar.Age);
+			actualTsar.Height.Should().Be(expectedTsar.Height);
+			actualTsar.Weight.Should().Be(expectedTsar.Weight);
+			actualTsar.Parent!.Name.Should().Be(expectedTsar.Parent!.Name);
+			actualTsar.Parent.Age.Should().Be(expectedTsar.Parent.Age);
+			actualTsar.Parent.Height.Should().Be(expectedTsar.Parent.Height);
+			actualTsar.Parent.Parent.Should().Be(expectedTsar.Parent.Parent);
 		}
 
 		[Test]
