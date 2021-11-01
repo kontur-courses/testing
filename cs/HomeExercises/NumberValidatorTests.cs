fd67872a722e278018f74206c2afb746fad30740
Yurii <yuriigusev01@mail.ru>
2021-11-02 01:55:10 +0500

    Добавил новые тесты в NumberValidatorTests.cs, переписал ObjectComparison.cs с использованием FluentAssertions

--- cs/HomeExercises/NumberValidatorTests.cs
+++ cs/HomeExercises/NumberValidatorTests.cs
@@ -28,6 +28,131 @@ namespace HomeExercises
 			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
 			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
 		}
+
+		[TestCase(-1, 2, true, TestName = "Precision can't be negative")]
+		[TestCase(0, 1, true, TestName = "Precision can't be equal to zero")]
+		[TestCase(1, -1, true, TestName = "Scale can't be negative")]
+		[TestCase(5, 10, true, TestName = "Scale can't be more than precision")]
+		[TestCase(5, 5, true, TestName = "Scale can't be equal to precision")]
+		public void Constructor_ThrowsArgumentException_WhenInvalidParametersGiven(
+			int precision,
+			int scale,
+			bool positiveOnly)
+		{
+			Action action = () => new NumberValidator(precision, scale, positiveOnly);
+			action.Should().Throw<ArgumentException>();
+		}
+
+
+		[TestCase("", TestName = "Empty string is not a number")]
+		[TestCase(null, TestName = "Null is not a number")]
+		[TestCase("      ", TestName = "Multiplied white spaces are not a number")]
+		[TestCase("abc", TestName = "Letters are not a number")]
+		[TestCase("a1b2c3", TestName = "Group of letters and digits are not a number")]
+		[TestCase("-", TestName = "Symbol '-' is not a number")]
+		[TestCase("+", TestName = "Symbol '+' is not a number")]
+		[TestCase("+-12", TestName = "Minus are plus are not allowed together")]
+		[TestCase("12.3.4", TestName = "Two points in number are not allowed")]
+		[TestCase(".12", TestName = "Number can't start from point")]
+		[TestCase("12.", TestName = "Number can't end on point")]
+		[TestCase("12.,3", TestName = "Point and comma are not allowed together")]
+		public void IsValidNumber_ReturnFalse_WhenInputInInvalidFormat(string input)
+		{
+			var validator = new NumberValidator(10, 5, true);
+
+			validator.IsValidNumber(input).Should().BeFalse();
+		}
+
+		[TestCase(3, 0, true, "1234", TestName = "Number with integer part more than precision")]
+		[TestCase(3, 2, true, "12.34", TestName = "Number with integer plus fraction parts more than precision")]
+		[TestCase(5, 2, true, "0.123", TestName = "Number with fractional part more than scale")]
+		[TestCase(5, 0, true, "-123", TestName = "Negative number when only positive allowed")]
+		[TestCase(3, 0, true, "+123", TestName = "Number length with symbol '+' is more than precision")]
+		[TestCase(3, 0, false, "-123", TestName = "Number length with symbol '-' is more than precision")]
+		public void IsValidNumber_ReturnFalse_WhenNumberDoesNotMatchRequirements(
+			int precision,
+			int scale,
+			bool positiveOnly,
+			string input)
+		{
+			var validator = new NumberValidator(precision, scale, positiveOnly);
+
+			validator.IsValidNumber(input).Should().BeFalse();
+		}
+
+		[TestCase(5,
+			0,
+			true,
+			"123",
+			TestName =
+				"Number is integer and it has length less than precision")]
+		[TestCase(5,
+			0,
+			true,
+			"12345",
+			TestName =
+				"Number is integer and it has same length as precision")]
+		[TestCase(5,
+			2,
+			true,
+			"1.4",
+			TestName =
+				"Fractional part lenght is less than scale")]
+		[TestCase(5,
+			2,
+			true,
+			"1.45",
+			TestName =
+				"Fractional part lenght is same as scale")]
+		[TestCase(5,
+			2,
+			true,
+			"123.45",
+			TestName =
+				"Sum of Integer and Fractional parts lenght same as precision")]
+		[TestCase(5,
+			0,
+			false,
+			"-123",
+			TestName =
+				"Negative integer have size less than precision and negative numbers Allowed")]
+		[TestCase(5,
+			2,
+			false,
+			"+12.3",
+			TestName =
+				"Correct positive number when negative numbers allowed")]
+		public void IsValidNumber_ReturnTrue_WhenInputMatchesRules(
+			int precision,
+			int scale,
+			bool positiveOnly,
+			string input)
+		{
+			var validator = new NumberValidator(precision, scale, positiveOnly);
+
+			validator.IsValidNumber(input).Should().BeTrue();
+		}
+
+
+
+		//Нашел 2 сценария, на которых реализация возвращает неправильный ответ 
+		/*[Test]
+		public void IsValidNumber_ReturnTrue_WhenOnlyPositiveNumbersAllowedAndMinusZeroGiven()
+		{
+			var validator = new NumberValidator(10, 2, true);
+			var input = "-0";
+		
+			validator.IsValidNumber(input).Should().BeTrue();
+		}
+		
+		[Test]
+		public void IsValidNumber_ReturnFalse_WhenNonZeroNumberStartsFromZero()
+		{
+			var validator = new NumberValidator(10, 5, true);
+			var input = "0123";
+		
+			validator.IsValidNumber(input).Should().BeFalse();
+		}*/
 	}
 
 	public class NumberValidator
--- cs/HomeExercises/ObjectComparison.cs
+++ cs/HomeExercises/ObjectComparison.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Text.RegularExpressions;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace HomeExercises
@@ -12,7 +13,10 @@ namespace HomeExercises
 		{
 			var actualTsar = TsarRegistry.GetCurrentTsar();
 
-			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
+			var expectedTsar = new Person("Ivan IV The Terrible",
+				54,
+				170,
+				70,
 				new Person("Vasili III of Russia", 28, 170, 60, null));
 
 			// Перепишите код на использование Fluent Assertions.
@@ -25,6 +29,8 @@ namespace HomeExercises
 			Assert.AreEqual(expectedTsar.Parent.Age, actualTsar.Parent.Age);
 			Assert.AreEqual(expectedTsar.Parent.Height, actualTsar.Parent.Height);
 			Assert.AreEqual(expectedTsar.Parent.Parent, actualTsar.Parent.Parent);
+
+			//Переписал в RefactoredCheckCurrentTsar()
 		}
 
 		[Test]
@@ -32,10 +38,20 @@ namespace HomeExercises
 		public void CheckCurrentTsar_WithCustomEquality()
 		{
 			var actualTsar = TsarRegistry.GetCurrentTsar();
-			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
+			var expectedTsar = new Person("Ivan IV The Terrible",
+				54,
+				170,
+				70,
 				new Person("Vasili III of Russia", 28, 170, 60, null));
 
 			// Какие недостатки у такого подхода? 
+			
+			/* Недостатки
+			 * 1. Изменение класса Person влечет за собой изменение метода AreEqual
+			 * 2. Не очень читаемый код в AreEqual (я не сразу заметил, что там Id не сравниваются)
+			 *
+			 */
+			
 			Assert.True(AreEqual(actualTsar, expectedTsar));
 		}
 
@@ -44,11 +60,31 @@ namespace HomeExercises
 			if (actual == expected) return true;
 			if (actual == null || expected == null) return false;
 			return
-				actual.Name == expected.Name
-				&& actual.Age == expected.Age
-				&& actual.Height == expected.Height
-				&& actual.Weight == expected.Weight
-				&& AreEqual(actual.Parent, expected.Parent);
+				actual.Name == expected.Name &&
+				actual.Age == expected.Age &&
+				actual.Height == expected.Height &&
+				actual.Weight == expected.Weight &&
+				AreEqual(actual.Parent, expected.Parent);
+		}
+
+		[Test]
+		[Category("Refactored")]
+		public void RefactoredCheckCurrentTsar()
+		{
+			var actualTsar = TsarRegistry.GetCurrentTsar();
+			var expectedTsar = new Person("Ivan IV The Terrible",
+				54,
+				170,
+				70,
+				new Person("Vasili III of Russia", 28, 170, 60, null));
+			//Регулярка находит строки формата "Id", "Parent.Id", "Parent.Parent.Id" и тд
+			var idFieldRegex = new Regex("^((Parent\\.)*Id)$");
+
+			actualTsar
+				.Should()
+				.BeEquivalentTo(
+					expectedTsar,
+					config => config.Excluding(ctx => idFieldRegex.IsMatch(ctx.SelectedMemberPath)));
 		}
 	}
 
@@ -57,7 +93,10 @@ namespace HomeExercises
 		public static Person GetCurrentTsar()
 		{
 			return new Person(
-				"Ivan IV The Terrible", 54, 170, 70,
+				"Ivan IV The Terrible",
+				54,
+				170,
+				70,
 				new Person("Vasili III of Russia", 28, 170, 60, null));
 		}
 	}
