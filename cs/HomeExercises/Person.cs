0b76439c95aff5a90f02ac1f22cc74b90a8f3fa2
IvanSibirtsev <sibirtsev.ivan@gmail.com>
2021-11-03 16:35:06 +0500

    moved Person and TsarRegistry to their own files

---
+++ cs/HomeExercises/Person.cs
@@ -0,0 +1,21 @@
+﻿namespace HomeExercises
+{
+	public class Person
+	{
+		private static int idCounter = 0;
+		public int Age, Height, Weight;
+		public string Name;
+		public Person? Parent;
+		public int Id;
+
+		public Person(string name, int age, int height, int weight, Person? parent)
+		{
+			Id = idCounter++;
+			Name = name;
+			Age = age;
+			Height = height;
+			Weight = weight;
+			Parent = parent;
+		}
+	}
+}
