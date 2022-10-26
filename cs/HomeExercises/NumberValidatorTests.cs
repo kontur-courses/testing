5e9efaebb32182e2d2e1ae1b5d581f90d9889f27
Defalt1878 <ilyaudalov1@mail.ru>
2022-10-27 02:44:18 +0500

    NumberValidator class refactored

--- cs/HomeExercises/NumberValidatorTests.cs
+++ cs/HomeExercises/NumberValidatorTests.cs
@@ -32,49 +32,50 @@ namespace HomeExercises
 
 	public class NumberValidator
 	{
-		private readonly Regex numberRegex;
-		private readonly bool onlyPositive;
-		private readonly int precision;
-		private readonly int scale;
+		private const string NumberRegexPattern = @"^([+-]?)(\d+)([.,](\d+))?$";
+		private static readonly Regex NumberRegex = new(NumberRegexPattern, RegexOptions.IgnoreCase);
+
+		private readonly int _precision;
+		private readonly int _scale;
+		private readonly bool _onlyPositive;
 
 		public NumberValidator(int precision, int scale = 0, bool onlyPositive = false)
 		{
-			this.precision = precision;
-			this.scale = scale;
-			this.onlyPositive = onlyPositive;
 			if (precision <= 0)
-				throw new ArgumentException("precision must be a positive number");
+				throw new ArgumentException($"{nameof(precision)} must be a positive number");
+			_precision = precision;
+
 			if (scale < 0 || scale >= precision)
-				throw new ArgumentException("precision must be a non-negative number less or equal than precision");
-			numberRegex = new Regex(@"^([+-]?)(\d+)([.,](\d+))?$", RegexOptions.IgnoreCase);
+				throw new ArgumentException(
+					$"{nameof(scale)} must be a non-negative number less or equal than {nameof(precision)}");
+			_scale = scale;
+
+			_onlyPositive = onlyPositive;
 		}
 
 		public bool IsValidNumber(string value)
 		{
 			// Проверяем соответствие входного значения формату N(m,k), в соответствии с правилом, 
-			// описанным в Формате описи документов, направляемых в налоговый орган в электронном виде по телекоммуникационным каналам связи:
-			// Формат числового значения указывается в виде N(m.к), где m – максимальное количество знаков в числе, включая знак (для отрицательного числа), 
-			// целую и дробную часть числа без разделяющей десятичной точки, k – максимальное число знаков дробной части числа. 
-			// Если число знаков дробной части числа равно 0 (т.е. число целое), то формат числового значения имеет вид N(m).
+			// описанным в Формате описи документов, направляемых в налоговый орган в электронном
+			// виде по телекоммуникационным каналам связи:
+			// Формат числового значения указывается в виде N(m.к), где m – максимальное количество знаков в числе,
+			// включая знак (для отрицательного числа), целую и дробную часть числа без разделяющей десятичной
+			// точки, k – максимальное число знаков дробной части числа. Если число знаков дробной части
+			// числа равно 0 (т.е. число целое), то формат числового значения имеет вид N(m).
 
 			if (string.IsNullOrEmpty(value))
 				return false;
 
-			var match = numberRegex.Match(value);
+			var match = NumberRegex.Match(value);
 			if (!match.Success)
 				return false;
 
-			// Знак и целая часть
-			var intPart = match.Groups[1].Value.Length + match.Groups[2].Value.Length;
-			// Дробная часть
-			var fracPart = match.Groups[4].Value.Length;
+			var intPartLength = match.Groups[1].Value.Length + match.Groups[2].Value.Length;
+			var fracPartLength = match.Groups[4].Value.Length;
 
-			if (intPart + fracPart > precision || fracPart > scale)
-				return false;
-
-			if (onlyPositive && match.Groups[1].Value == "-")
-				return false;
-			return true;
+			return intPartLength + fracPartLength <= _precision &&
+			       fracPartLength <= _scale &&
+			       (!_onlyPositive || match.Groups[1].Value != "-");
 		}
 	}
 }
