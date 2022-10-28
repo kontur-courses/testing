404dabeee208b47c9c348d8f6fd68e7759ac78f8
Lodgent <vovagri2003@gmail.com>
2022-10-28 20:43:02 +0500

    fix tests in NumberValidatorTests

--- cs/HomeExercises/NumberValidatorTests.cs
+++ cs/HomeExercises/NumberValidatorTests.cs
@@ -7,26 +7,129 @@ namespace HomeExercises
 {
 	public class NumberValidatorTests
 	{
-		[Test]
-		public void Test()
-		{
-			Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
-			Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
-			Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, false));
-			Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
 
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0"));
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("00.00"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-0.00"));
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+0.00"));
-			Assert.IsTrue(new NumberValidator(4, 2, true).IsValidNumber("+1.23"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+1.23"));
-			Assert.IsFalse(new NumberValidator(17, 2, true).IsValidNumber("0.000"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
+		[TestCase(0, 0, TestName = "Zero precision")]
+		[TestCase(-1, 0, TestName = "Simple negative precision")]
+		[TestCase(int.MinValue, 0, TestName = "Max negative precision")]
+		public void ArgumentException_When_Negative_Zero_Precision(int precision, int scale)
+		{
+			Action validatorAction = () => new NumberValidator(precision, scale);
+			validatorAction.Should().Throw<ArgumentException>("precision must be a positive number");
+		}
+
+		[TestCase(10, 10, TestName = "Precision equal scale")]
+		[TestCase(int.MaxValue, int.MaxValue, TestName = "Precision equal scale maxInteger")]
+		[TestCase(10, -10, TestName = "Negative scale")]
+		[TestCase(10, int.MinValue, TestName = "Negative scale minInteger")]
+		public void ArgumentException_When_Negative_UpperPrecision_Scale(int precision, int scale)
+		{
+			Action validatorAction = () => new NumberValidator(precision, scale);
+			validatorAction.Should()
+				.Throw<ArgumentException>("precision must be a non-negative number less or equal than precision");
+		}
+
+
+
+		[TestCase("abc", TestName = "Simple strings")]
+		[TestCase("#$%^&*", TestName = "Special symbol")]
+		[TestCase("200a", TestName = "String with number")]
+		public void IsValidNumber_Not_Number(string value)
+		{
+			var NumberValidator = new NumberValidator(1, 0, true).IsValidNumber(value);
+			NumberValidator.Should().BeFalse();
+		}
+
+		[TestCase("20,0", TestName = "String with comma")]
+		[TestCase("20 0", TestName = "String with whitespace")]
+		[TestCase("20-0", TestName = "String with minus")]
+		public void IsValidNumber_Have_Not_Dot(string value)
+		{
+			var numberValidator = new NumberValidator(1, 0, true).IsValidNumber(value);
+			numberValidator.Should().BeFalse();
+		}
+
+		[TestCase("-1", TestName = "Simple value without dot")]
+		[TestCase("-1.0", TestName = "Special value with dot")]
+
+		public void IsValidNumber_Only_Positive_With_Negative(string value)
+		{
+			var numberValidator = new NumberValidator(10, 1, true).IsValidNumber(value);
+			numberValidator.Should().BeFalse();
+		}
+
+		[TestCase("1.0", TestName = "Null number validator")]
+
+		public void IsValidNumber_Null(string value)
+		{
+			NumberValidator numberValidator = null;
+			Action action = () => numberValidator.IsValidNumber(value);
+			action.Should().Throw<NullReferenceException>("NumberValidator must not be null");
+		}
+
+		[TestCase(".10", TestName = "Number without int part")]
+		[TestCase("10.", TestName = "Number without fractional part")]
+		[TestCase("$1.10", TestName = "Number with special symbol")]
+		public void IsValidNumber_Not_Match_With_Regex(string value)
+		{
+			var numberValidator = new NumberValidator(4, 3).IsValidNumber(value);
+			numberValidator.Should().BeFalse();
+		}
+
+
+
+
+		[TestCase("+1.0", TestName = "Positive int")]
+		[TestCase("-1.1", TestName = "Negative int")]
+
+		public void IsValidNumber_When_Int_And_Fractional_Part_Not_Bigger_Precision(string value)
+		{
+			var numberValidator = new NumberValidator(3, 2).IsValidNumber(value);
+			numberValidator.Should().BeTrue();
+		}
+
+		[TestCase("1.100", TestName = "Positive int with big scale")]
+		[TestCase("-1.10", TestName = "Negative int with scale")]
+
+		public void IsValidNumber_When_Fractional_Part_Not_Bigger_Scale(string value)
+		{
+			var numberValidator = new NumberValidator(4, 3).IsValidNumber(value);
+			numberValidator.Should().BeTrue();
+		}
+
+
+
+		[TestCase("100.00", TestName = "Positive int bigger precision")]
+		[TestCase("-100.00", TestName = "Negative int bigger precision")]
+
+		public void IsValidNumber_When_Int_And_Fractional_Part_Bigger_Precision(string value)
+		{
+			var numberValidator = new NumberValidator(3, 2).IsValidNumber(value);
+			numberValidator.Should().BeFalse();
+		}
+
+		[TestCase("+1.1000", TestName = "Positive fractional with scale")]
+		[TestCase("-1.1000", TestName = "Negative fractional with scale")]
+
+		public void IsValidNumber_When_Fractional_Part_Bigger_Scale(string value)
+		{
+			var numberValidator = new NumberValidator(4, 3).IsValidNumber(value);
+			numberValidator.Should().BeFalse();
+		}
+
+		[Test]
+		public void IsValidNumber_WithMaxInt()
+		{
+			var numberValidator =
+				new NumberValidator(int.MaxValue.ToString().Length).IsValidNumber(int.MaxValue.ToString());
+			numberValidator.Should().BeTrue();
+		}
+
+		[Test]
+		public void IsValidNumber_WithMinInt()
+		{
+			var numberValidator =
+				new NumberValidator(int.MaxValue.ToString().Length + 1).IsValidNumber(int.MinValue.ToString());
+			numberValidator.Should().BeTrue();
 		}
 	}
 
@@ -64,12 +167,10 @@ namespace HomeExercises
 			if (!match.Success)
 				return false;
 
-			// Знак и целая часть
 			var intPart = match.Groups[1].Value.Length + match.Groups[2].Value.Length;
-			// Дробная часть
-			var fracPart = match.Groups[4].Value.Length;
+			var fractionalPart = match.Groups[4].Value.Length;
 
-			if (intPart + fracPart > precision || fracPart > scale)
+			if (intPart + fractionalPart > precision || fractionalPart > scale)
 				return false;
 
 			if (onlyPositive && match.Groups[1].Value == "-")
