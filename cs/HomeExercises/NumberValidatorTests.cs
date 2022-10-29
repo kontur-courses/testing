bf79c900f4587a4a12fab3498c3d91a6f3889392
Art <bartalamu@list.ru>
2022-10-29 20:22:08 +0500

    v1.1 форматирование

--- cs/HomeExercises/NumberValidatorTests.cs
+++ cs/HomeExercises/NumberValidatorTests.cs
@@ -21,7 +21,7 @@ namespace HomeExercises
         [TestCase(1, 1, true)]
         public void Construct_WithIncorrectScale_Expected_ArgumentException(int precision, int scale, bool onlyPositive)
         {
-	        Action action = () => new NumberValidator(precision, scale, onlyPositive);
+            Action action = () => new NumberValidator(precision, scale, onlyPositive);
 
             action.Should().Throw<ArgumentException>().WithMessage("scale must be a non-negative number less or equal than precision");
         }
@@ -32,9 +32,9 @@ namespace HomeExercises
         [TestCase(2, 1, false)]
         public void Construct_WithCorrectArguments_Expected_NoExceptions(int precision, int scale, bool onlyPositive)
         {
-	        Action act = () => new NumberValidator(precision, scale, onlyPositive);
+            Action action = () => new NumberValidator(precision, scale, onlyPositive);
 
-            act.Should().NotThrow<Exception>();
+            action.Should().NotThrow<Exception>();
         }
 
         [TestCase(1, 0, true, "1", ExpectedResult = true, TestName = "Integer whithout sign")]
@@ -72,50 +72,50 @@ namespace HomeExercises
     }
 
     public class NumberValidator
-	{
-		private readonly Regex numberRegex;
-		private readonly bool onlyPositive;
-		private readonly int precision;
-		private readonly int scale;
+    {
+        private readonly Regex numberRegex;
+        private readonly bool onlyPositive;
+        private readonly int precision;
+        private readonly int scale;
 
-		public NumberValidator(int precision, int scale = 0, bool onlyPositive = false)
-		{
-			this.precision = precision;
-			this.scale = scale;
-			this.onlyPositive = onlyPositive;
-			if (precision <= 0)
-				throw new ArgumentException("precision must be a positive number");
-			if (scale < 0 || scale >= precision)
-				throw new ArgumentException("scale must be a non-negative number less or equal than precision");
-			numberRegex = new Regex(@"^([+-]?)(\d+)([.,](\d+))?$", RegexOptions.IgnoreCase);
-		}
+        public NumberValidator(int precision, int scale = 0, bool onlyPositive = false)
+        {
+            this.precision = precision;
+            this.scale = scale;
+            this.onlyPositive = onlyPositive;
+            if (precision <= 0)
+                throw new ArgumentException("precision must be a positive number");
+            if (scale < 0 || scale >= precision)
+                throw new ArgumentException("scale must be a non-negative number less or equal than precision");
+            numberRegex = new Regex(@"^([+-]?)(\d+)([.,](\d+))?$", RegexOptions.IgnoreCase);
+        }
 
-		public bool IsValidNumber(string value)
-		{
-			// Проверяем соответствие входного значения формату N(m,k), в соответствии с правилом, 
-			// описанным в Формате описи документов, направляемых в налоговый орган в электронном виде по телекоммуникационным каналам связи:
-			// Формат числового значения указывается в виде N(m.к), где m – максимальное количество знаков в числе, включая знак (для отрицательного числа), 
-			// целую и дробную часть числа без разделяющей десятичной точки, k – максимальное число знаков дробной части числа. 
-			// Если число знаков дробной части числа равно 0 (т.е. число целое), то формат числового значения имеет вид N(m).
+        public bool IsValidNumber(string value)
+        {
+            // Проверяем соответствие входного значения формату N(m,k), в соответствии с правилом, 
+            // описанным в Формате описи документов, направляемых в налоговый орган в электронном виде по телекоммуникационным каналам связи:
+            // Формат числового значения указывается в виде N(m.к), где m – максимальное количество знаков в числе, включая знак (для отрицательного числа), 
+            // целую и дробную часть числа без разделяющей десятичной точки, k – максимальное число знаков дробной части числа. 
+            // Если число знаков дробной части числа равно 0 (т.е. число целое), то формат числового значения имеет вид N(m).
 
-			if (string.IsNullOrEmpty(value))
-				return false;
+            if (string.IsNullOrEmpty(value))
+                return false;
 
-			var match = numberRegex.Match(value);
-			if (!match.Success)
-				return false;
+            var match = numberRegex.Match(value);
+            if (!match.Success)
+                return false;
 
-			// Знак и целая часть
-			var intPart = match.Groups[1].Value.Length + match.Groups[2].Value.Length;
-			// Дробная часть
-			var fracPart = match.Groups[4].Value.Length;
+            // Знак и целая часть
+            var intPart = match.Groups[1].Value.Length + match.Groups[2].Value.Length;
+            // Дробная часть
+            var fracPart = match.Groups[4].Value.Length;
 
-			if (intPart + fracPart > precision || fracPart > scale)
-				return false;
+            if (intPart + fracPart > precision || fracPart > scale)
+                return false;
 
-			if (onlyPositive && match.Groups[1].Value == "-")
-				return false;
-			return true;
-		}
-	}
+            if (onlyPositive && match.Groups[1].Value == "-")
+                return false;
+            return true;
+        }
+    }
 }
--- cs/HomeExercises/ObjectComparison.cs
+++ cs/HomeExercises/ObjectComparison.cs
@@ -4,97 +4,97 @@ using System.Text.RegularExpressions;
 
 namespace HomeExercises
 {
-	public class ObjectComparison
-	{
-		[Test]
-		[Description("Проверка текущего царя")]
-		[Category("ToRefactor")]
-		public void CheckCurrentTsar()
-		{
-			var actualTsar = TsarRegistry.GetCurrentTsar();
+    public class ObjectComparison
+    {
+        [Test]
+        [Description("Проверка текущего царя")]
+        [Category("ToRefactor")]
+        public void CheckCurrentTsar()
+        {
+            var actualTsar = TsarRegistry.GetCurrentTsar();
 
-			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
-				new Person("Vasili III of Russia", 28, 170, 60, null));
+            var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
+                new Person("Vasili III of Russia", 28, 170, 60, null));
 
-			actualTsar.Should().BeEquivalentTo(expectedTsar, options => options
-				.Excluding(p => p.Id)
-				.Excluding(p => p.Parent!.Id)
-				.Excluding(p => p.Parent!.Weight) // в оригинале нет теста для weight
-				.Excluding(p => p.Parent!.Parent));
-			//Assert.AreEqual(expectedTsar.Parent.Parent, actualTsar.Parent.Parent); - сравнивает ссылки
-			actualTsar.Parent!.Parent.Should().BeSameAs(expectedTsar.Parent!.Parent);
+            actualTsar.Should().BeEquivalentTo(expectedTsar, options => options
+                .Excluding(p => p.Id)
+                .Excluding(p => p.Parent!.Id)
+                .Excluding(p => p.Parent!.Weight) // в оригинале нет теста для weight
+                .Excluding(p => p.Parent!.Parent));
+            //Assert.AreEqual(expectedTsar.Parent.Parent, actualTsar.Parent.Parent); - сравнивает ссылки
+            actualTsar.Parent!.Parent.Should().BeSameAs(expectedTsar.Parent!.Parent);
         }
-		[Test]
-		[Description("Вариация альтернативного решения")]
-		[Category("ToRefactorAlternative")]
-		public void CheckCurrentTsarAlternative()
-		{
-			var actualTsar = TsarRegistry.GetCurrentTsar();
+        [Test]
+        [Description("Вариация альтернативного решения")]
+        [Category("ToRefactorAlternative")]
+        public void CheckCurrentTsarAlternative()
+        {
+            var actualTsar = TsarRegistry.GetCurrentTsar();
 
-			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
-				new Person("Vasili III of Russia", 28, 170, 60, null));
+            var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
+                new Person("Vasili III of Russia", 28, 170, 60, null));
 
 
-			var idField = new Regex(@"(^|\.)Id$");
-			actualTsar.Should().BeEquivalentTo(expectedTsar, options => options
-															.Excluding(p => idField.IsMatch(p.Path)));
-		}
-		[Test]
-		[Description("Альтернативное решение. Какие у него недостатки?")]
-		public void CheckCurrentTsar_WithCustomEquality()
-		{
-			var actualTsar = TsarRegistry.GetCurrentTsar();
-			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
-				new Person("Vasili III of Russia", 28, 170, 60, null));
+            var idField = new Regex(@"(^|\.)Id$");
+            actualTsar.Should().BeEquivalentTo(expectedTsar, options => options
+                                                            .Excluding(p => idField.IsMatch(p.Path)));
+        }
+        [Test]
+        [Description("Альтернативное решение. Какие у него недостатки?")]
+        public void CheckCurrentTsar_WithCustomEquality()
+        {
+            var actualTsar = TsarRegistry.GetCurrentTsar();
+            var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
+                new Person("Vasili III of Russia", 28, 170, 60, null));
 
-			// Какие недостатки у такого подхода? 
-			Assert.True(AreEqual(actualTsar, expectedTsar));
-			// 1 Не показательно - судя по названию методов тестирования, интересует не результат сравнения:
-			// равны объекты или нет, а проверить соответствует ли объект заданным параметрам и => всего 1 assert
-			// не будет показателен, в плане где именно произошло несоответствие
-			// 2 может уйти в цикл parent1.parent2.parent1...
-			// 3 проверка сильно привязана к структуре объекта
+            // Какие недостатки у такого подхода? 
+            Assert.True(AreEqual(actualTsar, expectedTsar));
+            // 1 Не показательно - судя по названию методов тестирования, интересует не результат сравнения:
+            // равны объекты или нет, а проверить соответствует ли объект заданным параметрам и => всего 1 assert
+            // не будет показателен, в плане где именно произошло несоответствие
+            // 2 может уйти в цикл parent1.parent2.parent1...
+            // 3 проверка сильно привязана к структуре объекта
         }
 
         private bool AreEqual(Person? actual, Person? expected)
-		{
-			if (actual == expected) return true;
-			if (actual == null || expected == null) return false;
-			return
-				actual.Name == expected.Name
-				&& actual.Age == expected.Age
-				&& actual.Height == expected.Height
-				&& actual.Weight == expected.Weight
-				&& AreEqual(actual.Parent, expected.Parent);
-		}
-	}
+        {
+            if (actual == expected) return true;
+            if (actual == null || expected == null) return false;
+            return
+                actual.Name == expected.Name
+                && actual.Age == expected.Age
+                && actual.Height == expected.Height
+                && actual.Weight == expected.Weight
+                && AreEqual(actual.Parent, expected.Parent);
+        }
+    }
 
-	public class TsarRegistry
-	{
-		public static Person GetCurrentTsar()
-		{
-			return new Person(
-				"Ivan IV The Terrible", 54, 170, 70,
-				new Person("Vasili III of Russia", 28, 170, 60, null));
-		}
-	}
+    public class TsarRegistry
+    {
+        public static Person GetCurrentTsar()
+        {
+            return new Person(
+                "Ivan IV The Terrible", 54, 170, 70,
+                new Person("Vasili III of Russia", 28, 170, 60, null));
+        }
+    }
 
-	public class Person
-	{
-		public static int IdCounter = 0;
-		public int Age, Height, Weight;
-		public string Name;
-		public Person? Parent;
-		public int Id;
+    public class Person
+    {
+        public static int IdCounter = 0;
+        public int Age, Height, Weight;
+        public string Name;
+        public Person? Parent;
+        public int Id;
 
-		public Person(string name, int age, int height, int weight, Person? parent)
-		{
-			Id = IdCounter++;
-			Name = name;
-			Age = age;
-			Height = height;
-			Weight = weight;
-			Parent = parent;
-		}
-	}
+        public Person(string name, int age, int height, int weight, Person? parent)
+        {
+            Id = IdCounter++;
+            Name = name;
+            Age = age;
+            Height = height;
+            Weight = weight;
+            Parent = parent;
+        }
+    }
 }
