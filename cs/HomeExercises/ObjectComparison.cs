cff5b568f871b63b0a084d8f4f2f1d97bcc48d45
Ovchinnikov Nikita <ovchinnikov.poisk@gmail.com>
2022-10-29 15:59:10 +0500

    Solved ObjectComparison

--- cs/HomeExercises/ObjectComparison.cs
+++ cs/HomeExercises/ObjectComparison.cs
@@ -15,16 +15,10 @@ namespace HomeExercises
 			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
 				new Person("Vasili III of Russia", 28, 170, 60, null));
 
-			// Перепишите код на использование Fluent Assertions.
-			Assert.AreEqual(actualTsar.Name, expectedTsar.Name);
-			Assert.AreEqual(actualTsar.Age, expectedTsar.Age);
-			Assert.AreEqual(actualTsar.Height, expectedTsar.Height);
-			Assert.AreEqual(actualTsar.Weight, expectedTsar.Weight);
-
-			Assert.AreEqual(expectedTsar.Parent!.Name, actualTsar.Parent!.Name);
-			Assert.AreEqual(expectedTsar.Parent.Age, actualTsar.Parent.Age);
-			Assert.AreEqual(expectedTsar.Parent.Height, actualTsar.Parent.Height);
-			Assert.AreEqual(expectedTsar.Parent.Parent, actualTsar.Parent.Parent);
+			actualTsar.Should().BeEquivalentTo(expectedTsar, options => options
+				.Excluding(person =>
+					person.SelectedMemberInfo.Name == nameof(Person.Id) &&
+					person.SelectedMemberInfo.DeclaringType == typeof(Person)));
 		}
 
 		[Test]
@@ -35,7 +29,16 @@ namespace HomeExercises
 			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
 				new Person("Vasili III of Russia", 28, 170, 60, null));
 
-			// Какие недостатки у такого подхода? 
+			/* Какие недостатки у такого подхода? 
+			 * 
+			 * Недостаточная читаемость и расширяемость. 
+			 * По названию не понятно, как именно происходит сравнение, в случае падения теста 
+			 * нужно будет пробегать отладчиком и выяснять из-за чего именно он упал.
+			 * При изменении/добавлении полей в Person, придётся переписывать/дописывать
+			 * сравнение полей в AreEqual. Также AreEqual сам по себе находится вне теста, 
+			 * возможно, это нормальная практика, но мне такое расположение кажется неудобным.
+			 * 
+			 */
 			Assert.True(AreEqual(actualTsar, expectedTsar));
 		}
 
