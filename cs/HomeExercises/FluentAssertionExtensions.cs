55b402f3e1f136e4a92872ff75a092642fb907ce
Alexander Yasinovskiy <alex2021cyberhuman@gmail.com>
2022-10-29 14:43:16 +0500

    Change ObjectComparison.cs, Add FluentAssertionExtensions.cs
    Rewrite code of test CheckCurrentTsar in ObjectComparison using fluent assertion extension method BeEquivalentToPersonIgnoringIdentifiers from file FluentAssertionExtensions.cs, that use BeEquivalentTo method

---
+++ cs/HomeExercises/FluentAssertionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq.Expressions;
+using FluentAssertions.Equivalency;
+using FluentAssertions.Primitives;
+
+namespace HomeExercises
+{
+	public static class FluentAssertionExtensions
+	{
+		public static void BeEquivalentToPersonIgnoringIdentifiers(this ObjectAssertions assertions, Person person)
+		{
+			assertions.BeAssignableTo<Person>()
+				.And.BeEquivalentTo(person, ExcludingPersonIdentifiers);
+		}
+
+		private static EquivalencyAssertionOptions<Person> ExcludingPersonIdentifiers(
+			this EquivalencyAssertionOptions<Person> assertionOptions)
+		{
+			Expression<Func<IMemberInfo, bool>> excludingExpression = memberInfo =>
+				CheckPersonIdMember(memberInfo);
+			return assertionOptions.Excluding(excludingExpression);
+		}
+
+		private static bool CheckPersonIdMember(IMemberInfo memberInfo)
+		{
+			return memberInfo.SelectedMemberInfo != null
+				&& memberInfo.SelectedMemberInfo.DeclaringType == typeof(Person)
+				&& memberInfo.SelectedMemberInfo.Name == nameof(Person.Id)
+				&& memberInfo.SelectedMemberInfo.MemberType == typeof(int);
+		}
+	}
+}
