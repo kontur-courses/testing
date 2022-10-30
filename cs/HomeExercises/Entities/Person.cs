91d388c0a8b1f2c557b105028443f00e52027112
Source34 <rom.steam2010@yandex.ru>
2022-10-31 01:09:21 +0300

    Class decomposing
    Testing classes were decomposed by entities. Entities moved to separate classes

---
+++ cs/HomeExercises/Entities/Person.cs
@@ -0,0 +1,21 @@
+﻿namespace HomeExercises.Entities
+{
+    public class Person
+    {
+        public static int IdCounter = 0;
+        public int Age, Height, Weight;
+        public string Name;
+        public Person? Parent;
+        public int Id;
+
+        public Person(string name, int age, int height, int weight, Person? parent)
+        {
+            Id = IdCounter++;
+            Name = name;
+            Age = age;
+            Height = height;
+            Weight = weight;
+            Parent = parent;
+        }
+    }
+}
