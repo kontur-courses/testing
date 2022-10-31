b100817321efe2dab35bb2c99799e27b618f9076
massshasch <massshasch@gmail.com>
2022-10-31 20:19:10 +0500

    Corrected ObjectComparison.cs

--- cs/HomeExercises/ObjectComparison.cs
+++ cs/HomeExercises/ObjectComparison.cs
@@ -3,105 +3,108 @@ using NUnit.Framework;
 
 namespace HomeExercises
 {
-	public class ObjectComparison
-	{
-		[Test]
-		[Description("Проверка текущего царя")]
-		[Category("ToRefactor")]
-		public void CheckCurrentTsar()
-		{
-			var actualTsar = TsarRegistry.GetCurrentTsar();
+    public class ObjectComparison
+    {
+        [Test]
+        [Description("Проверка текущего царя")]
+        [Category("ToRefactor")]
+        public void CheckCurrentTsar()
+        {
+            var actualTsar = TsarRegistry.GetCurrentTsar();
 
-			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
-				new Person("Vasili III of Russia", 28, 170, 60, null));
+            var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
+                new Person("Vasili III of Russia", 28, 170, 60, null));
 
-			//код переписан в тесте "Should_FailToCheckTsars_When_FieldsOrNestedObjectsAreNotTheSame"
-			#region 
-			// Перепишите код на использование Fluent Assertions.
-			Assert.AreEqual(actualTsar.Name, expectedTsar.Name);
-			Assert.AreEqual(actualTsar.Age, expectedTsar.Age);
-			Assert.AreEqual(actualTsar.Height, expectedTsar.Height);
-			Assert.AreEqual(actualTsar.Weight, expectedTsar.Weight);
+            #region refactored in Equals_ShouldSuccess_WhenClassMembersAreTheSame
 
-			Assert.AreEqual(expectedTsar.Parent!.Name, actualTsar.Parent!.Name);
-			Assert.AreEqual(expectedTsar.Parent.Age, actualTsar.Parent.Age);
-			Assert.AreEqual(expectedTsar.Parent.Height, actualTsar.Parent.Height);
-			Assert.AreEqual(expectedTsar.Parent.Parent, actualTsar.Parent.Parent);
-			#endregion
-		}
+            // Перепишите код на использование Fluent Assertions.
+            Assert.AreEqual(actualTsar.Name, expectedTsar.Name);
+            Assert.AreEqual(actualTsar.Age, expectedTsar.Age);
+            Assert.AreEqual(actualTsar.Height, expectedTsar.Height);
+            Assert.AreEqual(actualTsar.Weight, expectedTsar.Weight);
 
-		[Test]
-		public void Should_FailToCheckTsars_When_FieldsOrNestedObjectsAreNotTheSame()
-		{
-			var actualTsar = TsarRegistry.GetCurrentTsar();
+            Assert.AreEqual(expectedTsar.Parent!.Name, actualTsar.Parent!.Name);
+            Assert.AreEqual(expectedTsar.Parent.Age, actualTsar.Parent.Age);
+            Assert.AreEqual(expectedTsar.Parent.Height, actualTsar.Parent.Height);
+            Assert.AreEqual(expectedTsar.Parent.Parent, actualTsar.Parent.Parent);
 
-			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
-				new Person("Vasili III of Russia", 28, 170, 60, null));
-			
-			actualTsar.Should().BeEquivalentTo(expectedTsar, options =>
-			options.Excluding(t => t.SelectedMemberInfo.Name == "Id"));
-		}
-		
-		/*
-		 * Решение в тесте "Should_FailToCheckTsars_When_FieldsOrNestedObjectsAreNotTheSame"
-		 * лучше решения в тесте "CheckCurrentTsar_WithCustomEquality" потому что:
-		 * 1: при добавлении новых полей в класс Person не придется изменять тест, он останется корректным
-		 * 2: более читабельный
-		 * 3: 1-й тест в случае несовпадения выведет что именно не совпало, а 2-й тест выведет только false
-		 * 4: можно запутаться и передать в метод "AreEqual" аргументы не в том порядке
-		 */
-		
-		[Test]
-		[Description("Альтернативное решение. Какие у него недостатки?")]
-		public void CheckCurrentTsar_WithCustomEquality()
-		{
-			var actualTsar = TsarRegistry.GetCurrentTsar();
-			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
-				new Person("Vasili III of Russia", 28, 170, 60, null));
+            #endregion
+        }
 
-			// Какие недостатки у такого подхода? 
-			Assert.True(AreEqual(actualTsar, expectedTsar));
-		}
+        [Test]
+        public void Equals_ShouldSuccess_WhenClassMembersAreTheSame()
+        {
+            var actualTsar = TsarRegistry.GetCurrentTsar();
 
-		private bool AreEqual(Person? actual, Person? expected)
-		{
-			if (actual == expected) return true;
-			if (actual == null || expected == null) return false;
-			return
-				actual.Name == expected.Name
-				&& actual.Age == expected.Age
-				&& actual.Height == expected.Height
-				&& actual.Weight == expected.Weight
-				&& AreEqual(actual.Parent, expected.Parent);
-		}
-	}
+            var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
+                new Person("Vasili III of Russia", 28, 170, 60, null));
 
-	public class TsarRegistry
-	{
-		public static Person GetCurrentTsar()
-		{
-			return new Person(
-				"Ivan IV The Terrible", 54, 170, 70,
-				new Person("Vasili III of Russia", 28, 170, 60, null));
-		}
-	}
+            actualTsar.Should().BeEquivalentTo(expectedTsar, options => options
+                .Excluding(p =>
+                    p.SelectedMemberInfo.DeclaringType == typeof(Person) &&
+                    p.SelectedMemberInfo.Name == nameof(Person.Id)));
+        }
 
-	public class Person
-	{
-		public static int IdCounter = 0;
-		public int Age, Height, Weight;
-		public string Name;
-		public Person? Parent;
-		public int Id;
+        /*
+         * Решение в тесте "Should_FailToCheckTsars_When_FieldsOrNestedObjectsAreNotTheSame"
+         * лучше решения в тесте "CheckCurrentTsar_WithCustomEquality" потому что:
+         * 1: при добавлении новых полей в класс Person не придется изменять тест, он останется корректным
+         * 2: более читабельный
+         * 3: 1-й тест в случае несовпадения выведет что именно не совпало, а 2-й тест выведет только false
+         * 4: можно запутаться и передать в метод "AreEqual" аргументы не в том порядке
+         */
 
-		public Person(string name, int age, int height, int weight, Person? parent)
-		{
-			Id = IdCounter++;
-			Name = name;
-			Age = age;
-			Height = height;
-			Weight = weight;
-			Parent = parent;
-		}
-	}
+        [Test]
+        [Description("Альтернативное решение. Какие у него недостатки?")]
+        public void CheckCurrentTsar_WithCustomEquality()
+        {
+            var actualTsar = TsarRegistry.GetCurrentTsar();
+            var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
+                new Person("Vasili III of Russia", 28, 170, 60, null));
+
+            // Какие недостатки у такого подхода? 
+            Assert.True(AreEqual(actualTsar, expectedTsar));
+        }
+
+        private bool AreEqual(Person? actual, Person? expected)
+        {
+            if (actual == expected) return true;
+            if (actual == null || expected == null) return false;
+            return
+                actual.Name == expected.Name
+                && actual.Age == expected.Age
+                && actual.Height == expected.Height
+                && actual.Weight == expected.Weight
+                && AreEqual(actual.Parent, expected.Parent);
+        }
+    }
+
+    public class TsarRegistry
+    {
+        public static Person GetCurrentTsar()
+        {
+            return new Person(
+                "Ivan IV The Terrible", 54, 170, 70,
+                new Person("Vasili III of Russia", 28, 170, 60, null));
+        }
+    }
+
+    public class Person
+    {
+        public static int IdCounter = 0;
+        public int Age, Height, Weight;
+        public string Name;
+        public Person? Parent;
+        public int Id;
+
+        public Person(string name, int age, int height, int weight, Person? parent)
+        {
+            Id = IdCounter++;
+            Name = name;
+            Age = age;
+            Height = height;
+            Weight = weight;
+            Parent = parent;
+        }
+    }
 }
