1ef12e5c2c801153f2fec55e7f35d3fe7d85e0ee
Фирстова_Александра <navasylava@mail.ru>
2022-10-31 22:38:24 +0500

    Практика

--- cs/Challenge/WordsStatistics_Tests.cs
+++ cs/Challenge/WordsStatistics_Tests.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using System;
+using System.Linq;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace Challenge
@@ -41,7 +43,91 @@ namespace Challenge
 			wordsStatistics.GetStatistics().Should().HaveCount(2);
 		}
 
+		[Test]
+		public void AddWord_ThrowArgumentNullException_WhenNull()
+		{
+			Action act = ()=>wordsStatistics.AddWord(null);
+			act.Should().Throw<ArgumentNullException>();
+		}
 
+		[TestCase("")]
+		[TestCase(" ")]
+		//[TestCase(@"\u2000")]
+		//[TestCase(@"\t")]
+		public void AddWord_Return_WhenWhitespace(string input)
+		{
+			wordsStatistics.AddWord(input);
+			wordsStatistics.GetStatistics().Should().BeEmpty();
+		}
+        [Test]
+        public void AddWord_Trim10_WhenMore10()
+        {
+            string input = "abcdefghijk";
+            wordsStatistics.AddWord(input);
+            wordsStatistics.GetStatistics().Should().Equal(new WordCount("abcdefghij", 1));
+            wordsStatistics.GetStatistics().ElementAt(0).Word.Length.Should().Be(10);
+        }
+
+        [Test]
+        public void AddWord_WhenMore5()
+        {
+	        string input = "abcdef";
+	        wordsStatistics.AddWord(input);
+	        wordsStatistics.GetStatistics().ElementAt(0).Word.Length.Should().Be(6);
+        }
+
+        [Test]
+        public void AddWord_CountMore1237()
+        {
+	        for (int i = 0; i < 1238; i++)
+	        {
+				wordsStatistics.AddWord(i.ToString());
+			}
+
+	        wordsStatistics.GetStatistics().Should().HaveCount(1238);
+        }
+
+		[Test]
+        public void AddWord_Lower_WhenBeLarge()
+        {
+	        string input = "ABCD";
+	        wordsStatistics.AddWord(input);
+	        wordsStatistics.GetStatistics().Should().Equal(new WordCount("abcd", 1));
+        }
+
+        [Test]
+        public void AddWord_Count2_When2Same2Word()
+        {
+	        string input = "ABCD";
+	        wordsStatistics.AddWord(input);
+	        wordsStatistics.AddWord(input);
+			wordsStatistics.GetStatistics().Should().Equal(new WordCount("abcd", 2));
+        }
+
+        [Test]
+        public void GetStatistics_AscendingSortWord()
+        {
+	        wordsStatistics.AddWord("a");
+	        wordsStatistics.AddWord("ab");
+			wordsStatistics.AddWord("abc");
+			wordsStatistics.GetStatistics().Select(w => w.Word).Should().BeInAscendingOrder();
+        }
+
+        [Test]
+        public void GetStatistics_DescendingSortCount()
+        {
+	        wordsStatistics.AddWord("a");
+	        wordsStatistics.AddWord("abc");
+	        wordsStatistics.AddWord("abc");
+	        wordsStatistics.GetStatistics().Select(w => w.Count).Should().BeInDescendingOrder();
+        }
+
+        [Test]
+        public void AddWord()
+        {
+	        wordsStatistics.AddWord("          a");
+	        wordsStatistics.GetStatistics().Should().HaveCount(1);
+        }
 		// Документация по FluentAssertions с примерами : https://github.com/fluentassertions/fluentassertions/wiki
 	}
 }
