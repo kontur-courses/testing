46e3fb70460c67e49c06cfb009d7514269acd6fd
IuxMorti <zdracte21@bk.ru>
2022-10-31 14:06:05 +0500

    typo fixed in NumberValidator.cs

--- cs/HomeExercises/NumberValidator.cs
+++ cs/HomeExercises/NumberValidator.cs
@@ -18,7 +18,7 @@ namespace HomeExercises
 			if (precision <= 0)
 				throw new ArgumentException("precision must be a positive number");
 			if (scale < 0 || scale >= precision)
-				throw new ArgumentException("precision must be a non-negative number less or equal than precision");
+				throw new ArgumentException("scale must be a non-negative number less or equal than precision");
 			numberRegex = new Regex(@"^([+-]?)(\d+)([.,](\d+))?$", RegexOptions.IgnoreCase);
 		}
 
