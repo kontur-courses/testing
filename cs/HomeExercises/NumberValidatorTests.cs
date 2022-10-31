8bbf5c2ea89bcd88c8e9a99e2ff92d61ab6e449f
glushakov.m <glushakovmax@mail.ru>
2022-11-01 00:10:36 +0300

    done NumberValidatorTests home work

--- cs/HomeExercises/NumberValidatorTests.cs
+++ cs/HomeExercises/NumberValidatorTests.cs
@@ -8,6 +8,133 @@ namespace HomeExercises
 	public class NumberValidatorTests
 	{
 		[Test]
+		[TestCase(-1)]
+		[TestCase(0)]
+		public void Constructor_NotPositivePrecious_ShouldThrow(int precision)
+		{
+			new Action(() =>
+				{
+					var numberValidator = new NumberValidator(precision);
+				})
+				.Should().Throw<ArgumentException>();
+		}
+
+		[Test]
+		[TestCase(1, TestName = "PositivePrecious_NoThrow")]
+		public void Constructor_PositivePrecious_NoThrow(int precision)
+		{
+			new Action(() =>
+				{
+					var numberValidator = new NumberValidator(precision);
+				})
+				.Should().NotThrow();
+		}
+
+		[Test]
+		[TestCase(1, -1, TestName = "NegativeScale_Throw")]
+		[TestCase(1, 5, TestName = "ScaleGreaterThenPrecious_Throw")]
+		[TestCase(5, 5, TestName = "ScaleEqualPrecious_Throw")]
+		public void Constructor_NotValidScale_Throw(int precision, int scale)
+		{
+			new Action(() =>
+				{
+					var numberValidator = new NumberValidator(precision);
+				})
+				.Should().Throw<ArgumentException>();
+		}
+
+		[Test]
+		[TestCase("")]
+		[TestCase(null)]
+		public void IsValidNumber_EmptyOrNullString_BeFalse(string value)
+		{
+			var validator = new NumberValidator(5);
+			validator.IsValidNumber(value).Should().BeFalse();
+		}
+
+
+		[Test]
+		[TestCase("-1.0")]
+		public void ValidOnlyPositiveNum_NegativeNum_BeFalse(string value)
+		{
+			var validator = new NumberValidator(17, 2, true);
+			validator.IsValidNumber(value).Should().BeFalse();
+		}
+
+		[Test]
+		[TestCase(5, 1, "1.12")]
+		[TestCase(5, 2, "1.123")]
+		[TestCase(5, 3, "1.1234")]
+		public void IsValidNumber_FracPartGreaterScale_BeFalse(int precious, int scale, string value)
+		{
+			var validator = new NumberValidator(precious, scale);
+			validator.IsValidNumber(value).Should().BeFalse();
+		}
+
+		[Test]
+		[TestCase(5, 1, "0.1")]
+		[TestCase(5, 2, "0.12")]
+		[TestCase(5, 4, "0.123")]
+		public void IsValidNumber_FracPartLessOrEqualsScale_BeTrue(int precious, int scale, string value)
+		{
+			var validator = new NumberValidator(precious, scale);
+			validator.IsValidNumber(value).Should().BeTrue();
+		}
+
+		[Test]
+		[TestCase(5, "1223123123.1")]
+		[TestCase(2, "1223.1")]
+		public void ValidNumber_GreaterPreciousPart_BeFalse(int precious, string value)
+		{
+			var validator = new NumberValidator(precious, 1);
+			validator.IsValidNumber(value).Should().BeFalse();
+		}
+
+
+		[Test]
+		[TestCase(5, 2, "-123.1")]
+		[TestCase(5, 2, "+123.1")]
+		[TestCase(5, 2, "122.1")]
+		public void ValidNumber_SignedNum_BeTrue(int precious, int scale, string value)
+		{
+			var validator = new NumberValidator(precious, 1);
+			validator.IsValidNumber(value).Should().BeTrue();
+		}
+
+		[Test]
+		[TestCase(7, 4, "abc.def")]
+		[TestCase(7, 4, "-abc.1w3")]
+		[TestCase(7, 4, "a1c.123")]
+		[TestCase(7, 4, "123.bef")]
+		[TestCase(7, 4, "123.b4f")]
+		[TestCase(7, 4, "@!.123")]
+		[TestCase(7, 4, "123.$#%")]
+		[TestCase(7, 4, "-.123")]
+		[TestCase(7, 4, "-+.123")]
+		public void ValidNumber_LetterOrSignInNum_BeFalse(int precious, int scale, string value)
+		{
+			var validator = new NumberValidator(precious, scale);
+			validator.IsValidNumber(value).Should().BeFalse();
+		}
+
+		[Test]
+		[TestCase(5, "123")]
+		public void ValidNumber_ZeroScale_BeTrue(int precious, string value)
+		{
+			var validator = new NumberValidator(precious);
+			validator.IsValidNumber(value).Should().BeTrue();
+		}
+
+		[Test]
+		[TestCase(5, 2, "12.25")]
+		[TestCase(5, 2, "12,25")]
+		public void ValidNumber_DotOrComma_BeTrue(int precious, int scale, string value)
+		{
+			var validator = new NumberValidator(precious, scale);
+			validator.IsValidNumber(value).Should().BeTrue();
+		}
+		
+		[Test]
 		public void Test()
 		{
 			Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
