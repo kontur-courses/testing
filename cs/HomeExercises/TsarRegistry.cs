0e188f6b4b2099750d7aba99b62094844cab12f6
Ilya <ilyapol92@gmail.com>
2022-10-31 21:44:29 +0500

    move TsarRegistry to separate file

---
+++ cs/HomeExercises/TsarRegistry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeExercises
+{
+	public class TsarRegistry
+	{
+		public static Person GetCurrentTsar()
+		{
+			return new Person(
+				"Ivan IV The Terrible", 54, 170, 70,
+				new Person("Vasili III of Russia", 28, 170, 60, null));
+		}
+	}
+}
