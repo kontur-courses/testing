e094da85febf3a2b957b5f4f12c2d0d4abd777a9
massshasch <massshasch@gmail.com>
2022-11-01 13:52:48 +0500

    Fixed tests in NumberValidatorTests.cs

--- cs/HomeExercises/NumberValidatorTests.cs
+++ cs/HomeExercises/NumberValidatorTests.cs
@@ -38,8 +38,7 @@ namespace HomeExercises
 
         [TestCase(4, 2, "", TestName = "String is empty")]
         [TestCase(4, 2, null, TestName = "String = null")]
-        public void IsValidNumber_ShouldReturnFalse_WhenInputIsNullOrEmpty(int precision, int scale,
-            string value)
+        public void IsValidNumber_ShouldReturnFalse_WhenInputIsNullOrEmpty(int precision, int scale, string value)
         {
             new NumberValidator(precision, scale).IsValidNumber(value).Should().BeFalse();
         }
@@ -48,14 +47,19 @@ namespace HomeExercises
         [TestCase(3, 1, "1..1", TestName = "Contains 2 points in a row")]
         [TestCase(3, 2, ".11", TestName = "Starts with point")]
         [TestCase(4, 2, "1.1.1.1", TestName = "Count of points > 1")]
-        [TestCase(4, 2, "a.sd", TestName = "Contains unresolved symbols")]
+        [TestCase(4, 2, "a.sd", TestName = "Contains letters with point")]
+        [TestCase(3, 0, "abc", TestName = "Contains letters")]
+        [TestCase(4, 0, "13sd", TestName = "Contains letters with numbers")]
+        [TestCase(2, 0, "\n\r", TestName = "Contains escape-symbols")]
+        [TestCase(4, 0, "10.\n\r", TestName = "Contains number and escape-symbols")]
+        [TestCase(3, 0, "✹✹✹", TestName = "Contains unresolved symbols")]
+        [TestCase(4, 0, "13✹✹", TestName = "Contains number and unresolved symbol")]
         [TestCase(2, 0, "11.", TestName = "Ends with point")]
         [TestCase(1, 0, "-", TestName = "Contains only sing")]
         [TestCase(1, 0, ".", TestName = "Contains only point")]
         [TestCase(3, 2, "-.11", TestName = "Starts with sing and point")]
         [TestCase(4, 2, "1.-11", TestName = "Sing isn't ahead")]
-        public void IsValidNumber_ShouldReturnFalse_WhenInputDoesNotMatchRegex(int precision, int scale,
-            string value)
+        public void IsValidNumber_ShouldReturnFalse_WhenInputDoesNotMatchRegex(int precision, int scale, string value)
         {
             new NumberValidator(precision, scale).IsValidNumber(value).Should().BeFalse();
         }
@@ -64,30 +68,27 @@ namespace HomeExercises
         [TestCase(3, 2, "+0.00", TestName = "Input longer than precision and contains sign")]
         [TestCase(5, 2, "11.111", TestName = "Frac part longer than scale")]
         [TestCase(2, 0, "-10", TestName = "Sign included in precision")]
-        public void IsValidNumber_ShouldReturnFalse_OnIncorrectPrecisionOrScale(int precision, int scale,
-            string value)
+        public void IsValidNumber_ShouldReturnFalse_OnIncorrectPrecisionOrScale(int precision, int scale, string value)
         {
             new NumberValidator(precision, scale).IsValidNumber(value).Should().BeFalse();
         }
 
-        [TestCase(2, 0, true, "-1", TestName = "Negative number")]
-        [TestCase(2, 0, true, "-0", TestName = "Number is -0")]
-        public void IsValidNumber_ShouldReturnFalse_WhenFlagOnlyPositiveIsTrueAndInputIsNegative(int precision,
-            int scale, bool onlyPositive,
-            string value)
+        [TestCase(2, 0, "-1", TestName = "Negative number")]
+        [TestCase(2, 0, "-0", TestName = "Number is -0")]
+        public void IsValidNumber_ShouldReturnFalse_WhenFlagOnlyPositiveIsTrueAndInputIsNegative(
+            int precision, int scale, string value)
         {
-            new NumberValidator(precision, scale, onlyPositive).IsValidNumber(value).Should().BeFalse();
+            new NumberValidator(precision, scale, true).IsValidNumber(value).Should().BeFalse();
         }
 
         [TestCase(6, 2, false, "-10,15", TestName = "Num is negative, \",\" instead \".\"")]
         [TestCase(2, 0, false, "10", TestName = "Positive integer, onlyPositive is false")]
         [TestCase(17, 2, true, "0.0", TestName = "Input shorter than precision and scale > frac part")]
         [TestCase(4, 2, true, "10.11", TestName = "Precision = lenght of input (except point), scale = frac part")]
-        [TestCase(4, 2, false, "+1",
-            TestName = "OnlyPositive is false and num is positive, scale > 0 and frac part is missing")]
+        [TestCase(4, 2, false, "+1", TestName = "OnlyPositive is false, num > 0, scale > 0, frac part is missing")]
         [TestCase(100, 0, false, "13", TestName = "Input shorter than precision")]
-        public void IsValidNumber_ShouldReturnTrue_OnCorrectInput(int precision, int scale, bool onlyPositive,
-            string value)
+        public void IsValidNumber_ShouldReturnTrue_OnCorrectInput(
+            int precision, int scale, bool onlyPositive, string value)
         {
             new NumberValidator(precision, scale, onlyPositive).IsValidNumber(value).Should().BeTrue();
         }
