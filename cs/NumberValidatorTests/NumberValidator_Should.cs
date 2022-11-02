67170c2942acfe1152ca348170908e70656ee2d0
angst-storm <serezha.kiprin@gmail.com>
2022-11-02 18:07:18 +0500

    refactored tests

--- cs/NumberValidatorTests/NumberValidator_Should.cs
+++ cs/NumberValidatorTests/NumberValidator_Should.cs
@@ -26,62 +26,67 @@ public class NumberValidator_Should
 			.Throw<ArgumentException>()
 			.WithMessage("scale must be a non-negative number less or equal than precision");
 	}
-	
-	[TestCase(2, 1, TestName = "Scale less then precision")]
-	[TestCase(1, 0, TestName = "Zero scale")]
-	[TestCase(1, 0, true, TestName = "Only positive")]
-	[TestCase(1, 0, false, TestName = "Not only positive")]
-	[TestCase(int.MaxValue, 0, TestName = "Max value precision")]
-	[TestCase(int.MaxValue, int.MaxValue - 1, TestName = "Max value scale")]
-	public void Constructor_NotThrowArgumentException_OnValidParameters(int precision,
+
+	[TestCase(2, 1, TestName = "{m}ScaleLessThenPrecision")]
+	[TestCase(1, 0, TestName = "{m}ZeroScale")]
+	[TestCase(1, 0, true, TestName = "{m}OnlyPositive")]
+	[TestCase(1, 0, false, TestName = "{m}NotOnlyPositive")]
+	[TestCase(int.MaxValue, 0, TestName = "{m}MaxValuePrecision")]
+	[TestCase(int.MaxValue, int.MaxValue - 1, TestName = "{m}MaxValueScale")]
+	public void Constructor_NotThrowArgumentException_On(int precision,
 		int scale, bool onlyPositive = false)
 	{
 		Action constructor = () => new NumberValidator(precision, scale, onlyPositive);
 		constructor.Should()
 			.NotThrow<ArgumentException>();
 	}
-	
-			[TestCase(null, TestName = "Null string")]
-		[TestCase("", TestName = "Empty string")]
-		[TestCase(" ", TestName = "Space string")]
-		[TestCase("lette.rs", TestName = "String with letters")]
-		[TestCase("+", TestName = "Only plus sign")]
-		[TestCase("-", TestName = "Only minus sign")]
-		[TestCase("0.", TestName = "With dot but without fractional part")]
-		[TestCase("0,", TestName = "With comma but without fractional part")]
-		[TestCase(".0", TestName = "With dot but without integer part")]
-		[TestCase(",0", TestName = "With comma but without integer part")]
-		public void IsValidNumber_ReturnFalse_OnNotNumber(string number)
-		{
-			new NumberValidator(10, 2).IsValidNumber(number).Should().BeFalse();
-		}
 
-		[TestCase("000", 2, 0, TestName = "Too long number without fractional part")]
-		[TestCase("0.000", 3, 1, TestName = "Too long number without sign")]
-		[TestCase("+0.00", 3, 2, TestName = "Too long number with plus sign")]
-		[TestCase("-0.00", 3, 2, TestName = "Too long number with minus sign")]
-		[TestCase("0.00", 3, 1, TestName = "Too long fractional part")]
-		[TestCase("0.00", 3, 0, TestName = "Fractional part and integer validator")]
-		[TestCase("-0.00", 4, 2, true, TestName = "Negative Number and only positive validator")]
-		public void IsValidNumber_ReturnFalse__OnIncorrectParameters(
-			string number, int precision, int scale, bool onlyPositive = false)
-		{
-			new NumberValidator(precision, scale, onlyPositive).IsValidNumber(number).Should().BeFalse();
-		}
+	[TestCase(null, TestName = "{m}IsNull")]
+	[TestCase("", TestName = "{m}IsEmpty")]
+	[TestCase(" ", TestName = "{m}IsOnlySpace")]
+	[TestCase("lette.rs", TestName = "{m}WithLetters")]
+	[TestCase("+", TestName = "{m}IsOnlyPlusSign")]
+	[TestCase("-", TestName = "{m}IsOnlyMinusSign")]
+	[TestCase("0.", TestName = "{m}WithDotButWithoutFractionalPart")]
+	[TestCase("0,", TestName = "{m}WithCommaButWithoutFractionalPart")]
+	[TestCase(".0", TestName = "{m}WithDotButWithoutIntegerPart")]
+	[TestCase(",0", TestName = "{m}WithCommaButWithoutIntegerPart")]
+	[TestCase("0..0", TestName = "{m}WithDoubleComma")]
+	[TestCase("0,,0", TestName = "{m}WithDoubleDot")]
+	[TestCase("0.,0", TestName = "{m}WithDotAndComma")]
+	[TestCase("++0", TestName = "{m}WithDoublePlus")]
+	[TestCase("--0", TestName = "{m}WithDoubleMinus")]
+	[TestCase("0, 0", TestName = "{m}WithExcessSpaceInsideNumber")]
+	[TestCase("0,0 ", TestName = "{m}WithExcessSpaceOutsideNumber")]
+	public void IsValidNumber_ReturnFalse_OnString(string number)
+	{
+		new NumberValidator(10, 2).IsValidNumber(number).Should().BeFalse();
+	}
 
-		[TestCase("0", 1, 0, TestName = "Zero")]
-		[TestCase("1", 1, 0, TestName = "Integer number")]
-		[TestCase("123", 3, 2, TestName = "Integer number and not integer validator")]
-		[TestCase("1", 10, 2, TestName = "Precision greater then number length")]
-		[TestCase("1.23", 3, 2, TestName = "Fractional number with dot")]
-		[TestCase("1,23", 3, 2, TestName = "Fractional number with comma")]
-		[TestCase("1.23", 11, 10, TestName = "Fractional number and big scale")]
-		[TestCase("+1.23", 4, 2, TestName = "Positive number with sign")]
-		[TestCase("-1.23", 4, 2, TestName = "Negative number with sign and not only positive validator")]
-		[TestCase("+1.23", 4, 2, true, TestName = "Positive number with sign and only positive validator")]
-		public void IsValidNumber_ReturnTrue_OnCorrectParameters(
-			string number, int precision, int scale, bool onlyPositive = false)
-		{
-			new NumberValidator(precision, scale, onlyPositive).IsValidNumber(number).Should().BeTrue();
-		}
+	[TestCase("000", 2, 0, TestName = "{m}TooLongNumberWithoutFractionalPart")]
+	[TestCase("0.000", 3, 1, TestName = "{m}TooLongNumberWithoutSign")]
+	[TestCase("+0.00", 3, 2, TestName = "{m}TooLongNumberWithPlusSign")]
+	[TestCase("-0.00", 3, 2, TestName = "{m}TooLongNumberWithMinusSign")]
+	[TestCase("0.00", 3, 1, TestName = "{m}TooLongFractionalPart")]
+	[TestCase("0.00", 3, 0, TestName = "{m}FractionalPartAndIntegerValidator")]
+	[TestCase("-0.00", 4, 2, true, TestName = "{m}NegativeNumberAndOnlyPositiveValidator")]
+	public void IsValidNumber_ReturnFalse_On(string number, int precision, int scale, bool onlyPositive = false)
+	{
+		new NumberValidator(precision, scale, onlyPositive).IsValidNumber(number).Should().BeFalse();
+	}
+
+	[TestCase("0", 1, 0, TestName = "{m}Zero")]
+	[TestCase("1", 1, 0, TestName = "{m}IntegerNumber")]
+	[TestCase("123", 3, 2, TestName = "{m}IntegerNumberAndNotIntegerValidator")]
+	[TestCase("1", 10, 2, TestName = "{m}PrecisionGreaterThenNumberLength")]
+	[TestCase("1.23", 3, 2, TestName = "{m}FractionalNumberWithDot")]
+	[TestCase("1,23", 3, 2, TestName = "{m}FractionalNumberWithComma")]
+	[TestCase("1.23", 11, 10, TestName = "{m}ScaleGreaterThenFractionalPartLength")]
+	[TestCase("+1.23", 4, 2, TestName = "{m}PositiveNumberWithSign")]
+	[TestCase("-1.23", 4, 2, TestName = "{m}NegativeNumberAndNotOnlyPositiveValidator")]
+	[TestCase("+1.23", 4, 2, true, TestName = "{m}PositiveNumberAndOnlyPositiveValidator")]
+	public void IsValidNumber_ReturnTrue_On(string number, int precision, int scale, bool onlyPositive = false)
+	{
+		new NumberValidator(precision, scale, onlyPositive).IsValidNumber(number).Should().BeTrue();
+	}
 }
