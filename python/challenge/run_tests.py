4565ad3d8e12d77c174878f4194f7e62816dac75
d184230 <dubkov.a@skbkontur.ru>
2023-06-28 15:28:57 +0300

    improve

--- python/challenge/run_tests.py
+++ python/challenge/run_tests.py
@@ -1,8 +1,10 @@
-import subprocess
 import os
 import fb
+import pytest
 from glob import iglob
 from your_name import AUTHORS
+from io import StringIO
+from contextlib import redirect_stdout
 
 
 def get_folder_with_incorrect_tests():
@@ -16,28 +18,27 @@ def get_path_to_correct_tests():
 
 
 def run_tests_for_correct_implementation():
-    r = subprocess.run([f"pytest {get_path_to_correct_tests()}"], shell=True, stdout=subprocess.DEVNULL)
-    result = "ПРОЙДЕНЫ" if not r.returncode else "НЕ ПРОЙДЕНЫ"
+    temp_stdout = StringIO()
+    with redirect_stdout(temp_stdout):
+        tests_result = pytest.main([get_path_to_correct_tests(),])
+    result = "ПРОЙДЕНЫ" if not tests_result else "НЕ ПРОЙДЕНЫ"
     print(f"Тесты на правильную имплементацию: {result}")
-    return True if not r.returncode else False
+    return bool(not tests_result)
 
 
 def run_tests_for_incorrect_implementation():
+    temp_stdout = StringIO()
     firebase_data = {}
     total_count = 0
     correct_passed = 0
     tests_folder = get_folder_with_incorrect_tests()
     for filename in os.listdir(tests_folder):
         if filename.startswith("test_") and filename.endswith(".py"):
-            r = subprocess.run(
-                    [f"pytest {tests_folder}/{filename}"],
-                    shell=True,
-                    stdout=subprocess.DEVNULL
-            )
+            with redirect_stdout(temp_stdout):
+                test_result = pytest.main([f"{tests_folder}/{filename}",])
             total_count += 1
-            passed = 1 if r.returncode else 0
-            correct_passed += passed
-            firebase_data[fb.get_firebase_key_from_filename(filename)] = passed
+            correct_passed += test_result
+            firebase_data[fb.get_firebase_key_from_filename(filename)] = test_result
     print(f"Тесты на неправильную имплементацию: КОРРЕКТНО {correct_passed} из {total_count}")
     fb.send_to_firebase(AUTHORS, firebase_data)
 
