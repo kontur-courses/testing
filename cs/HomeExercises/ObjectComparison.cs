1b9c8daf73127847c3ec29978fb0c190332b020b
ShpakovDaniel <44341164+Reltig@users.noreply.github.com>
2023-11-24 10:50:31 +0500

    add two variants of test

--- cs/HomeExercises/ObjectComparison.cs
+++ cs/HomeExercises/ObjectComparison.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using System;
+using FluentAssertions;
+using FluentAssertions.Equivalency;
 using NUnit.Framework;
 
 namespace HomeExercises
@@ -16,15 +18,29 @@ namespace HomeExercises
 				new Person("Vasili III of Russia", 28, 170, 60, null));
 
 			// Перепишите код на использование Fluent Assertions.
-			Assert.AreEqual(actualTsar.Name, expectedTsar.Name);
-			Assert.AreEqual(actualTsar.Age, expectedTsar.Age);
-			Assert.AreEqual(actualTsar.Height, expectedTsar.Height);
-			Assert.AreEqual(actualTsar.Weight, expectedTsar.Weight);
-
-			Assert.AreEqual(expectedTsar.Parent!.Name, actualTsar.Parent!.Name);
-			Assert.AreEqual(expectedTsar.Parent.Age, actualTsar.Parent.Age);
-			Assert.AreEqual(expectedTsar.Parent.Height, actualTsar.Parent.Height);
-			Assert.AreEqual(expectedTsar.Parent.Parent, actualTsar.Parent.Parent);
+			
+			// 1 вариант, эквивалентен тесту CheckCurrentTsar
+			actualTsar.Should().BeEquivalentTo(expectedTsar, options => 
+				options
+					.Excluding(p => p.Id)
+					.Excluding(p => p.Parent!.Id)
+			);
+			// 2 вариант, эквивалентен тесту CheckCurrentTsar_WithCustomEquality
+			actualTsar.Should().BeEquivalentTo(expectedTsar, options => 
+				options
+					.Excluding((IMemberInfo info) => 
+						info.SelectedMemberInfo.DeclaringType == typeof(Person)
+						&& info.SelectedMemberInfo.Name == nameof(Person.Id))
+			);
+			// Assert.AreEqual(actualTsar.Name, expectedTsar.Name);
+			// Assert.AreEqual(actualTsar.Age, expectedTsar.Age);
+			// Assert.AreEqual(actualTsar.Height, expectedTsar.Height);
+			// Assert.AreEqual(actualTsar.Weight, expectedTsar.Weight);
+			//
+			// Assert.AreEqual(expectedTsar.Parent!.Name, actualTsar.Parent!.Name);
+			// Assert.AreEqual(expectedTsar.Parent.Age, actualTsar.Parent.Age);
+			// Assert.AreEqual(expectedTsar.Parent.Height, actualTsar.Parent.Height);
+			// Assert.AreEqual(expectedTsar.Parent.Parent, actualTsar.Parent.Parent);
 		}
 
 		[Test]
@@ -35,6 +51,7 @@ namespace HomeExercises
 			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
 				new Person("Vasili III of Russia", 28, 170, 60, null));
 
+			
 			// Какие недостатки у такого подхода? 
 			Assert.True(AreEqual(actualTsar, expectedTsar));
 		}
