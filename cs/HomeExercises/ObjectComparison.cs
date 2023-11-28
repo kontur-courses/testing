f9d2b34726e4c16c05a624197d5569a30e8b737d
Alexander Gorbatov <shantem965@mail.ru>
2023-11-28 22:41:48 +0500

    refactor

--- cs/HomeExercises/ObjectComparison.cs
+++ cs/HomeExercises/ObjectComparison.cs
@@ -1,66 +1,6 @@
-﻿using FluentAssertions;
-using NUnit.Framework;
-
-namespace HomeExercises
+﻿namespace HomeExercises
 {
-	public class ObjectComparison
-	{
-		[Test]
-		[Description("Проверка текущего царя")]
-		[Category("ToRefactor")]
-		public void CheckCurrentTsar()
-		{
-			var actualTsar = TsarRegistry.GetCurrentTsar();
-
-			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
-				new Person("Vasili III of Russia", 28, 170, 60, null));
-			
-			/*
-			 * Чем это решение лучше решения в CheckCurrentTsar_WithCustomEquality:
-			 * 1. Это решение не нужно будет переписывать при добавлении/удалении в класс/из класса Person полей.
-			 * 2. В этом решении наглядно видно, какое именно поле не учитывается при сравнении объектов.
-			 * 3. При падении теста мы увидим, какие именно поля объектов не прошли проверку на равенство.
-			 */
-			actualTsar
-				.Should()
-				.BeEquivalentTo(expectedTsar, options => options
-					.Excluding(info => info.SelectedMemberInfo.Name.Equals(nameof(Person.Id))));
-		}
-
-		[Test]
-		[Description("Альтернативное решение. Какие у него недостатки?")]
-		public void CheckCurrentTsar_WithCustomEquality()
-		{
-			var actualTsar = TsarRegistry.GetCurrentTsar();
-			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
-				new Person("Vasili III of Russia", 28, 170, 60, null));
-
-			// Какие недостатки у такого подхода? 
-			
-			/*
-			 * 1. Если мы решим изменить класс Person (например, добавить или удалить какие-то поля),
-			 * то тогда придется переписывать метод AreEqual
-			 * 2. Если тест не пройдет, то мы не увидим, какие именно поля у двух объектов не совпали.
-			 * Нам просто выдаст сообщение 'Expected: True, But was: False'
-			 */
-			
-			Assert.True(AreEqual(actualTsar, expectedTsar));
-		}
-
-		private static bool AreEqual(Person? actual, Person? expected)
-		{
-			if (actual == expected) return true;
-			if (actual == null || expected == null) return false;
-			return
-				actual.Name == expected.Name
-				&& actual.Age == expected.Age
-				&& actual.Height == expected.Height
-				&& actual.Weight == expected.Weight
-				&& AreEqual(actual.Parent, expected.Parent);
-		}
-	}
-
-	public abstract class TsarRegistry
+	public static class TsarRegistry
 	{
 		public static Person GetCurrentTsar()
 		{
@@ -77,8 +17,9 @@ namespace HomeExercises
 		public readonly int Height;
 		public readonly int Weight;
 		public readonly string Name;
-		public readonly Person? Parent;
+		public Person? Parent;
 		public int Id;
+
 
 		public Person(string name, int age, int height, int weight, Person? parent)
 		{
---
+++ cs/HomeExercisesTests/NumberValidatorTests.cs
@@ -0,0 +1,92 @@
+﻿using FluentAssertions;
+using HomeExercises;
+
+namespace HomeExercisesTests
+{
+	public class NumberValidatorTests
+	{
+		[TestCase(-1, 0, Description = "Precision should be a positive number")]
+		[TestCase(1, -1, Description = "Scale must be a non-negative number")]
+		[TestCase(1, 2, Description = "Scale must be less than precision")]
+		public void Constructor_ThrowsArgumentException_OnIncorrectInput(int precision, int scale)
+		{
+			Assert.Throws<ArgumentException>(() => new NumberValidator(precision, scale, true));
+		}
+
+		[Test]
+		public void Constructor_SuccessfullyCreatesObject_OnCorrectInput()
+		{
+			Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
+		}
+
+		[TestCase(null)]
+		[TestCase(" ")]
+		[TestCase("                   ")]
+		[TestCase("")]
+		[TestCase("\n")]
+		public void IsValidNumber_ReturnsFalse_WhenValueIsNullOrEmpty(string value)
+		{
+			var numberValidator = new NumberValidator(1, 0, true);
+
+			numberValidator.IsValidNumber(value).Should().BeFalse();
+		}
+
+		[TestCase(".")]
+		[TestCase("a.sd")]
+		[TestCase("+-5")]
+		[TestCase("1..3")]
+		[TestCase("1.")]
+		[TestCase(".3")]
+		public void IsValidNumber_ReturnsFalse_WhenValueIsNaN(string value)
+		{
+			var numberValidator = new NumberValidator(2, 1, true);
+
+			numberValidator.IsValidNumber(value).Should().BeFalse();
+		}
+
+		[TestCase("00.00", 3, 2)]
+		[TestCase("+1.00", 3, 2)]
+		[TestCase("-1.00", 3, 2)]
+		[TestCase("-1", 1, 0)]
+		public void IsValidNumber_ReturnsFalse_WhenIntAndFracPartsAreGreaterThanPrecision(string value, int precision,
+			int scale)
+		{
+			var numberValidator = new NumberValidator(precision, scale);
+
+			numberValidator.IsValidNumber(value).Should().BeFalse();
+		}
+
+		[TestCase("0.00", 3, 1)]
+		[TestCase("-123.12", 6, 0)]
+		public void IsValidNumber_ReturnsFalse_WhenFracPartIsGreaterThanScale(string value, int precision, int scale)
+		{
+			var numberValidator = new NumberValidator(precision, scale);
+
+			numberValidator.IsValidNumber(value).Should().BeFalse();
+		}
+
+		[TestCase("-1.2", 3, 1)]
+		[TestCase("-0.00", 4, 2)]
+		public void IsValidNumber_ReturnsFalse_WhenValueIsNegativeButOnlyPositiveIsTrue(string value, int precision,
+			int scale)
+		{
+			var numberValidator = new NumberValidator(precision, scale, true);
+
+			numberValidator.IsValidNumber(value).Should().BeFalse();
+		}
+
+		[TestCase("0.0", 17, 2, true)]
+		[TestCase("0.0", 2, 1, false)]
+		[TestCase("+1.23", 4, 2, true)]
+		[TestCase("-1.23", 4, 2, false)]
+		[TestCase("0,1", 2, 1, true)]
+		[TestCase("0", 1, 0, true)]
+		public void IsValidNumber_ReturnsTrue_OnCorrectInputData(string value, int precision, int scale,
+			bool onlyPositive)
+		{
+			var numberValidator = new NumberValidator(precision, scale, onlyPositive);
+
+			numberValidator.IsValidNumber(value).Should().BeTrue();
+		}
+	}
+}
