25ca7c7d123d933169a1cebaa64b9bcfd352051e
ShpakovDaniel <44341164+Reltig@users.noreply.github.com>
2023-11-28 18:18:35 +0500

    upd 2

--- cs/HomeExercises/NumberValidatorTests.cs
+++ cs/HomeExercises/NumberValidatorTests.cs
@@ -9,112 +9,86 @@ namespace HomeExercises
 {
 	public class NumberValidatorTests
 	{
-		#region TestCases
-
-		public static object[][] numberTestCases =
+		public static TestCaseData[] unsignedPositiveNumberTestCases =
 		{
-			new object[] { 1, 0, "0" },
-			new object[] { 2, 1, "0.0" },
-			new object[] { 1, 0, "1" },
-			new object[] { 2, 0, "12" },
-			new object[] { 2, 1, "1.2" },
-			new object[] { 3, 2, "1.23" },
-			new object[] { 3, 1, "12.3" },
-			new object[] { 4, 2, "12.34" },
+			new TestCaseData(1, 0, "0").SetName("unsigned zero"),
+			new TestCaseData(2, 1, "0.0").SetName("unsigned zero with 1 digit after"),
+			new TestCaseData(1, 0, "1").SetName("unsigned 1 digit number"),
+			new TestCaseData(2, 0, "12").SetName("unsigned 2 digit number"),
+			new TestCaseData(2, 1, "1.2").SetName("unsigned 2 digit number with 1 digit after point"),
+			new TestCaseData(3, 2, "1.23").SetName("unsigned 3 digit number with 2 digit after point"),
+			new TestCaseData(3, 1, "12.3").SetName("unsigned 3 digit number with 1 digit after point"),
+			new TestCaseData(4, 2, "12.34").SetName("unsigned 4 digit number with 2 digit after point"),
 		};
 
-		public static IEnumerable CorrectSignedNumbersCases()
+		public static TestCaseData[] signedPositiveNumberTestCases =
 		{
-			int precision, scale;
-			string validatingString;
-			foreach (var testCase in numberTestCases)
-			{
-				precision = (int)testCase[0];
-				scale = (int)testCase[1];
-				validatingString = (string)testCase[2];
-				yield return new TestCaseData(precision + 1, scale, $"+{validatingString}").Returns(true);
-				yield return new TestCaseData(precision + 1, scale, $"-{validatingString}").Returns(true);
-			}
+			new TestCaseData(2, 0, "+0").SetName("positive zero"),
+			new TestCaseData(3, 1, "+0.0").SetName("positive zero with 1 digit after"),
+			new TestCaseData(2, 0, "+1").SetName("positive 1 digit number"),
+			new TestCaseData(3, 0, "+12").SetName("positive 2 digit number"),
+			new TestCaseData(3, 1, "+1.2").SetName("positive 2 digit number with 1 digit after point"),
+			new TestCaseData(4, 2, "+1.23").SetName("positive 3 digit number with 2 digit after point"),
+			new TestCaseData(4, 1, "+12.3").SetName("positive 3 digit number with 1 digit after point"),
+			new TestCaseData(5, 2, "+12.34").SetName("positive 4 digit number with 2 digit after point"),
+		};
+
+		public static TestCaseData[] negativeNumberTestCases =
+		{
+			new TestCaseData(2, 0, "-0").SetName("negative zero"),
+			new TestCaseData(3, 1, "-0.0").SetName("negative zero with 1 digit after"),
+			new TestCaseData(2, 0, "-1").SetName("negative 1 digit number"),
+			new TestCaseData(3, 0, "-12").SetName("negative 2 digit number"),
+			new TestCaseData(3, 1, "-1.2").SetName("negative 2 digit number with 1 digit after point"),
+			new TestCaseData(4, 2, "-1.23").SetName("negative 3 digit number with 2 digit after point"),
+			new TestCaseData(4, 1, "-12.3").SetName("negative 3 digit number with 1 digit after point"),
+			new TestCaseData(5, 2, "-12.34").SetName("negative 4 digit number with 2 digit after point"),
+		};
+
+		public static TestCaseData[] wrongCases =
+		{
+			new TestCaseData(1, 0, "-1")
+				.SetName("integer number with \"+\" sign must have precision more than digits count").Returns(false),
+			new TestCaseData(1, 0, "+1")
+				.SetName("integer number with \"-\" sign must have precision more than digits count").Returns(false),
+			new TestCaseData(2, 1, "+1.2")
+				.SetName("number with fractional part with \"+\" sign must have precision more than digits count")
+				.Returns(false),
+			new TestCaseData(2, 1, "-1.2")
+				.SetName("number with fractional part with \"-\" sign must have precision more than digits count")
+				.Returns(false),
+		};
+
+		[TestCaseSource(nameof(unsignedPositiveNumberTestCases))]
+		[TestCaseSource(nameof(signedPositiveNumberTestCases))]
+		[TestCaseSource(nameof(negativeNumberTestCases))]
+		public void IsValidNumber(int precision, int scale, string validatingString)
+		{
+			new NumberValidator(precision, scale)
+				.IsValidNumber(validatingString)
+				.Should()
+				.BeTrue();
 		}
 
-		public static IEnumerable CorrectUnsignedNumbersCases()
-		{
-			foreach (var testCase in numberTestCases)
-			{
-				yield return new TestCaseData(testCase).Returns(true);
-			}
-		}
-
-		public static IEnumerable IncorrectUnsignedNumbersCases()
-		{
-			int precision, scale;
-			string validatingString;
-			foreach (var testCase in numberTestCases)
-			{
-				precision = (int)testCase[0];
-				scale = (int)testCase[1];
-				validatingString = (string)testCase[2];
-				if (precision > 1 && precision - 1 > scale)
-					yield return new TestCaseData(precision - 1, scale, validatingString).Returns(false);
-				if (scale > 0)
-					yield return new TestCaseData(testCase[0], scale - 1, validatingString).Returns(false);
-			}
-		}
-
-		public static IEnumerable IncorrectSignedNumbersCases()
-		{
-			int precision, scale;
-			string validatingString;
-			foreach (var testCase in numberTestCases)
-			{
-				precision = (int)testCase[0];
-				scale = (int)testCase[1];
-				validatingString = (string)testCase[2];
-				yield return new TestCaseData(precision, scale, $"+{validatingString}").Returns(false);
-				yield return new TestCaseData(precision, scale, $"-{validatingString}").Returns(false);
-				if (scale > 0 && precision > scale)
-				{
-					yield return new TestCaseData(precision + 1, scale - 1, $"+{validatingString}")
-						.Returns(false);
-					yield return new TestCaseData(precision + 1, scale - 1, $"-{validatingString}")
-						.Returns(false);
-				}
-			}
-		}
-
-		public static IEnumerable NegativeNumbers()
-		{
-			int precision;
-			string validatingString;
-			foreach (var testCase in numberTestCases)
-			{
-				precision = (int)testCase[0];
-				validatingString = (string)testCase[2];
-				yield return new TestCaseData(precision + 1, testCase[1], $"-{validatingString}").Returns(false);
-			}
-		}
-
-		#endregion
-
-		[TestCaseSource(nameof(CorrectSignedNumbersCases))]
-		[TestCaseSource(nameof(CorrectUnsignedNumbersCases))]
-		[TestCaseSource(nameof(IncorrectUnsignedNumbersCases))]
-		[TestCaseSource(nameof(IncorrectSignedNumbersCases))]
-		public bool ValidationTests(int precision, int scale, string validatingString)
+		[TestCaseSource(nameof(wrongCases))]
+		public bool IsNotValidNumber(int precision, int scale, string validatingString)
 		{
 			return new NumberValidator(precision, scale).IsValidNumber(validatingString);
 		}
 
-		[TestCaseSource(nameof(NegativeNumbers))]
-		public bool ShouldFailWhenNumberNegativeWithOnlyPositive(int precision, int scale, string validatingString)
+		[TestCaseSource(nameof(negativeNumberTestCases))]
+		public void ShouldFailWhenNumberNegativeWithOnlyPositive(int precision, int scale, string validatingString)
 		{
-			return new NumberValidator(precision, scale, true).IsValidNumber(validatingString);
+			new NumberValidator(precision, scale, true)
+				.IsValidNumber(validatingString)
+				.Should()
+				.BeFalse();
 		}
 
 		[TestCase(3, 2, "a.sd", TestName = "non digit symbols")]
 		[TestCase(2, 1, ".0", TestName = "must have digits before point")]
 		[TestCase(1, 0, "0.", TestName = "must have digits after point (if exist)")]
-		public void IsNotValid(int precision, int scale, string validatingString, bool onlyPositive = true)
+		public void WrongFormat(int precision, int scale, string validatingString, bool onlyPositive = true)
 		{
 			new NumberValidator(precision, scale, onlyPositive)
 				.IsValidNumber(validatingString)
@@ -129,8 +103,7 @@ namespace HomeExercises
 		[TestCase(1, 2, TestName = "precision less than scale")]
 		public void ShouldThrow(int precision, int scale, bool onlyPositive = true)
 		{
-			Action act = () => new NumberValidator(precision, scale, onlyPositive);
-			act.Should().Throw<ArgumentException>();
+			Assert.Throws<ArgumentException>(() => new NumberValidator(precision, scale, onlyPositive));
 		}
 	}
 
