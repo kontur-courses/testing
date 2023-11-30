8832ca8df8b8277d0f311f01c54310a60d1b8ab1
Irina_Yatsenko <yatsenko258@mail.ru>
2023-11-30 12:49:45 +0500

    Разделение классов по файлам

---
+++ cs/HomeExercises/TsarRegistry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HomeExercises
+{
+	public class TsarRegistry
+	{
+		public static Person GetCurrentTsar()
+		{
+			return new Person(
+				"Ivan IV The Terrible", 54, 170, 70,
+				new Person("Vasili III of Russia", 28, 170, 60, null));
+		}
+	}
+}
