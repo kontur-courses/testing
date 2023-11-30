fe880f5d86ce8d6f97a86b4d63fbc7baf562e6d8
dragInSky <cronosdversus@mail.ru>
2023-11-30 22:10:32 +0500

    Exclude only Person's Id's in ObjectComparisonTests.CheckCurrentTsar()

--- cs/HomeExercises/ObjectComparisonTests.cs
+++ cs/HomeExercises/ObjectComparisonTests.cs
@@ -18,7 +18,9 @@ namespace HomeExercises
 			// Код, который не нужно менять при добавлении новых свойств, при проверке исключаем поле Id
 			actualTsar.Should().BeEquivalentTo(
 				expectedTsar, config => config
-					.Excluding(person => person.SelectedMemberInfo.Name.Equals("Id"))
+					.Excluding(person => 
+						person.SelectedMemberInfo.DeclaringType.Equals(typeof(Person)) &&
+						person.SelectedMemberInfo.Name.Equals(nameof(Person.Id)))
 			);
 
 			/*
