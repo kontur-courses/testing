2eb7645abefb3e9b18284779c00cc175770c09fc
luvairo-m <luvairo-228@yandex.ru>
2023-11-30 17:17:53 +0500

    Исправление NumberValidator в соответствии с требованиями из pull request

--- cs/HomeExercises/HomeExercises/TsarRegistry.cs
+++ cs/HomeExercises/HomeExercises/TsarRegistry.cs
@@ -1,12 +1,11 @@
-﻿namespace HomeExercises
+﻿namespace HomeExercises;
+
+public class TsarRegistry
 {
-    public class TsarRegistry
+    public static Person GetCurrentTsar()
     {
-        public static Person GetCurrentTsar()
-        {
-            return new Person(
-                "Ivan IV The Terrible", 54, 170, 70,
-                new Person("Vasili III of Russia", 28, 170, 60, null));
-        }
+        return new Person(
+            "Ivan IV The Terrible", 54, 170, 70,
+            new Person("Vasili III of Russia", 28, 170, 60, null));
     }
 }
